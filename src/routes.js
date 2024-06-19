a88a598eb02a703b7f207d7987f3bc0ae45bb27d
Shubham <shubham.manohar@loveco.com>
2024-06-19 16:35:33 +0530

    Revert "login changes"
    This reverts commit 40325d853758776092af4cb758438179ffd12493.
    
    reverted yesterdays commit#

--- src/routes.js
+++ src/routes.js
@@ -1,39 +1,16 @@
 // import Testing from './views/features/meta_Integ/index2';
-import PublicLayout from './views/features/auth/Auth';
-import UserLogin from './views/features/auth/login/userLogin';
 import ChatLanding from './views/features/meta_Integ/ChatLanding';
 import ChatScreen from './views/features/meta_Integ/index';
-import AuthWrapper from './views/layouts/authWrapper';
 
 const routes = [
-	//public routes
-	{
-		path: '/user/:authAction',
-		component: (
-			<PublicLayout subTitle={'Log in to your workspace'}>
-				<UserLogin />
-			</PublicLayout>
-		),
-		exact: true,
-	},
-
-	//private routes
 	{
 		path: '/:workspaceId/chats',
-		component: (
-			<AuthWrapper>
-				<ChatLanding />
-			</AuthWrapper>
-		),
+		component: <ChatLanding />,
 		exact: true,
 	},
 	{
 		path: '/:workspaceId/chats/:pageName',
-		component: (
-			<AuthWrapper>
-				<ChatScreen />
-			</AuthWrapper>
-		),
+		component: <ChatScreen />,
 		exact: true,
 	},
 ];
