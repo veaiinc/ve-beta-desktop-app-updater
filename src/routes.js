5e21f715ebac6078860108d882b6ac503607aca5
bhogeshsrivasthavaatVE <bhogeshsrivasthava@ve.co>
2024-06-28 22:38:52 +0530

    removed builder npm

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,6 @@ import MyWorkFlowDetails from './views/features/sales/myWorkFlowDetails';
 import ProposalCRUD from './views/features/modules/proposalCRUD';
 import MainContentWrapper from './views/features/workspace_settings/MainContentWrapper';
 import MySettingsWrapper from './views/features/profile_settings/MySettingsWrapper';
-import VEBuilder from '@sandeepkollabathula.ve/ve-builder';
 
 const routes = [
 	{
@@ -102,15 +101,6 @@ const routes = [
 		component: (
 			<AuthWrapper title={'Profile Settings'}>
 				<MySettingsWrapper />
-			</AuthWrapper>
-		),
-		exact: true,
-	},
-	{
-		path: '/builder',
-		component: (
-			<AuthWrapper title={'Profile Settings'}>
-				<VEBuilder />
 			</AuthWrapper>
 		),
 		exact: true,
