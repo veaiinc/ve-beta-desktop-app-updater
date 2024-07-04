b5a61d7d69295d159ae4382a431d9f2ffd63f33d
bhogeshsrivasthavaatVE <bhogeshsrivasthava@ve.co>
2024-07-04 19:22:53 +0530

    commit

--- src/views/features/sales/index.jsx
+++ src/views/features/sales/index.jsx
@@ -40,6 +40,7 @@ function Sales({ type }) {
 		if (response[0]) {
 			setLoading(false);
 			setInsights(_.filter(response[1]));
+		} else {
 		}
 	};
 
@@ -47,8 +48,8 @@ function Sales({ type }) {
 		let response = await getTemplates();
 		if (response[0]) {
 			setLoading(false);
-			setGlobalWorkflows(_.filter(response[1], { tenantId: '*' }));
-			setMyWorkflows(_.filter(response[1], (item) => item.tenantId !== '*'));
+			setGlobalWorkflows(_.filter(response[1], { tenantId: null }));
+			setMyWorkflows(_.filter(response[1], (item) => item.tenantId !== null));
 		}
 	};
 
