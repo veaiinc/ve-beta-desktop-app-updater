2aff22d1d45be4ad1a507e4120ccd766a3673c04
bhogeshsrivasthavaatVE <bhogeshsrivasthava@ve.co>
2024-08-27 02:28:00 +0530

    inbox live dev

--- src/services/config.jsx
+++ src/services/config.jsx
@@ -1,17 +1,17 @@
-// module.exports = {
-// 	tenant_api: 'https://api.ve.co/tenants/1.0',
-// 	tenant_users_api: 'https://api.ve.co/tenant-users/1.0',
-// 	ve_conversations_api: 'https://api.ve.co/ve-conversations/1.0',
-// 	proposals_api: 'https://api.ve.co/proposals/1.0',
-// 	workflows_Api: 'https://api.ve.co/workflows/1.0',
-// 	// workflows_Api: 'http://192.168.6.184:6789',
-// };
+module.exports = {
+	tenant_api: 'https://api.ve.co/tenants/1.0',
+	tenant_users_api: 'https://api.ve.co/tenant-users/1.0',
+	ve_conversations_api: 'https://api.ve.co/ve-conversations/1.0',
+	proposals_api: 'https://api.ve.co/proposals/1.0',
+	workflows_Api: 'https://api.ve.co/workflows/1.0',
+	// workflows_Api: 'http://192.168.6.184:6789',
+};
 
 //updated Config will push later
-module.exports = {
-	tenant_api: 'https://ap.api.ve.ai/tenants/1.0',
-	tenant_users_api: 'https://ap.api.ve.ai/tenant-users/1.0',
-	ve_conversations_api: 'https://api.ve.co/ve-conversations/1.0',
-	proposals_api: 'https://ap.api.ve.ai/proposals/1.0',
-	workflows_Api: 'https://ap.api.ve.ai/workflows/1.0',
-};
+// module.exports = {
+// 	tenant_api: 'https://ap.api.ve.ai/tenants/1.0',
+// 	tenant_users_api: 'https://ap.api.ve.ai/tenant-users/1.0',
+// 	ve_conversations_api: 'https://api.ve.co/ve-conversations/1.0',
+// 	proposals_api: 'https://ap.api.ve.ai/proposals/1.0',
+// 	workflows_Api: 'https://ap.api.ve.ai/workflows/1.0',
+// };
