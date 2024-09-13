8bba92175efae8a60cc8b7bf21e801980ae4b432
Shubham <shubham.manohar@loveco.com>
2024-09-13 18:26:55 +0530

    send smart file popup

--- src/views/components/modalsV2/proposalModals/SendProposalModal.jsx
+++ src/views/components/modalsV2/proposalModals/SendProposalModal.jsx
@@ -8,9 +8,29 @@ import { ReactComponent as Clock } from '../../../../assets/svg/workflow/clock.s
 import { ReactComponent as QuestionMark } from '../../../../assets/svg/workflow/questionMark.svg';
 import { ReactComponent as Ai } from '../../../../assets/svg/workflow/ai.svg';
 import { ReactComponent as Message } from '../../../../assets/svg/workflow/message.svg';
+import { ReactComponent as Security } from '../../../../assets/svg/workflow/security.svg';
+import { ReactComponent as Profile } from '../../../../assets/svg/workflow/profile.svg';
+import { ReactComponent as Settings } from '../../../../assets/svg/workflow/settings.svg';
+import { ReactComponent as DownArrow } from '../../../../assets/svg/workflow/smallDownArrow.svg';
 import ToggleSlider from '../../../components/input/slider';
 import JoditEditor from 'jodit-react';
 import { message } from 'antd';
+
+const initialState = {
+	subject: '',
+	emailBody: '',
+	name: '',
+	enableLinkExpiry: false,
+	accessSettings: false,
+	showEmail: false,
+	enableLinkExpiry: false,
+	showCustomExpiryButton: false,
+	showAccessSettings: false,
+	expiryInDays: 0,
+	nameAccess: false,
+	emailAccess: false,
+};
+
 const SendProposalModal = ({
 	open,
 	closeModal,
@@ -33,13 +53,7 @@ const SendProposalModal = ({
 	} = useContext(Context);
 	const editor = useRef(null);
 
-	const [info, setInfo] = useState({
-		subject: '',
-		emailBody: '',
-		name: clientDetails?.name,
-		enableLinkExpiry: false,
-		accessSettings: false,
-	});
+	const [info, setInfo] = useState({ ...initialState, name: clientDetails?.name });
 
 	useEffect(() => {
 		getSendSmartFileEmailTemplate();
@@ -55,13 +69,21 @@ const SendProposalModal = ({
 		}
 	}, [smartFileEmailTemplateData]);
 
+	useEffect(() => {
+		if (info?.showEmail) {
+			document
+				.querySelector('.sendProposalContainer')
+				?.scrollIntoView({ behavior: 'smooth' });
+		}
+	}, [info?.showEmail]);
+
 	const handleCopy = useCallback(async () => {
 		try {
 			const workspaceId = localStorage.getItem('workspaceId');
 			await navigator.clipboard.writeText(
 				`https://${workspaceId}.ve.ai/portal/${workflowSlug}`,
 			);
-			closeModal();
+			modifiedCloseModal();
 			openCopyModal();
 
 			if (workflowStatus === 'enquiry') {
@@ -99,14 +121,62 @@ const SendProposalModal = ({
 		}
 	}, [clientDetails, workflowId, workflowStatus, info?.emailBody, info?.subject]);
 
+	const modifiedCloseModal = useCallback(() => {
+		closeModal();
+		setInfo({
+			...initialState,
+			name: clientDetails?.name,
+			subject: smartFileEmailTemplateData?.subject || '',
+			emailBody: smartFileEmailTemplateData?.htmlBody || '',
+		});
+	}, [smartFileEmailTemplateData]);
+
+	const incrementDecrementExpiry = useCallback(
+		(type) => {
+			let newValue;
+			if (type === 'increment') {
+				newValue = info?.expiryInDays ? info?.expiryInDays + 1 : 1;
+			} else {
+				newValue =
+					info?.expiryInDays && info?.expiryInDays - 1 ? info?.expiryInDays - 1 : 0;
+			}
+			setInfo((prev) => ({ ...prev, expiryInDays: newValue }));
+		},
+		[info?.expiryInDays],
+	);
+
+	const handleDaysButtonOnClick = useCallback(
+		(val) => {
+			if (info?.expiryInDays === val) {
+				return;
+			}
+			setInfo((prev) => ({ ...prev, expiryInDays: val }));
+		},
+		[info?.expiryInDays],
+	);
+
+	// Handle change for checkboxes
+	const handleCheckboxChange = useCallback((e) => {
+		const { name, checked } = e.target;
+		setInfo((prevState) => ({
+			...prevState,
+			[name]: checked,
+		}));
+	}, []);
+
 	return (
 		<ReactModal
 			isOpen={open}
-			closeModal={closeModal}
+			closeModal={modifiedCloseModal}
 			modalType={'center'}
 			customStyles={{ content: { borderRadius: '15px' } }}
 		>
-			<div className="sendSmartFileupdatedContainer">
+			<div
+				className={`sendSmartFileupdatedContainer ${info?.showEmail ? 'showEmail' : ''} ${
+					info?.enableLinkExpiry ? 'enableLinkExpiry' : ''
+				}    ${info?.showAccessSettings ? 'showAccessSettings' : ''}`}
+				style={{ overflowY: info?.showEmail ? 'auto' : 'hidden' }}
+			>
 				{/* setting screen */}
 				<div className="sendSmartFileSettingScreen">
 					{/* smart File Header */}
@@ -121,25 +191,204 @@ const SendProposalModal = ({
 					{/* smartFileSettings */}
 
 					<div className="configurationSettingsContainer">
-						<div className="settingsContainer">
-							<div className="settingsIconHolder">
-								<Clock />
+						<div className="settingsWrapper">
+							<div className="settingsContainer">
+								<div className="settingsIconHolder">
+									<Clock />
+								</div>
+								<div className="settingsLabelholder">
+									<span className="settingsLabelText">Enable Link Expiry</span>
+									<ToggleSlider
+										value={info?.enableLinkExpiry}
+										onChange={(val) =>
+											setInfo((prev) => ({ ...prev, enableLinkExpiry: val }))
+										}
+									/>
+								</div>
+
+								<span className="svgHolder">
+									<QuestionMark />
+								</span>
 							</div>
-							<div className="settingsLabelholder">
-								<span className="settingsLabelText">Enable Link Expiry</span>
-								<ToggleSlider />
-							</div>
-							<span className="svgHolder">
-								<QuestionMark />
-							</span>
+							{info?.enableLinkExpiry ? (
+								<div className="enableLinkSettingsContainer">
+									<div className="linkExpireLabelContainer">
+										<span className="expiryHeadingLabel">
+											Days till file expires
+										</span>
+										<span className="expirySubLabel">
+											Link Expires on 5 Oct 2024
+										</span>
+									</div>
+									{!info?.showCustomExpiryButton ? (
+										<div className="linkExpiryInputParentContainer">
+											<div
+												className="daysButtons"
+												style={{
+													color: info?.expiryInDays === 7 ? '#fff' : '',
+													border:
+														info?.expiryInDays === 7
+															? '1px solid #fff'
+															: '',
+												}}
+												onClick={() => handleDaysButtonOnClick(7)}
+											>
+												7d
+											</div>
+											<div
+												className="daysButtons"
+												style={{
+													color: info?.expiryInDays === 15 ? '#fff' : '',
+													border:
+														info?.expiryInDays === 15
+															? '1px solid #fff'
+															: '',
+												}}
+												onClick={() => handleDaysButtonOnClick(15)}
+											>
+												15d
+											</div>
+											<div
+												className="daysButtons"
+												style={{
+													color: info?.expiryInDays === 30 ? '#fff' : '',
+													border:
+														info?.expiryInDays === 30
+															? '1px solid #fff'
+															: '',
+												}}
+												onClick={() => handleDaysButtonOnClick(30)}
+											>
+												30d
+											</div>
+											<span
+												className="customDateButton"
+												onClick={() =>
+													setInfo((prev) => ({
+														...prev,
+														showCustomExpiryButton: true,
+													}))
+												}
+											>
+												Custom
+											</span>
+										</div>
+									) : (
+										<div className="linkExpiryInputParentContainer">
+											<span
+												className="svgHolder"
+												onClick={() =>
+													setInfo((prev) => ({
+														...prev,
+														showCustomExpiryButton: false,
+													}))
+												}
+											>
+												<Close />
+											</span>
+											<div className="incrementContainer">
+												<span
+													className=" incrementFontstyling"
+													onClick={() =>
+														incrementDecrementExpiry('decrement')
+													}
+												>
+													-
+												</span>
+												<span className="incrementFontstyling">
+													{info?.expiryInDays || 0}
+												</span>
+												<span
+													className="incrementFontstyling"
+													onClick={() =>
+														incrementDecrementExpiry('increment')
+													}
+												>
+													+
+												</span>
+											</div>
+											<span className="customDateButton">Days</span>
+										</div>
+									)}
+								</div>
+							) : (
+								''
+							)}
 						</div>
-						<div className="settingsContainer">
-							<div className="settingsIconHolder">
-								<Clock />
+
+						<div className="settingsWrapper">
+							<div
+								className="settingsContainer"
+								onClick={() =>
+									setInfo((prev) => ({
+										...prev,
+										showAccessSettings: !prev.showAccessSettings,
+									}))
+								}
+							>
+								<div className="settingsIconHolder">
+									<Settings />
+								</div>
+								<div className="settingsLabelholder">
+									<span className="settingsLabelText">Access Settings</span>
+									<DownArrow />
+								</div>
 							</div>
-							<div className="settingsLabelholder">
-								<span className="settingsLabelText">Access Settings</span>
-							</div>
+							{info?.showAccessSettings ? (
+								<div className="accessSettingsContainer">
+									<div className="identificationContainer">
+										<div className="accessSettingsLabelContainer">
+											<Profile />
+											<span className="headingLabel">Identification</span>
+										</div>
+										<div className="checkBoxContainer">
+											<div className="checkboxWrapper">
+												<input
+													type="checkbox"
+													className="sendSmartFileCheckbox"
+													checked={info?.nameAccess}
+													name="nameAccess"
+													onChange={handleCheckboxChange}
+												/>
+												<span className="checkboxLabel">Name</span>
+											</div>
+											<div className="checkboxWrapper">
+												<input
+													type="checkbox"
+													className="sendSmartFileCheckbox"
+													checked={info?.emailAccess}
+													onChange={handleCheckboxChange}
+													name="emailAccess"
+												/>
+												<span className="checkboxLabel">Email</span>
+											</div>
+										</div>
+									</div>
+
+									<div className="securityContainer">
+										<div className="accessSettingsLabelContainer">
+											<Security />
+											<span className="headingLabel">Security</span>
+										</div>
+										<div className="checkBoxContainer">
+											<div className="checkboxWrapper">
+												<input
+													type="checkbox"
+													className="sendSmartFileCheckbox"
+													checked={info?.emailAccess}
+													onChange={handleCheckboxChange}
+													name="emailAccess"
+												/>
+												<span className="checkboxLabel">
+													Email Verification
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							) : (
+								''
+							)}
 						</div>
 					</div>
 
@@ -153,115 +402,78 @@ const SendProposalModal = ({
 					</div>
 
 					<div className="sendSmartFileBtnContainer">
-						<div className="sendSmartFileBtn">
+						<div
+							className="sendSmartFileBtn"
+							onClick={() => {
+								setInfo((prev) => ({ ...prev, showEmail: true }));
+							}}
+						>
 							<Message />
 							Send Email
 						</div>
 						<span className="orText">OR</span>
 						<div className="copyTextDiv">
-							<span className="copyLinkText">Copy Link</span>
+							<span className="copyLinkText" onClick={handleCopy}>
+								Copy Link
+							</span>
 							<QuestionMark />
 						</div>
 					</div>
 				</div>
-				<div className="sendProposalContainer">
-					<div className="uppercontainer">
-						<div className="sendSmartFileHeaderContainer">
-							<span className="sendSmartFileHeaderTitle">Send smart file</span>
-						</div>
+				{info?.showEmail ? (
+					<div className="sendProposalContainer">
+						<div className="uppercontainer">
+							<div className="sendSmartFileHeaderContainer">
+								<span className="sendSmartFileHeaderTitle">Send smart file</span>
+							</div>
 
-						<div className="inputWrapperForSendSmartFile">
-							<span className="inputlabel">Email to</span>
-							<input
-								type="email"
-								value={clientDetails?.email}
-								disabled
-								className="inputForSendSmartFile"
-							/>
-						</div>
+							<div className="inputWrapperForSendSmartFile">
+								<span className="inputlabel">Email to</span>
+								<input
+									type="email"
+									value={clientDetails?.email}
+									disabled
+									className="inputForSendSmartFile"
+								/>
+							</div>
 
-						<div className="inputWrapperForSendSmartFile">
-							<span className="inputlabel">Subject Line Here</span>
-							<input
-								type="text"
-								value={info?.subject}
-								disabled
-								className="inputForSendSmartFile"
-							/>
-						</div>
-
-						<div className="inputWrapperForSendSmartFile">
-							<span className="inputlabel">Email Body Here</span>
-							<div className="joditWrapper">
-								<JoditEditor
-									ref={editor}
-									value={info?.emailBody}
-									tabIndex={1} // tabIndex of textarea
-									onChange={(newContent) =>
-										setInfo((prev) => ({ ...prev, emailBody: newContent }))
+							<div className="inputWrapperForSendSmartFile">
+								<span className="inputlabel">Subject Line Here</span>
+								<input
+									type="text"
+									value={info?.subject}
+									className="inputForSendSmartFile"
+									onChange={(e) =>
+										setInfo((prev) => ({ ...prev, subject: e.target.value }))
 									}
 								/>
 							</div>
-						</div>
 
-						<div className="sendEmailBtn" onClick={handleSendProposalViaEmail}>
-							Send Email
+							<div className="inputWrapperForSendSmartFile">
+								<span className="inputlabel">Email Body Here</span>
+								<div className="joditWrapper">
+									<JoditEditor
+										ref={editor}
+										value={info?.emailBody}
+										tabIndex={1} // tabIndex of textarea
+										onChange={(newContent) =>
+											setInfo((prev) => ({ ...prev, emailBody: newContent }))
+										}
+									/>
+								</div>
+							</div>
+
+							<div className="sendEmailBtn" onClick={handleSendProposalViaEmail}>
+								Send Email
+							</div>
 						</div>
 					</div>
-				</div>
+				) : (
+					''
+				)}
 			</div>
 		</ReactModal>
 	);
 };
 
 export default memo(SendProposalModal);
-
-{
-	/* <div>
-	<div className="sendProposalContainer">
-		<div className="uppercontainer">
-			<div className="sendSmartFileHeaderContainer">
-				<span className="sendSmartFileHeaderTitle">Send smart file</span>
-			</div>
-
-			<div className="inputWrapperForSendSmartFile">
-				<span className="inputlabel">Email to</span>
-				<input
-					type="email"
-					value={clientDetails?.email}
-					disabled
-					className="inputForSendSmartFile"
-				/>
-			</div>
-
-			<div className="inputWrapperForSendSmartFile">
-				<span className="inputlabel">Subject Line Here</span>
-				<input
-					type="text"
-					value={info?.subject}
-					disabled
-					className="inputForSendSmartFile"
-				/>
-			</div>
-
-			<div className="inputWrapperForSendSmartFile">
-				<span className="inputlabel">Email Body Here</span>
-				<div className="joditWrapper">
-					<JoditEditor
-						ref={editor}
-						value={info?.emailBody}
-						tabIndex={1} // tabIndex of textarea
-						onChange={(newContent) =>
-							setInfo((prev) => ({ ...prev, emailBody: newContent }))
-						}
-					/>
-				</div>
-			</div>
-
-			<div className="sendEmailBtn" onClick={handleSendProposalViaEmail}>
-				Send Email
-			</div>
-		</div>
-	</div>
-</div>; */
-}
