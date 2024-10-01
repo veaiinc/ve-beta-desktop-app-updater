b6bf6363229d6fbdd95856bfcda3acaaecd34169
Shubham <shubham.manohar@ve.co>
2024-10-01 15:47:38 +0530

    added privacy policy route

--- src/views/components/modalsV2/landingPage/PrivacyPolicyModal.jsx
+++ src/views/components/modalsV2/landingPage/PrivacyPolicyModal.jsx
@@ -1,9 +1,28 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
 import ReactModal from '../../modalsV2/index';
 import '../../../../assets/scss/landingScreen/privacyModal.scss';
 import { ReactComponent as CrossIcon } from '../../../../assets/svg/close.svg';
 import { ReactComponent as DownloadIcon } from '../../../../assets/scss/landingScreen/downloadIcon.svg';
+import { useNavigate } from 'react-router-dom';
 const PrivacyPolicyModal = ({ isOpen, closeModal }) => {
+	const navigate = useNavigate();
+
+	const handlePrivacyPolicyClickHandler = useCallback(() => {
+		return navigate('/privacy-policy');
+	}, []);
+
+	const handleDownload = useCallback(() => {
+		// Google Drive direct download link
+		const downloadUrl =
+			'https://drive.google.com/uc?export=download&id=1ktST1T5uBCs-LlPvLTKmpVBKe09HhxOj';
+
+		const link = document.createElement('a');
+		link.href = downloadUrl;
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	}, []);
+
 	return (
 		<ReactModal isOpen={isOpen} closeModal={closeModal}>
 			<div className={'PrivacyAndTermsModalContainer'}>
@@ -13,7 +32,10 @@ const PrivacyPolicyModal = ({ isOpen, closeModal }) => {
 				<h1 className={'heading'}>Your Data Privacy and Security</h1>
 				<ul>
 					<li>
-						<span>Privacy Policy</span> <DownloadIcon />
+						<span onClick={handlePrivacyPolicyClickHandler}>Privacy Policy</span>{' '}
+						<span onClick={handleDownload}>
+							<DownloadIcon />
+						</span>
 					</li>
 					<li>
 						<span>AI Terms & Conditions</span> <DownloadIcon />
