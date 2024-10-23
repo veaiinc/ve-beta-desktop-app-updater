b505cf34a7a2ec1d059e6adde856b2778dc2b54d
Your Name <shubham.manohar@ve.co>
2024-10-23 13:09:16 +0530

    added comments

--- src/views/components/smartFileComponets/Services.jsx
+++ src/views/components/smartFileComponets/Services.jsx
@@ -99,7 +99,7 @@ const Services = ({ serviceData, serviceOnChangeFunc, editable }) => {
 			//for service subtotal value
 			if (type === 'subTotalValue') {
 				let value = val?.replace(/[^0-9]/g, '');
-				//fetching subtotal value incoming styling and replacing it with new value
+				//fetching subtotal value  with incoming styling and replacing it with new value
 				let subtotalValueWithTags =
 					info?.subTotalValueMapper?.[outerIndex]?.itsHtmlTags || '';
 				let incomingValue =
