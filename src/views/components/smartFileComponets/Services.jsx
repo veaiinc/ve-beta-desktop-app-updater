bdad7f5bb827a71fd9fd05bb59760282e5679a98
Your Name <shubham.manohar@ve.co>
2024-10-23 19:37:11 +0530

    subtotal with text styling

--- src/views/components/smartFileComponets/Services.jsx
+++ src/views/components/smartFileComponets/Services.jsx
@@ -99,21 +99,27 @@ const Services = ({ serviceData, serviceOnChangeFunc, editable }) => {
 			//for service subtotal value
 			if (type === 'subTotalValue') {
 				let value = val?.replace(/[^0-9]/g, '');
-				//fetching subtotal value  with incoming styling and replacing it with new value
-				// let subtotalValueWithTags =
-				// 	info?.subTotalValueMapper?.[outerIndex]?.itsHtmlTags || '';
-				// let incomingValue =
-				// 	subtotalValueWithTags
-				// 		?.replace(/&nbsp;/g, ' ')
-				// 		.replace(/<\/?[^>]+(>|$)/g, '')
-				// 		.replace(/"/g, '') || '';
-				// subtotalValueWithTags = subtotalValueWithTags?.replace(incomingValue, value);
-
 				let { style } = selectedServiceTable || {};
 				style = { ...style, subTotalValue: value };
 				selectedServiceTable.style = style;
 				updatedData?.splice(outerIndex, 1, selectedServiceTable);
 				setInfo((prev) => ({ ...prev, data: updatedData }));
+
+				//fetching subtotal value  with incoming styling and replacing it with new value
+				let subtotalValueWithTags =
+					info?.subTotalValueMapper?.[outerIndex]?.itsHtmlTags || '';
+				let incomingValue =
+					subtotalValueWithTags
+						?.replace(/&nbsp;/g, ' ')
+						.replace(/<\/?[^>]+(>|$)/g, '')
+						.replace(/"/g, '') || '';
+				subtotalValueWithTags = subtotalValueWithTags?.replace(incomingValue, value);
+
+				selectedServiceTable = {
+					...selectedServiceTable,
+					style: { ...selectedServiceTable.style, subTotalValue: subtotalValueWithTags },
+				};
+
 				serviceOnChangeFunc(selectedServiceTable, outerIndex);
 				return;
 			}
