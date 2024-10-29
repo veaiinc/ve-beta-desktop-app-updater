3329e256ac763714b6807d4f66487f2162c33f0c
SheshantManure <sheshantmanure@ve.co>
2024-10-29 16:52:43 +0530

    UPDATE: Add new font to assets and import in index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ContextState from './context/ContextStates';
 import './assets/fonts/Inter/inter.css';
 import './assets/fonts/brasley/brasley.css';
+import './assets/fonts/brasley/brasley.css';
 import './assets/scss/global.scss';
 import './index.css';
 
