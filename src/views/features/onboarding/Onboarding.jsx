d110e6ac308e863b2c2a00349cb02de6b6225202
SheshantManure <sheshantmanure@ve.co>
2024-11-09 15:46:38 +0530

    UPD: Onboarding route protect

--- src/views/features/onboarding/Onboarding.jsx
+++ src/views/features/onboarding/Onboarding.jsx
@@ -10,7 +10,6 @@ import Profession from '../../components/onboarding/Profession';
 import { updateUserDetails, createWorkspace } from '../../../services/authServices/authServices';
 import { message } from 'antd';
 import CreatingNewWorkspace from '../../components/onboarding/CreatingNewWorkspace';
-import { checkUserSessionStatus } from '../../../services/authServices/authServices';
 
 const tl = gsap.timeline();
 const tl2 = gsap.timeline();
@@ -28,10 +27,6 @@ const Onboarding = () => {
 		isOnboard: false,
 	});
 
-	const [isOnboard, setIsOnboard] = useState(false);
-	const [tokenValid, setTokenValid] = useState(false);
-	const [workspaceIds, setWorkspaceIds] = useState([]);
-
 	const navigate = useNavigate();
 
 	const aiIntroRef = useRef(null);
@@ -43,32 +38,12 @@ const Onboarding = () => {
 	const step7Ref = useRef(null);
 
 	useEffect(() => {
-		checkUserSessionStatus()
-			.then((response) => {
-				if (response?.ok) {
-					setIsOnboard(response?.isOnboard);
-					setTokenValid(response?.tokenValid);
-					setWorkspaceIds(response?.workspaceIds);
-				}
-			})
-			.catch((error) => {
-				console.error('Error checking user session status:', error);
-			});
-	}, []);
-
-	useEffect(() => {
-		if (!isOnboard && tokenValid) {
-			if (workspaceIds?.length >= 1) {
-				navigate('/home');
-			} else {
-				navigate('/onboarding');
-			}
-		} else if (isOnboard && tokenValid) {
-			navigate('/home');
-		} else if (!tokenValid) {
+		if (!localStorage?.getItem('usertoken')) {
 			navigate('/');
+		} else if (localStorage?.getItem('isOnboard')) {
+			navigate('/home');
 		}
-	}, [isOnboard, tokenValid, workspaceIds]);
+	}, []);
 
 	useEffect(() => {
 		if (info?.step === 0 && aiIntroRef?.current) {
