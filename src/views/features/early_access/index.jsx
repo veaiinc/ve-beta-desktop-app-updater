56a432b3b1f3c294c3e0202e8206be7bf7394629
SheshantManure <sheshantmanure@ve.co>
2024-11-09 18:03:44 +0530

    UPD: Add loader to early-access pg

--- src/views/features/early_access/index.jsx
+++ src/views/features/early_access/index.jsx
@@ -7,6 +7,7 @@ import { ReactComponent as LinkedIn } from '../../../assets/svg/earlyAccess/link
 import { ReactComponent as Instagram } from '../../../assets/svg/earlyAccess/instagram.svg';
 import { useNavigate } from 'react-router-dom';
 import Context from '../../../context/context';
+import InitialPageLoader from '../../components/loaders/PageLoader';
 
 let isOnboard = JSON.parse(localStorage.getItem('isOnboard'));
 const usertoken = localStorage.getItem('usertoken');
@@ -54,26 +55,34 @@ const EarlyAccess = () => {
 
 	return (
 		<div className="earlyAccessParentContainer">
-			<VE />
-			<div className="backdropStuff"></div>
-			<div className="earlyAccessScreenTextContainer">
-				<span className="earlyAccessMainText">
-					Your request for <br></br>
-					<span className="earlyAccessText">Early access</span> is submitted!
-				</span>
-				<span className="earlyAccessSubText">
-					You will receive an email once your request is approved!
-				</span>
-			</div>
-			{/* footer */}
-			<div className="earlyAccessFooter">
-				<span className="earlyAccessFooterText">Follow us for more updates</span>
-				<div className="earlyAccessFooterIconHolder">
-					<Facebook />
-					<LinkedIn />
-					<Instagram />
+			{info?.loading ? (
+				<div className="loading-container">
+					<InitialPageLoader />
 				</div>
-			</div>
+			) : (
+				<>
+					<VE />
+					<div className="backdropStuff"></div>
+					<div className="earlyAccessScreenTextContainer">
+						<span className="earlyAccessMainText">
+							Your request for <br></br>
+							<span className="earlyAccessText">Early access</span> is submitted!
+						</span>
+						<span className="earlyAccessSubText">
+							You will receive an email once your request is approved!
+						</span>
+					</div>
+					{/* footer */}
+					<div className="earlyAccessFooter">
+						<span className="earlyAccessFooterText">Follow us for more updates</span>
+						<div className="earlyAccessFooterIconHolder">
+							<Facebook />
+							<LinkedIn />
+							<Instagram />
+						</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
