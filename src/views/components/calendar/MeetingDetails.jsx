ae7e0aae393d1b7e00ec16edc23ce2609ed5a4f4
Huemn <huemn@Huemns-MacBook-Air-2.local>
2024-11-10 21:18:56 +0530

    WIP: MeetingDetails Calendar Component

--- src/views/components/calendar/MeetingDetails.jsx
+++ src/views/components/calendar/MeetingDetails.jsx
@@ -1,7 +1,8 @@
 import React, { memo } from 'react';
+import '../../../assets/scss/calendar/meetingDetails.scss';
 
 const MeetingDetails = () => {
-	return <div>MeetingDetails</div>;
+	return <div className="meetingDetailsParentContainer">MeetingDetails</div>;
 };
 
 export default memo(MeetingDetails);
