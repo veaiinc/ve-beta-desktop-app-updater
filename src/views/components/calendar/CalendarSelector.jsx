530d8c9af01f174cb8605aadc72c3b72aa18c7c5
preetam <preetamsingh@ve.co>
2024-11-11 20:22:15 +0530

    WIP: CalendarSelector component

--- src/views/components/calendar/CalendarSelector.jsx
+++ src/views/components/calendar/CalendarSelector.jsx
@@ -1,17 +1,26 @@
 import React, { memo } from 'react';
 import '../../../assets/scss/calendar/calendarSelector.scss';
+import { ReactComponent as LeftSvg } from '../../../assets/svg/activity/left.svg';
+import { ReactComponent as OpenCalSvg } from '../../../assets/svg/calendar/openCalendar.svg';
+import { ReactComponent as RightSvg } from '../../../assets/svg/activity/right.svg';
 
 const CalendarSelector = () => {
 	return (
 		<div className="calendarSelectorParentContainer">
 			<div className="calendarContainer">
-				<div className="calendarHeader">
-					<div>
-						<div>September 2024</div>
-						<div>
-							<span>A</span>
-							<span>B</span>
-						</div>
+				<div className="calendarHeaderContainer">
+					<div className="headerLabelWrapper">
+						<span>September 2024</span>
+
+						<OpenCalSvg />
+					</div>
+					<div className="headerButtons">
+						<span>
+							<LeftSvg />
+						</span>
+						<span>
+							<RightSvg />
+						</span>
 					</div>
 				</div>
 				<div className="calendarBody">choose calendar</div>
