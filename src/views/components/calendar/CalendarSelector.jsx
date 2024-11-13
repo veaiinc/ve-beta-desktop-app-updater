c5b7bb15d1845d45330de20fbd554530cdfa39dc
preetam <preetamsingh@ve.co>
2024-11-13 18:18:25 +0530

    WIP: CalendarSelector DayPicker component

--- src/views/components/calendar/CalendarSelector.jsx
+++ src/views/components/calendar/CalendarSelector.jsx
@@ -1,14 +1,23 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/style.css';
 import '../../../assets/scss/calendar/calendarSelector.scss';
-import { ReactComponent as LeftSvg } from '../../../assets/svg/activity/left.svg';
-import { ReactComponent as OpenCalSvg } from '../../../assets/svg/calendar/openCalendar.svg';
-import { ReactComponent as RightSvg } from '../../../assets/svg/activity/right.svg';
+// import { ReactComponent as LeftSvg } from '../../../assets/svg/activity/left.svg';
+// import { ReactComponent as OpenCalSvg } from '../../../assets/svg/calendar/openCalendar.svg';
+// import { ReactComponent as RightSvg } from '../../../assets/svg/activity/right.svg';
 
 const CalendarSelector = () => {
+	const [info, setInfo] = useState({
+		selectedDate: new Date(),
+	});
+
+	const handleSelectDate = useCallback((selectedDate) => {
+		setInfo((prevInfo) => ({ ...prevInfo, selectedDate }));
+	}, []);
 	return (
 		<div className="calendarSelectorParentContainer">
 			<div className="calendarContainer">
-				<div className="calendarHeaderContainer">
+				{/* <div className="calendarHeaderContainer">
 					<div className="headerLabelWrapper">
 						<span>September 2024</span>
 
@@ -22,8 +31,17 @@ const CalendarSelector = () => {
 							<RightSvg />
 						</span>
 					</div>
-				</div>
-				<div className="calendarBody">choose calendar</div>
+				</div> */}
+				{/* <div className="calendarBody">
+				</div> */}
+				<DayPicker
+					mode="single"
+					selected={info?.selectedDate}
+					onSelect={handleSelectDate}
+					captionLayout="dropdown"
+					weekStartsOn={1}
+					// showOutsideDays
+				/>
 			</div>
 		</div>
 	);
