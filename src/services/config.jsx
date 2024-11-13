1ef2c7dfd919ac56f8a7fb71435a07ab2f33bec9
SheshantManure <sheshantmanure@ve.co>
2024-11-13 17:06:27 +0530

    Update: set production base urls

--- src/services/config.jsx
+++ src/services/config.jsx
@@ -1,16 +1,13 @@
 module.exports = {
 	//india
 	tenant_api: 'https://ap.api.ve.ai/tenants/1.0',
-	// tenant_users_api: 'https://ap.api.ve.ai/tenant-users/1.0',
-	tenant_users_api: 'http://192.168.2.190:9876',
+	tenant_users_api: 'https://ap.api.ve.ai/tenant-users/1.0',
 	ve_conversations_api: 'https://api.ve.co/ve-conversations/1.0',
 	proposals_api: 'https://ap.api.ve.ai/proposals/1.0',
 	workflows_Api: 'https://ap.api.ve.ai/workflows/1.0',
-	// auth_Api: 'https://auth.ve.ai',
-	auth_Api: 'http://192.168.2.190:5050', // Need to change to production base url
+	auth_Api: 'https://auth.ve.ai',
 	activity_api: 'https://ap.api.ve.ai/workflow-user-analytics/1.0',
 	galleries: 'https://ap.api.ve.ai/galleries/1.0',
-	// ai_setup_api: 'https://ap.api.ve.ai/sales-chat-assistant/1.0',
 	ai_assistant_api: 'https://api.ap-south-1.ve.ai',
 
 	//outside India
@@ -19,6 +16,6 @@ module.exports = {
 	ve_conversations_api_US: 'https://us.api.ve.ai/ve-conversations/1.0',
 	proposals_api_US: 'https://us.api.ve.ai/proposals/1.0',
 	workflows_Api_US: 'https://us.api.ve.ai/workflows/1.0',
-	ai_assistant_api_US: 'https://api.us-east-1.ve.ai', // Need confirmation regarding US region
+	ai_assistant_api_US: 'https://api.us-east-1.ve.ai',
 	activity_api_US: 'https://ap.api.ve.ai/workflow-user-analytics/1.0',
 };
