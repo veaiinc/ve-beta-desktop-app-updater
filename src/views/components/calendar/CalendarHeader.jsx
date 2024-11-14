48f6ed4a1525585fd70c8fc1c6453e4cbbf7551c
preetam <preetamsingh@ve.co>
2024-11-14 13:33:56 +0530

    WIP: CalendarHeader - WeekDayHeader - WeekHeader

--- src/views/components/calendar/CalendarHeader.jsx
+++ src/views/components/calendar/CalendarHeader.jsx
@@ -1,8 +1,24 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import '../../../assets/scss/calendar/calendarHeader.scss';
 import { ReactComponent as Down } from '../../../assets/svg/activity/down.svg';
+import WeekHeader from './WeekHeader';
+import DayHeader from './DaysHeader';
+import WeekDayHeader from './WeekDayHeader';
 
 const CalendarHeader = () => {
+	const [info, setInfo] = useState({
+		activeView: 'Month',
+	});
+
+	const handleToggleView = useCallback((view) => {
+		setInfo((prevInfo) => ({ ...prevInfo, activeView: view }));
+	}, []);
+
+	const views = {
+		Month: <WeekHeader />,
+		Week: <WeekDayHeader />,
+		Day: <DayHeader />,
+	};
 	return (
 		<>
 			{/* <!-- Header Section --> */}
@@ -12,9 +28,18 @@ const CalendarHeader = () => {
 						<div className="calendarDate">September, 2024</div>
 						<div className="viewToggleWrapper">
 							<div className="viewToggle">
-								<span className="toggleButton active">Month</span>
-								<span className="toggleButton">Week</span>
-								<span className="toggleButton">Day</span>
+								{/* map on views Object  */}
+								{Object.keys(views)?.map((view) => (
+									<span
+										key={view}
+										className={`toggleButton ${
+											info?.activeView === view ? 'active' : ''
+										}`}
+										onClick={() => handleToggleView(view)}
+									>
+										{view}
+									</span>
+								))}
 							</div>
 						</div>
 						<div className="calendarHeaderRight">
@@ -29,31 +54,8 @@ const CalendarHeader = () => {
 						</div>
 					</div>
 
-					<div className="weekHeader">
-						<div className="weekHeader">
-							<div className="dayHeader">
-								<span>Sunday</span>
-							</div>
-							<div className="dayHeader">
-								<span>Monday</span>
-							</div>
-							<div className="dayHeader">
-								<span>Tuesday</span>
-							</div>
-							<div className="dayHeader">
-								<span>Wednesday</span>
-							</div>
-							<div className="dayHeader">
-								<span>Thursday</span>
-							</div>
-							<div className="dayHeader">
-								<span>Friday</span>
-							</div>
-							<div className="dayHeader">
-								<span>Saturday</span>
-							</div>
-						</div>
-					</div>
+					{/* Render the active calendar day Header */}
+					{views[info?.activeView]}
 				</div>
 			</div>
 		</>
---
+++ src/views/components/calendar/WeekDayHeader.jsx
@@ -0,0 +1,44 @@
+import React, { memo, useCallback, useState } from 'react';
+import '../../../assets/scss/calendar/calendarHeader.scss';
+
+const WeekDayHeader = () => {
+	const [info, setInfo] = useState({
+		selectedWeekday: null,
+	});
+
+	const handleSelectedDay = useCallback((day) => {
+		setInfo((prevInfo) => ({
+			...prevInfo,
+			selectedWeekday: day,
+		}));
+	}, []);
+
+	const days = [
+		{ name: 'Monday', date: '12' },
+		{ name: 'Tuesday', date: '13' },
+		{ name: 'Wednesday', date: '14' },
+		{ name: 'Thursday', date: '15' },
+		{ name: 'Friday', date: '16' },
+		{ name: 'Saturday', date: '17' },
+		{ name: 'Sunday', date: '18' },
+	];
+
+	return (
+		<div className="weekDayHeaderContainer">
+			{days?.map((day, index) => (
+				<div
+					key={index}
+					className={`weekDayBlock ${info?.selectedWeekday === index ? 'active' : ''}`}
+					onClick={() => handleSelectedDay(index)}
+				>
+					<div className="weekDayWrapper">
+						<span style={{ color: 'rgba(228, 229, 230, 0.48)' }}>{day.name}</span>
+						<span style={{ color: '#E4E5E6' }}>{day.date}</span>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default memo(WeekDayHeader);
