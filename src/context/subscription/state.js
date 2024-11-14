176dbe7f01d9fbcb8520fff1371e7ce9a4d3b7b1
Your Name <shubham.manohar@ve.co>
2024-11-14 13:37:41 +0530

    subscription changes

---
+++ src/context/subscription/state.js
@@ -0,0 +1,101 @@
+import service from '../../services/graphQlServices';
+import { message } from 'antd';
+import {} from './graphqlFunctions';
+import { useReducer } from 'react';
+import Reducer from './reducer';
+import { Actions } from './Actions';
+import Service from '../../services/index';
+
+export const intialState = {
+	subscriptionPlans: null,
+	coupons: null,
+};
+
+export const SubscriptionState = (props) => {
+	const [state, dispatch] = useReducer(Reducer, intialState);
+
+	const getAllSubscriptionPlan = async () => {
+		try {
+			let workspaceId = localStorage.getItem('workspaceId');
+			let usertoken = localStorage.getItem('usertoken');
+			const response = await Service.fetchGet(
+				`/subscription/${workspaceId}/subscription-plans`,
+				usertoken,
+				'auth',
+			);
+			if (response?.[0] === true) {
+				dispatch({
+					type: Actions.GET_ALL_SUBSCRIPTION_PLAN_SUCCESS,
+					payload: response?.[1],
+				});
+			} else {
+				message.error('Unable to fetch subscription plans');
+				console.log('api failed ==>getAllSubscriptionPlan', response);
+			}
+		} catch (error) {
+			console.log('errror ==>getAllSubscriptionPlan', error);
+		}
+	};
+	const getAllCoupons = async () => {
+		try {
+			let workspaceId = localStorage.getItem('workspaceId');
+			let usertoken = localStorage.getItem('usertoken');
+			const response = await Service.fetchGet(
+				`/coupon/${workspaceId}/list-coupons`,
+				usertoken,
+				'auth',
+			);
+			if (response?.[0] === true) {
+				dispatch({
+					type: Actions.GET_ALL_COUPONS_SUCCESS,
+					payload: response?.[1],
+				});
+			} else {
+				message.error('Unable to fetch subscription plans');
+				console.log('api failed ==>getAllCoupons', response);
+			}
+		} catch (error) {
+			console.log('errror ==>getAllCoupons', error);
+		}
+	};
+
+	const createStripeCheckoutSession = async (payload) => {
+		try {
+			let workspaceId = localStorage.getItem('workspaceId');
+			let usertoken = localStorage.getItem('usertoken');
+			const response = await Service.fetchPost(
+				`/subscription/${workspaceId}/create-subscription`,
+				payload,
+				usertoken,
+				'auth',
+			);
+			if (response?.[0] === true) {
+				console.log('response==>', response);
+				// dispatch({
+				// 	type: Actions.GET_ALL_COUPONS_SUCCESS,
+				// 	payload: response?.[1],
+				// });
+			} else {
+				message.error('Unable to create  stripe sessions');
+				console.log('api failed ==>createStripeCheckoutSession', response);
+			}
+		} catch (error) {
+			console.log('errror ==>createStripeCheckoutSession', error);
+		}
+	};
+	const resetSubscriptionState = async () => {
+		try {
+			dispatch({ type: Actions.RESET_STATE });
+		} catch (error) {
+			console.log('error==>resetSubscriptionState', error);
+		}
+	};
+
+	return {
+		...state,
+		getAllSubscriptionPlan,
+		resetSubscriptionState,
+		getAllCoupons,
+		createStripeCheckoutSession,
+	};
+};
