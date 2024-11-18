cc7b488971a7c6cc66783602f8a1aa3be90edbab
SheshantManure <sheshantmanure@ve.co>
2024-11-18 12:22:10 +0530

    Wip: Save progress

--- src/views/features/landing_screen/index.jsx
+++ src/views/features/landing_screen/index.jsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../../assets/scss/landingScreen/index.scss';
 import { ReactComponent as VeAiLogo } from '../../../assets/svg/landingScreen/veai-logo.svg';
 import { ReactComponent as VeAiLogoGrey } from '../../../assets/svg/landingScreen/veai-logo-grey.svg';
 import { ReactComponent as ArrowUpBlack } from '../../../assets/svg/landingScreen/arrow-black.svg';
-import { ReactComponent as HeroImage } from './temp.svg';
+import { ReactComponent as DoubleQuote } from '../../../assets/svg/landingScreen/double-quote.svg';
 
 const navItems = ['Privacy', 'Terms', 'Blogs'];
-const currentYear = new Date().getFullYear();
+const currentYear = new Date()?.getFullYear();
 
 const LandingPage = () => {
+	const navigate = useNavigate();
+	const [info, setInfo] = useState({
+		activeToggle: 'Path',
+	});
+
+	const handleToggleClick = (toggleType) => {
+		setInfo({
+			...info,
+			activeToggle: toggleType,
+		});
+	};
+
+	const handleNavigation = () => {
+		navigate('/verify-user');
+	};
+
 	return (
 		<div className="landing-page-container">
 			<header className="header-container">
 				<VeAiLogo aria-label="VeAi Logo" />
-				<button className="login-button" aria-label="Log in to VeAi">
+				<button
+					onClick={handleNavigation}
+					className="login-button"
+					aria-label="Log in to VeAi"
+				>
 					Log in
 				</button>
 			</header>
 			<main className="landing-page-content">
 				<section aria-label="Main content" className="hero-section-1">
-					<span className="veai-logo-container">
+					<div className="veai-logo-container">
 						<span className="veai-logo-text">Meet</span>
 						<VeAiLogoGrey aria-label="VeAi Logo in grey" />
-					</span>
+					</div>
 					<h1 className="heading">AI team that minds your business!</h1>
 					<p className="description">
 						Launch Intelligent, enterprise-ready, and seamlessly embedded in your
@@ -30,14 +51,53 @@ const LandingPage = () => {
 						scaling effortlessly to drive outcomes and push productivity.
 					</p>
 					<div className="cta-container">
-						<button className="get-started-button">
+						<button
+							onClick={handleNavigation}
+							className="get-started-button"
+							aria-label="Get started"
+						>
 							Get Started <ArrowUpBlack aria-label="Arrow up black" />
 						</button>
 						<button className="request-demo-button">Request a Demo</button>
 					</div>
 				</section>
 				<section className="hero-section-2">
-					<HeroImage aria-label="Hero Image" />
+					<div className="toggle-container">
+						<div className="toggle-button">
+							<span
+								className={`toggle-text ${
+									info?.activeToggle === 'Path' ? 'active' : ''
+								}`}
+								onClick={() => handleToggleClick('Path')}
+							>
+								Path
+							</span>
+							<span
+								className={`toggle-text ${
+									info?.activeToggle === 'Story' ? 'active' : ''
+								}`}
+								onClick={() => handleToggleClick('Story')}
+							>
+								Story
+							</span>
+						</div>
+						<div className="content-container">
+							<DoubleQuote aria-label="Double quote" />
+							<p className="content-text">
+								The world is full of dreamers, Yet it's shaped by those who do.
+								<br />
+								<br />
+								we see ourselves as the blacksmiths of the future. whose purpose is
+								To forge simple and intuitive tools that awaken your chi (your vital
+								energy).
+								<br />
+								<br />
+								Those tools are - Our AI agents, whose sole existence is to help you
+								in your pursuit and do a lot of heavy lifting for you! So you can
+								focus on your vision and get there faster!
+							</p>
+						</div>
+					</div>
 				</section>
 			</main>
 			<footer className="footer-container">
@@ -48,7 +108,7 @@ const LandingPage = () => {
 						))}
 					</ul>
 				</nav>
-				<p className="copyright">&copy; {currentYear} VeAi. All rights reserved.</p>
+				<p className="copyright">&copy; {currentYear} VeAI. All rights reserved.</p>
 			</footer>
 		</div>
 	);
