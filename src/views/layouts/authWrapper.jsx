0b5d00bd3a5eb15c54cd866c7161c9b7e98cee54
Mohammed Shahid <mohammedshahid@ve.co>
2024-11-26 14:10:22 +0530

    added a isFullScreen prop to authWrapper

--- src/views/layouts/authWrapper.jsx
+++ src/views/layouts/authWrapper.jsx
@@ -7,7 +7,7 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import Sidebar from '../components/sidebar/Sidebar';
 import useAuth from '../hooks/useAuth';
 
-const AuthWrapper = ({ title, children }) => {
+const AuthWrapper = ({ title, children, isFullScreen = false, maxWidth = '90vw' }) => {
 	const [workspaceId, setActiveWorkspaceId] = useActiveWorkspace();
 
 	const checkAuth = useAuth();
@@ -40,7 +40,12 @@ const AuthWrapper = ({ title, children }) => {
 						style={{ flex: 1, overflowY: 'auto', maxHeight: '100%', height: '100%' }}
 						id="scrollableTarget"
 					>
-						<div className="childrenContainer">{children}</div>
+						<div
+							className="childrenContainer"
+							style={{ maxWidth: isFullScreen ? maxWidth : '1200px' }}
+						>
+							{children}
+						</div>
 					</div>
 				</SkeletonTheme>
 			</div>
