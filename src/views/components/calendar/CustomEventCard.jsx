58fe575edf95bbaf9db9d0e54111bb25a3b2f8f5
Ve Ai <veai@admins-iMac.local>
2024-11-26 17:14:13 +0530

    added border to events, fixed overflow issue of week view events

--- src/views/components/calendar/CustomEventCard.jsx
+++ src/views/components/calendar/CustomEventCard.jsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 import '../../../assets/scss/calendar/customEventCard.scss';
 import { ReactComponent as User } from '../../../assets/svg/calendar/calendarEllipse.svg';
+import moment from 'moment';
 
 const colorsArray = [
 	'#CF824B',
@@ -16,24 +17,34 @@ const colorsArray = [
 ];
 
 const CustomEventCard = ({ event }) => {
+	const startTime = moment(event.start).format('h:mm A');
+	const endTime = moment(event.end).format('h:mm A');
 	return (
 		<div className="customEventCardParentContainer">
 			<div className="cardContainer">
-				<div className="eventHeadWrapper">
-					<span className="eventTitle">{event.title}</span>
-					<span
-						className="statusIndicator"
-						// style={{ backgroundColor: $color }}
-						onClick={(event) => {
-							event.stopPropagation();
-						}}
-					>
-						<span></span>
-					</span>
+				<div className="eventHeader">
+					<div className="eventDetails">
+						<div className="title">{event.title}</div>
+						<div className="subText">
+							{startTime}-{endTime}
+						</div>
+					</div>
+					<div className="eventStatusDiv">
+						<span
+							className="statusIndicator"
+							// style={{ backgroundColor: $color }}
+							onClick={(event) => {
+								event.stopPropagation();
+							}}
+						>
+							<span></span>
+						</span>
+					</div>
 				</div>
-
-				<div className="userSvg">
-					<User />
+				<div className="eventFooter">
+					<div className="userSvg">
+						<User />
+					</div>
 				</div>
 			</div>
 		</div>
