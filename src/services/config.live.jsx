d425b196bb0f12b2fa92bf3bf367a548929bb035
preetam <preetamsingh@ve.co>
2024-12-05 11:23:28 +0530

    CONFLITS: FIX in config file

--- src/services/config.live.jsx
+++ src/services/config.live.jsx
@@ -23,7 +23,7 @@ module.exports = {
 	workflows_Api_US: 'https://us.api.ve.ai/workflows/1.0',
 	ai_assistant_api_US: 'https://api.us-east-1.ve.ai',
 	activity_api_US: 'https://ap.api.ve.ai/workflow-user-analytics/1.0',
-	galleries_api_US: 'https://ap.api.ve.ai/galleries/1.0',
+	galleries_api_US: 'https://us.api.ve.ai/galleries/1.0',
 	ai_predictions_US: 'https://ai.ap-south-1.ve.ai',
 	calendar_chat_api_US: 'https://ai.ap-south-1.ve.ai',
 	calendar_api_US: 'https://ap.api.ve.ai/google/1.0',
