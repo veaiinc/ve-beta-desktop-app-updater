3a8a3a796e72170c2dadbf8ed816ca456c048638
Your Name <shubham.manohar@ve.co>
2024-12-06 21:47:03 +0530

    subscription UI

--- src/views/features/settings/PlanBilling.jsx
+++ src/views/features/settings/PlanBilling.jsx
@@ -6,6 +6,9 @@ import Context from '../../../context/context';
 import SubscriptionDetailsComponent from '../../components/settings/planbilling/SubscriptionDetails';
 import { ReactComponent as BackgroundSvg } from '../../../assets/svg/Settings/subscriptionbackground.svg';
 import BillingHistoryComponent from '../../components/settings/planbilling/BillingHistory';
+import { ReactComponent as Tick } from '../../../assets/svg/tick.svg';
+import { useNavigate } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
 //constants
 const noOfDay = 7;
 const period = 'On Trial Plan';
@@ -110,23 +113,220 @@ const usedGB = 3;
 // 	);
 // };
 
+const features = [
+	'Form Management Assistant',
+	'Proposal Builder',
+	'Sales Performance Tracker',
+	'Workflow Automation',
+	'Business Insights Dashboard',
+];
+const addOns = [
+	{
+		title: 'Credits',
+		price: '$16',
+		totalAvailable: '100',
+		used: '40',
+	},
+	{
+		title: 'Credits',
+		price: '$16',
+		totalAvailable: '100',
+		used: '40',
+	},
+];
+
+const updatePlans = [
+	{
+		title: 'Credits',
+		price: '$16',
+		totalAvailable: '100',
+		used: '40',
+	},
+	{
+		title: 'Credits',
+		price: '$16',
+		totalAvailable: '100',
+		used: '40',
+	},
+	{
+		title: 'Credits',
+		price: '$16',
+		totalAvailable: '100',
+		used: '40',
+	},
+	{
+		title: 'Credits',
+		price: '$16',
+		totalAvailable: '100',
+		used: '40',
+	},
+];
 const PlanBilling = () => {
+	let {
+		subscriptionInfo: {
+			// getAllSubscriptionPlan,
+			// subscriptionPlans,
+			// getAllCoupons,
+			getCurrentSubscriptionPlan,
+			currentPlan,
+		},
+	} = useContext(Context);
+	const [info, setInfo] = useState({
+		loading: true,
+		plan: null,
+		expiresAt: null,
+	});
+
+	useEffect(() => {
+		getCurrentSubscriptionPlan();
+	}, []);
+
+	useEffect(() => {
+		if (currentPlan) {
+			setInfo((prev) => ({
+				...prev,
+				loading: false,
+				plan: currentPlan?.currentSubscriptionPlan,
+				expiresAt: currentPlan?.expiresAt,
+			}));
+		}
+	}, [currentPlan]);
+
 	return (
 		<div className="planBillingContianer">
-			<div className="subscriptionDetailsComponent">
-				<div className="backgroundDesingDiv">
-					<div>
-						<BackgroundSvg />
-					</div>
-				</div>
-				<SubscriptionDetailsComponent />
-			</div>
+			{info?.loading ? (
+				<Skeleton height={'700px'} style={{ borderRadius: '32px' }} />
+			) : info?.plan ? (
+				<SubscribedUserPlanCard />
+			) : (
+				<FreeTierPlanCard />
+			)}
 
-			<div className="settingsBoxContainer billingHinstoryComponent">
+			{/* <div className="settingsBoxContainer billingHinstoryComponent">
 				<BillingHistoryComponent />
-			</div>
+			</div> */}
 		</div>
 	);
 };
 
 export default memo(PlanBilling);
+
+const SubscribedUserPlanCard = () => {
+	return (
+		<div className="subscriptionWrapperContainer">
+			<div className="subscriptionUpdatedPlanCard">
+				<span className="subscriptionPlanHeader">Current Plan</span>
+				<div className="subscriptionPlanContent">
+					<div className="subscriptionPlanPricingDetails">
+						<span className="subscriptionPlanPricing">$35</span>
+						<span className="subscritptionPlanPeriod">/ month</span>
+					</div>
+					<div className="subscriptionUsageContainer">
+						<div className="subscriptionUsageDeatails">
+							<span className="subscriptionUsageData">
+								<span style={{ color: '#fff' }}>150 of</span> 500 used
+							</span>
+						</div>
+						<div className="subscriptionPlanProgressBar">
+							<div className="subscriptionPlanProgressIndicator"></div>
+						</div>
+					</div>
+					<div className="subscritptionFeaturesContainer">
+						{features?.map((ele, index) => (
+							<div className="subscriptionFeature" key={index}>
+								<Tick />
+								<span className="subscriptionFeatureContent">{ele}</span>
+							</div>
+						))}
+					</div>
+					<div className="addOnContianer">
+						<span className="addOnStates">Current Add-on’s</span>
+
+						<div className="addOnStuffsHolder">
+							{addOns?.map((ele, index) => (
+								<div className="addOnCards" key={index}>
+									<div className="addOnCardsHeaderChanges">
+										<span className="addOnCardsTitle">{ele?.title}</span>
+										<span className="addOnCardPriceContainer">
+											<span style={{ color: '#fff' }}>$16 /</span> month
+										</span>
+									</div>
+									<div className="addOnUsageDetails">
+										<div className="subscriptionUsageDeatails">
+											<span className="subscriptionUsageData">
+												150 of 500 used
+											</span>
+										</div>
+										<div className="subscriptionPlanProgressBar">
+											<div className="subscriptionPlanProgressIndicator"></div>
+										</div>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+				<div className="subscriptionSeperator"></div>
+				<div className="subscriptionActionContainer">
+					<div className="manageSubscriptionButton">Manage Subscription</div>
+					<div className="expiringText">Expiring on 25th Dec 2024</div>
+				</div>
+			</div>
+			{/* <div className="updateSubscriptionPlanContainer">
+				<span className="updatePlansTextStyling">Update Plans</span>
+				<div className="updatePlansCardHolder">
+					{updatePlans?.map((ele, index) => (
+						<div className="updatePlansCardContainer">
+							<div className="updatePlansCardContent">
+								<div className="updatePlansHeader">
+									<span className="updatePlansHeaderTextStyling">
+										{ele?.title}
+									</span>
+								</div>
+								<span className="randomUpdatePlans">
+									Launch Intelligent, enterprise-ready, and seamlessly embedded in
+									your operations
+								</span>
+							</div>
+							<div className="addNowButtonForUpdatePlans">Add Now</div>
+						</div>
+					))}
+				</div>
+			</div> */}
+		</div>
+	);
+};
+
+const FreeTierPlanCard = () => {
+	const navigate = useNavigate();
+	return (
+		<div className="freePlanCardContainer">
+			<span className="subscriptionPlanHeader">Free trail</span>
+			<div className="subscriptionPlanContent">
+				<div className="subscriptionPlanPricingDetails">
+					<span className="subscriptionPlanPricing">$0</span>
+				</div>
+
+				<div className="subscritptionFeaturesContainer">
+					{features?.map((ele, index) => (
+						<div className="subscriptionFeature" key={index}>
+							<Tick />
+							<span className="subscriptionFeatureContent">{ele}</span>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="subscriptionSeperator"></div>
+			<div className="freePlanSubscriptionCardContainer">
+				<span className="freeTrialText">
+					Your free trial expires in 5 days! Don’t miss out - upgrade now to keep enjoying
+					premium features.
+				</span>
+
+				<div className="manageSubscriptionButton" onClick={() => navigate('/subscription')}>
+					Upgrade Subscription
+				</div>
+			</div>
+		</div>
+	);
+};
