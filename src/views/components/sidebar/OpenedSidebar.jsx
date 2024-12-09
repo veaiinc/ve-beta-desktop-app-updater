481d389ba6e92a89098f2fddb4d60de696e70534
KomuravelliKarthikeya <komuravellikarthikeya@ve.co>
2024-12-09 16:29:11 +0530

    Update the sidebar for mobile width

--- src/views/components/sidebar/OpenedSidebar.jsx
+++ src/views/components/sidebar/OpenedSidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, memo, useCallback } from 'react';
+import React, { useState, memo, useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { veAiModulesItemsList, bottomOptionsList, veAiSubModulesItemsList } from './sidebarindex';
 import { ReactComponent as DownArrowSmallSvg } from '../../../assets/svg/sidebar/downarrowsmall.svg';
@@ -12,22 +12,8 @@ import useLogout from '../../hooks/useLogout';
 
 const CommonBottomSection = ({ handleLogout, openWorkspacesFunction }) => (
 	<>
-		<div
-			className="currentWorkspaceDiv"
-			onClick={openWorkspacesFunction}
-			style={{ cursor: 'pointer', width: '218px' }}
-		>
-			<div
-				className="detailsDiv"
-				style={{
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					width: '218px',
-					padding: '12px 16px',
-					cursor: 'pointer',
-				}}
-			>
+		<div className="currentWorkspaceDiv" onClick={openWorkspacesFunction}>
+			<div className="detailsDiv">
 				<div style={{ fontSize: '14px', fontWeight: '500', color: 'white' }}>
 					Switch Workspace
 				</div>
@@ -36,18 +22,7 @@ const CommonBottomSection = ({ handleLogout, openWorkspacesFunction }) => (
 				</div>
 			</div>
 		</div>
-		<div
-			className={'singleModuleItem logoutItem'}
-			onClick={handleLogout}
-			style={{
-				cursor: 'pointer',
-				display: 'flex',
-				flexDirection: 'row',
-				width: '218px',
-				justifyContent: 'space-between',
-				padding: '12px 16px',
-			}}
-		>
+		<div className={'singleModuleItem logoutItem'} onClick={handleLogout}>
 			<p style={{ fontSize: '14px', fontWeight: '500', color: '#D73A49' }}>Logout</p>
 			<LogoutRedSvg />
 		</div>
@@ -105,6 +80,7 @@ const OpenedSideBarHoverStateIcons = ({
 						isDropdownVisible && subModules?.length
 							? `${subModules.length * 40}px`
 							: '0',
+					justifyContent: 'space-between',
 				}}
 			>
 				<p>{name}</p>
@@ -118,18 +94,19 @@ const OpenedSideBarHoverStateIcons = ({
 						))}
 					</div>
 				)}
+				{subModules?.length > 0 && (
+					<div onClick={toggleDropdown}>
+						<DownArrowSmallSvg
+							className={`downArrow ${isDropdownVisible ? 'rotate' : ''}`}
+							style={{ height: '16px', width: '16px' }}
+						/>
+					</div>
+				)}
 			</div>
-			{subModules?.length > 0 && (
-				<div onClick={toggleDropdown} style={{ marginTop: '10px' }}>
-					<DownArrowSmallSvg
-						className={`downArrow ${isDropdownVisible ? 'rotate' : ''}`}
-						style={{ height: '16px', width: '16px' }}
-					/>
-				</div>
-			)}
 		</div>
 	);
 };
+
 const OpenedSideBarHoverStateIcons2 = ({
 	name,
 	Icon,
@@ -190,6 +167,16 @@ const OpenedSideBarItemsComponent = ({
 	const [aiChatsDropdownVisible, setAiChatsDropdownVisible] = useState(false);
 	const [selectedChat, setSelectedChat] = useState(null);
 	const [activeChat, setActiveChat] = useState(false);
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 500);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 500);
+		};
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
 	const handleLogout = useCallback(async () => {
 		logoutFunc();
@@ -207,13 +194,20 @@ const OpenedSideBarItemsComponent = ({
 			selectedModule: singleItems?.name,
 		});
 	};
+
 	const handleSidebarCollapse = (e) => {
 		e.stopPropagation();
 		setsidebarStates({ ...sidebarStates, isOpen: false, navStyle: 'close' });
 	};
+
 	const handleChatSelect = (chatName) => {
 		setSelectedChat(chatName);
 		setActiveChat(chatName);
+	};
+
+	const handleCloseChatPanel = () => {
+		setSelectedChat(null);
+		setActiveChat(null);
 	};
 
 	return sidebarStates?.workSpaceOpen ? (
@@ -231,223 +225,226 @@ const OpenedSideBarItemsComponent = ({
 		</>
 	) : (
 		<div style={{ display: 'flex' }}>
-			<div className="openSideBarComponent">
-				<div className="topOptionsList">
-					<div
-						className="veAiLogoDiv"
-						style={{ cursor: 'pointer' }}
-						onClick={() => navigate(`/home`)}
-					>
-						<div className="workspaceDetailsDiv">
-							<img
-								src={info?.activeBusniessName?.logo_s3_500w_key}
-								alt={info?.activeBusniessName?.activeWorkspaceId}
-							/>
-							<h6>{info?.activeBusniessName?.businessName}</h6>
-						</div>
-						<SidebarClosingSvg
-							className="collapseArrow"
-							onClick={handleSidebarCollapse}
-							style={{ cursor: 'pointer' }}
-						/>
-					</div>
-					<div
-						className="allmodulesList"
-						style={{
-							height: '100%',
-							gap: '4px',
-							display: 'flex',
-							flexDirection: 'column',
-						}}
-					>
-						{veAiModulesItemsList?.map((singleItems, index) => (
-							<div>
-								<OpenedSideBarHoverStateIcons
-									name={singleItems.name}
-									Icon={singleItems.icon}
-									initialColor={singleItems.initialColor}
-									route={singleItems?.route}
-									navigateTo={(route) => {
-										handleNavigateFunction(route, singleItems);
-									}}
-									key={index}
-									isSelected={selectedOption === singleItems?.name}
-									isActive={info?.activeRoute === singleItems?.moduleRoute}
-									subModules={singleItems?.subModules}
-									style={{
-										fontSize: '14px',
-										fontStyle: 'normal',
-										fontWeight: '500',
-										fontFamily: 'Inter',
-									}}
-								/>
-							</div>
-						))}
-					</div>
-					<hr style={{ border: '0.7px solid #333334' }} />
-					<div
-						className="AichatDiv"
-						onClick={() => setAiChatsDropdownVisible(!aiChatsDropdownVisible)}
-					>
-						<div style={{ fontSize: '14px', fontWeight: '500', color: '#E8E8E8' }}>
-							Recent Ai chats
-						</div>
-						<DownArrowSmallSvg
-							className={`downArrow ${aiChatsDropdownVisible ? 'rotate' : ''}`}
-							style={{ height: '16px', width: '16px' }}
-						/>
-					</div>
-					{aiChatsDropdownVisible && (
+			{(!isMobile || (isMobile && !selectedChat)) && (
+				<div className="openSideBarComponent">
+					<div className="topOptionsList">
 						<div
-							className="aiChatsSubmodules"
-							style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}
+							className="veAiLogoDiv"
+							style={{ cursor: 'pointer' }}
+							onClick={() => navigate(`/home`)}
 						>
-							{['Chat 1', 'Chat 2', 'Chat 3'].map((chatName) => (
-								<div
-									key={chatName}
-									className={`subModule ${
-										activeChat === chatName ? 'activeListModule' : ''
-									}`}
-									style={{
-										display: 'flex',
-										justifyContent: 'space-between',
-										alignItems: 'center',
-										padding: '12px 16px',
-										cursor: 'pointer',
-										borderRadius: activeChat === chatName ? '100px' : '',
-										background: activeChat === chatName ? '#2E2F33' : '',
-									}}
-									onClick={() => handleChatSelect(chatName)}
-								>
-									<p
+							<div className="workspaceDetailsDiv">
+								<img
+									src={info?.activeBusniessName?.logo_s3_500w_key}
+									alt={info?.activeBusniessName?.activeWorkspaceId}
+								/>
+								<h6>{info?.activeBusniessName?.businessName}</h6>
+							</div>
+							<SidebarClosingSvg
+								className="collapseArrow"
+								onClick={handleSidebarCollapse}
+								style={{ cursor: 'pointer' }}
+							/>
+						</div>
+						<div
+							className="allmodulesList"
+							style={{
+								height: '100%',
+								gap: '4px',
+								display: 'flex',
+								flexDirection: 'column',
+							}}
+						>
+							{veAiModulesItemsList?.map((singleItems, index) => (
+								<div key={index}>
+									<OpenedSideBarHoverStateIcons
+										name={singleItems.name}
+										Icon={singleItems.icon}
+										initialColor={singleItems.initialColor}
+										route={singleItems?.route}
+										navigateTo={(route) => {
+											handleNavigateFunction(route, singleItems);
+										}}
+										isSelected={selectedOption === singleItems?.name}
+										isActive={info?.activeRoute === singleItems?.moduleRoute}
+										subModules={singleItems?.subModules}
 										style={{
 											fontSize: '14px',
+											fontStyle: 'normal',
 											fontWeight: '500',
-											color: '#E8E8E8',
+											fontFamily: 'Inter',
 										}}
-									>
-										{chatName}
-									</p>
+									/>
 								</div>
 							))}
 						</div>
-					)}
-				</div>
-
-				<div className="bottomOptionsList">
-					{false ? (
-						<>
-							<div className="planExpiresDiv">
-								<div className="planExpiresTitle">Plan Expires in 7 days</div>
+						<hr style={{ border: '0.7px solid #333334', margin: '16px 0px' }} />
+						<div
+							className="AichatDiv"
+							onClick={() => setAiChatsDropdownVisible(!aiChatsDropdownVisible)}
+						>
+							<div style={{ fontSize: '14px', fontWeight: '500', color: '#E8E8E8' }}>
+								Recent AI chats
 							</div>
-							<div className="creditsLeft">
-								<div>
-									<div
-										style={{
-											fontSize: '14px',
-											fontWeight: '500',
-											color: '#E8E8E8',
-										}}
-									>
-										100
-									</div>
-									<div
-										style={{
-											fontSize: '14px',
-											fontWeight: '500',
-											color: '#939393',
-										}}
-									>
-										Credits Left This Month
-									</div>
-								</div>
-								<div className="creditSvg">
-									<svg width="30" height="30" viewBox="0 0 30 30">
-										<defs>
-											<linearGradient
-												id="paint0_linear_14532_74799"
-												x1="-0.661765"
-												y1="2.69729e-07"
-												x2="30.4666"
-												y2="1.98941"
-												gradientUnits="userSpaceOnUse"
-											>
-												<stop offset="0.000100017" stop-color="#C39DF8" />
-												<stop offset="1" stop-color="#EC7C9D" />
-											</linearGradient>
-										</defs>
-										<circle
-											cx="15"
-											cy="15"
-											r="12.5"
-											fill="none"
-											stroke="#333334"
-											strokeWidth="5"
-										/>
-										{/* Progress circle */}
-										<circle
-											cx="15"
-											cy="15"
-											r="12.5"
-											fill="none"
-											stroke="url(#paint0_linear_14532_74799)"
-											strokeWidth="5"
-											strokeDasharray={`${(25 / 100) * 78.54} 78.54`}
-											transform="rotate(-90 15 15)"
-										/>
-									</svg>
-								</div>
-							</div>
-						</>
-					) : (
-						<div className="planExpiredDiv">
-							<div className="planExpiredTitle">Your plan has been Expired</div>
-							<div
-								style={{
-									display: 'flex',
-									justifyContent: 'flex-end',
-									alignItems: 'center',
-									alignSelf: 'stretch',
-								}}
-							>
-								<button className="renewNowDiv">Renew Now</button>
-								{/* <RightArrowSvg /> */}
-							</div>
+							<DownArrowSmallSvg
+								className={`downArrow ${aiChatsDropdownVisible ? 'rotate' : ''}`}
+								style={{ height: '16px', width: '16px' }}
+							/>
 						</div>
-					)}
-					{bottomOptionsList?.map((singleItems, index) => (
-						<OpenedSideBarHoverStateIcons2
-							name={singleItems?.name}
-							Icon={singleItems?.icon}
-							route={singleItems?.route}
-							initialColor={singleItems?.initialColor}
-							navigateTo={handleNavigateFunction}
-							key={singleItems?.name}
-							isActive={info?.activeRoute === singleItems?.moduleRoute}
-							style={{
-								fontSize: '14px',
-								padding: '12px 16px',
-							}}
+						{aiChatsDropdownVisible && (
+							<div
+								className="aiChatsSubmodules"
+								style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}
+							>
+								{['Chat 1', 'Chat 2', 'Chat 3'].map((chatName) => (
+									<div
+										key={chatName}
+										className={`subModule ${
+											activeChat === chatName ? 'activeListModule' : ''
+										}`}
+										style={{
+											display: 'flex',
+											justifyContent: 'space-between',
+											alignItems: 'center',
+											padding: '12px 16px',
+											cursor: 'pointer',
+											borderRadius: activeChat === chatName ? '100px' : '',
+											background: activeChat === chatName ? '#2E2F33' : '',
+										}}
+										onClick={() => handleChatSelect(chatName)}
+									>
+										<p
+											style={{
+												fontSize: '14px',
+												fontWeight: '500',
+												color: '#E8E8E8',
+											}}
+										>
+											{chatName}
+										</p>
+									</div>
+								))}
+							</div>
+						)}
+					</div>
+
+					<div className="bottomOptionsList">
+						{true ? (
+							<>
+								<div className="planExpiresDiv">
+									<div className="planExpiresTitle">Plan Expires in 7 days</div>
+								</div>
+								<div className="creditsLeft">
+									<div>
+										<div
+											style={{
+												fontSize: '14px',
+												fontWeight: '500',
+												color: '#E8E8E8',
+											}}
+										>
+											100
+										</div>
+										<div
+											style={{
+												fontSize: '14px',
+												fontWeight: '500',
+												color: '#939393',
+											}}
+										>
+											Credits Left This Month
+										</div>
+									</div>
+									<div className="creditSvg">
+										<svg width="30" height="30" viewBox="0 0 30 30">
+											<defs>
+												<linearGradient
+													id="paint0_linear_14532_74799"
+													x1="-0.661765"
+													y1="2.69729e-07"
+													x2="30.4666"
+													y2="1.98941"
+													gradientUnits="userSpaceOnUse"
+												>
+													<stop
+														offset="0.000100017"
+														stop-color="#C39DF8"
+													/>
+													<stop offset="1" stop-color="#EC7C9D" />
+												</linearGradient>
+											</defs>
+											<circle
+												cx="15"
+												cy="15"
+												r="12.5"
+												fill="none"
+												stroke="#333334"
+												strokeWidth="5"
+											/>
+											<circle
+												cx="15"
+												cy="15"
+												r="12.5"
+												fill="none"
+												stroke="url(#paint0_linear_14532_74799)"
+												strokeWidth="5"
+												strokeDasharray={`${(100 / 100) * 78.54} 78.54`}
+												transform="rotate(-90 15 15)"
+											/>
+										</svg>
+									</div>
+								</div>
+							</>
+						) : (
+							<div className="planExpiredDiv">
+								<div className="planExpiredTitle">Your plan has been Expired</div>
+								<div
+									style={{
+										display: 'flex',
+										justifyContent: 'flex-end',
+										alignItems: 'center',
+										alignSelf: 'stretch',
+									}}
+								>
+									<button className="renewNowDiv">Renew Now</button>
+								</div>
+							</div>
+						)}
+						{bottomOptionsList?.map((singleItems, index) => (
+							<OpenedSideBarHoverStateIcons2
+								name={singleItems?.name}
+								Icon={singleItems?.icon}
+								route={singleItems?.route}
+								initialColor={singleItems?.initialColor}
+								navigateTo={handleNavigateFunction}
+								key={singleItems?.name}
+								isActive={info?.activeRoute === singleItems?.moduleRoute}
+								style={{
+									fontSize: '14px',
+									padding: '12px 16px',
+								}}
+							/>
+						))}
+						<CommonBottomSection
+							handleLogout={handleLogout}
+							openWorkspacesFunction={openWorkspacesFunction}
 						/>
-					))}
-					<CommonBottomSection
-						handleLogout={handleLogout}
-						openWorkspacesFunction={openWorkspacesFunction}
-					/>
+					</div>
 				</div>
-			</div>
+			)}
 			{selectedChat && (
 				<div
 					className="chatDetailPanel"
 					style={{
-						width: '300px',
+						width: isMobile ? '100%' : '300px',
 						backgroundColor: '#1E1E1E',
-						borderLeft: '1px solid #333334',
+						borderLeft: !isMobile ? '1px solid #333334' : 'none',
 						padding: '20px',
-						position: 'absolute',
-						left: '100%',
+						position: isMobile ? 'fixed' : 'absolute',
+						left: isMobile ? '0' : '100%',
 						top: '0',
 						height: '100vh',
+						zIndex: isMobile ? 1000 : 1,
 					}}
 				>
 					<div
@@ -459,13 +456,7 @@ const OpenedSideBarItemsComponent = ({
 						}}
 					>
 						<h2 style={{ color: '#E8E8E8' }}>{selectedChat}</h2>
-						<CrossSvg
-							style={{ cursor: 'pointer' }}
-							onClick={() => {
-								setSelectedChat(null);
-								setActiveChat(null);
-							}}
-						/>
+						<CrossSvg style={{ cursor: 'pointer' }} onClick={handleCloseChatPanel} />
 					</div>
 				</div>
 			)}
