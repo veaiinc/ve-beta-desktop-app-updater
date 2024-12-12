60dc419049fd715f50e7f374706e2508c4fa312a
sheshantmanure <sheshant@ve.ai>
2024-12-12 11:53:39 +0530

    merge development

--- src/context/Templates/state.js
+++ src/context/Templates/state.js
@@ -40,7 +40,7 @@ import { useReducer } from 'react';
 import Reducer from './reducer';
 import { Actions } from './Actions';
 import Service from '../../services/index';
-import { sendCustomMailMutation } from '../subscription/graphqlFunctions';
+import { errorCodes } from '@apollo/client/invariantErrorCodes';
 
 export const intialState = {
 	workflowslist: null,
@@ -63,7 +63,6 @@ export const intialState = {
 	tabItemCount: null,
 	eventsPresetData: null,
 	sendSmartFileSettings: null,
-	aiPredictedData: null,
 };
 
 export const TemplatesState = (props) => {
@@ -1027,12 +1026,37 @@ export const TemplatesState = (props) => {
 		}
 	};
 
-	const sendCustomEmailToClients = async (payload) => {
+	//updated steps functions
+	const addNewSteps = async (payload) => {
 		try {
 			let workspaceId = localStorage.getItem('workspaceId');
 			let usertoken = localStorage.getItem('usertoken');
 			const response = await service.query(
-				sendCustomMailMutation,
+				addNewStepsQuery,
+				payload,
+				workspaceId,
+				usertoken,
+				'workflows_Api',
+			);
+
+			if (response?.[0]) {
+				const dataResponse = response?.[1];
+				return [true, dataResponse?.data?.addStep];
+			} else {
+				console.log('Api failed ==>addNewSteps', response);
+				return [false];
+			}
+		} catch (error) {
+			console.log('error==>addNewSteps', error);
+		}
+	};
+
+	const updateSteps = async (payload) => {
+		try {
+			let workspaceId = localStorage.getItem('workspaceId');
+			let usertoken = localStorage.getItem('usertoken');
+			const response = await service.query(
+				updateStepsQuery,
 				payload,
 				workspaceId,
 				usertoken,
@@ -1042,11 +1066,11 @@ export const TemplatesState = (props) => {
 			if (response?.[0]) {
 				return [true];
 			} else {
-				message.error('Error sending Email');
+				console.log('Api failed ==>updateSteps', response);
 				return [false];
 			}
 		} catch (error) {
-			console.log('errror ==>sendCustomEmailToClients', error);
+			console.log('error==>updateSteps', error);
 		}
 	};
 
@@ -1096,7 +1120,5 @@ export const TemplatesState = (props) => {
 		leaveWorkspace,
 		addNewSteps,
 		updateSteps,
-		leaveWorkspace,
-		sendCustomEmailToClients,
 	};
 };
