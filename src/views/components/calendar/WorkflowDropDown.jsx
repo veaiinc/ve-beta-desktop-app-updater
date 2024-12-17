615bf66eac3f933a3e18a118dc405f4b6e091882
preetam <preetamsingh@ve.co>
2024-12-17 13:53:56 +0530

    CMT: Workflow List DropDown

--- src/views/components/calendar/WorkflowDropDown.jsx
+++ src/views/components/calendar/WorkflowDropDown.jsx
@@ -1,6 +1,5 @@
 import React, { memo, useCallback, useContext, useEffect, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { FetchMoreLoaderComp } from '../../../helpers';
+import { FetchMoreLoaderComp } from '../../../helpers/index';
 import Context from '../../../context/context';
 import { Select } from 'antd';
 
@@ -15,25 +14,21 @@ const WorkflowDropDown = () => {
 		hasNextPage: false,
 		currentPage: 1,
 		workflowOptions: [],
-		selectedWorkflow: null,
+		selectedWorkflowId: null,
 	});
-	console.log('workflowOptions', info?.workflowOptions);
 
 	useEffect(() => {
-		console.log('calling getMyWorkflowTemplatesData API onMount of WorkflowDropDown');
 		getMyWorkflowTemplatesData(1);
 	}, []);
 
 	useEffect(() => {
 		if (myWorkflows) {
-			console.log('calling myWorkflowsDataParser whenn myWorkflows context is updated');
 			myWorkflowsDataParser(myWorkflows);
 		}
 	}, [myWorkflows]);
 
 	useEffect(() => {
 		if (myMoreWorkflows) {
-			console.log('calling myWorkflowsDataParser whenn myMoreWorkflows context is updated');
 			myWorkflowsDataParser(myMoreWorkflows, true);
 		}
 	}, [myMoreWorkflows]);
@@ -60,6 +55,7 @@ const WorkflowDropDown = () => {
 
 	const fetchMoreMyWorkflows = useCallback(() => {
 		console.log('fetchMoreMyWorkflows');
+		setInfo((prev) => ({ ...prev, loading: true }));
 		getMyWorkflowTemplatesData(info?.currentPage + 1, true);
 	}, [info?.hasNextPage, info?.currentPage]);
 
@@ -109,52 +105,52 @@ const WorkflowDropDown = () => {
 		}
 	}, [info?.myWorkflowData]);
 
+	console.log('selectedWorkflowId', JSON.stringify(info?.selectedWorkflowId));
+
 	return (
-		<>
-			<Select
-				allowClear
-				showSearch
-				loading={info?.loading}
-				style={{ width: 150 }}
-				placeholder="Workflows"
-				options={info?.workflowOptions || []}
-				optionFilterProp="label"
-				dropdownRender={(menu) => (
-					<div>
-						<InfiniteScroll
-							dataLength={info?.myWorkflowData?.length || 0}
-							next={fetchMoreMyWorkflows}
-							hasMore={info?.hasNextPage}
-							loader={<FetchMoreLoaderComp />}
-							scrollableTarget={'ant-select-dropdown'}
-							endMessage={
-								<div
-									style={{ textAlign: 'center', padding: '10px', color: '#999' }}
-								>
-									No more workflows
-								</div>
-							}
-							// scrollableTarget={'ant-select-dropdown'}
-							height={250} // Change 2: Match listHeight
-							style={{ overflow: 'auto' }} // Change 3: Enable scrolling
-						>
-							{menu}
-						</InfiniteScroll>
-					</div>
-				)}
-				listHeight={250}
-				dropdownStyle={{
-					overflow: 'auto', // Enable scrolling in dropdown
-				}}
-				getPopupContainer={(trigger) => trigger.parentNode}
-				onChange={(value) => {
-					setInfo((prev) => ({
-						...prev,
-						selectedWorkflow: value,
-					}));
-				}}
-			/>
-		</>
+		<Select
+			allowClear
+			showSearch
+			loading={info?.loading}
+			style={{ width: 120, height: 35 }}
+			placeholder="Workflows"
+			options={info?.workflowOptions || []}
+			optionFilterProp="label"
+			dropdownRender={(menu) => (
+				<div>
+					{menu}
+					{info?.loading && (
+						<FetchMoreLoaderComp
+							wrapperStyle={{
+								fontSize: '10px',
+								fontWeight: '500',
+							}}
+							spinnerWidth="10px"
+							spinnerHeight="10px"
+							spinnerColor="white"
+							gap="8px"
+						/>
+					)}
+				</div>
+			)}
+			listHeight={250}
+			onPopupScroll={(e) => {
+				const target = e.target;
+				const isBottom =
+					Math.abs(target.scrollHeight - target.scrollTop - target.clientHeight) < 1;
+
+				if (!info?.loading && info?.hasNextPage && isBottom) {
+					fetchMoreMyWorkflows();
+				}
+			}}
+			onChange={(workflowId) => {
+				setInfo((prev) => ({
+					...prev,
+					selectedWorkflowId: workflowId,
+				}));
+			}}
+			getPopupContainer={(trigger) => trigger.parentNode}
+		/>
 	);
 };
 
