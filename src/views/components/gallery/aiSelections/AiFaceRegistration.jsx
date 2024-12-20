13c38e3f8bbd29ffe1072393fefaae35ea771d4b
Mohammed Shahid <mohammedshahid@ve.co>
2024-12-20 20:19:45 +0530

    ai images processing notify block

--- src/views/components/gallery/aiSelections/AiFaceRegistration.jsx
+++ src/views/components/gallery/aiSelections/AiFaceRegistration.jsx
@@ -1,21 +1,37 @@
 import React, { useState, useRef, useContext, useEffect, useCallback } from 'react';
 import { ReactComponent as CopyIcon } from '../../../../assets/svg/gallery/copy.svg';
 import { ReactComponent as DownloadIcon } from '../../../../assets/svg/gallery/download2.svg';
+import { ReactComponent as CloseIcon } from '../../../../assets/svg/close.svg';
 import Context from '../../../../context/context';
 import Table from './RegisteredUsersTable';
 import QRCode from 'react-qr-code';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { message } from 'antd';
+import { message, Progress, Tooltip } from 'antd';
+import NotifyPopup from './NotifyPopup';
+import { useParams } from 'react-router-dom';
 
-const AiFaceRegistration = ({ link, galleryId }) => {
+const AiFaceRegistration = ({ link }) => {
 	const qrRef = useRef(null);
 	const [loading, setLoading] = useState(false);
 	const [page, setPage] = useState(1);
+	const [info, setinfo] = useState({
+		isNotifyPopupOpen: false,
+		notifyType: null,
+		numberOfImagesGroupedFaces: 108,
+		numberOfImagesPeoples: 123,
+		imagesCount: 1178,
+	});
 
 	const {
-		galleryInfo: { getImagesReadyNotify, preRegisteredUsers, getPreRegisteredUsers },
+		galleryInfo: {
+			getImagesReadyNotify,
+			preRegisteredUsers,
+			getPreRegisteredUsers,
+			getImageProcessingStatus,
+			imageProcessingStatus,
+		},
 	} = useContext(Context);
-
+	const { galleryId } = useParams();
 	useEffect(() => {
 		if (!preRegisteredUsers) {
 			setLoading(true);
@@ -23,7 +39,15 @@ const AiFaceRegistration = ({ link, galleryId }) => {
 		}
 	}, [preRegisteredUsers]);
 
+	useEffect(() => {
+		if (galleryId) {
+			getImageProcessingStatus(galleryId);
+		}
+	}, []);
+
 	const hasRegisteredUsers = preRegisteredUsers?.data?.length > 0;
+
+	console.log('imageProcessingStatus', imageProcessingStatus);
 
 	const fetchMoreData = () => {
 		const nextPage = page + 1;
@@ -88,6 +112,15 @@ const AiFaceRegistration = ({ link, galleryId }) => {
 		}
 	};
 
+	const openNotifyPopup = (type) => {
+		notifyUser();
+		// setinfo((prev) => ({
+		// 	...prev,
+		// 	isNotifyPopupOpen: true,
+		// 	notifyType: type,
+		// }));
+	};
+
 	return (
 		<div className="aiFaceRegistration">
 			<p className="heading">All data from the client gallery, album and selection views</p>
@@ -110,30 +143,83 @@ const AiFaceRegistration = ({ link, galleryId }) => {
 								<DownloadIcon className="downloadIcon" />
 								<p>Download QR</p>
 							</div>
-							{hasRegisteredUsers && (
+							{/* {hasRegisteredUsers && (
 								<div className="notifyUser" onClick={() => notifyUser()}>
 									<span>Notify User</span>
 								</div>
+							)} */}
+						</div>
+					</div>
+				</div>
+
+				{imageProcessingStatus?.imagesCount !== 0 && (
+					<div className="aiProcessingContainer">
+						{/* <div className="close-button">
+						<CloseIcon />
+					</div> */}
+						<p className="aiProcessingTotalImages">
+							Total Images in Gallery: {imageProcessingStatus?.imagesCount || 0}
+						</p>
+						<div className="aiProcessingDetailsContainer">
+							{imageProcessingStatus?.numberOfImagesPeoples !== 0 && (
+								<div className="progress-bar">
+									<Progress
+										percent={parseInt(
+											(imageProcessingStatus?.numberOfImagesGroupedFaces /
+												imageProcessingStatus?.numberOfImagesPeoples) *
+												100,
+										)}
+										type="circle"
+										size={46}
+										strokeColor="#E8E8E8"
+										strokeWidth={12}
+										trailWidth={12}
+										trailColor="##939393"
+										textStyle={{ color: '#fff' }}
+									/>
+								</div>
+							)}
+							<p className="aiProcessingText">
+								{imageProcessingStatus?.numberOfImagesGroupedFaces !==
+								imageProcessingStatus?.numberOfImagesPeoples
+									? 'AI still Processing your images'
+									: 'AI has processed all your images'}
+							</p>
+
+							{imageProcessingStatus?.numberOfImagesPeoples !== 0 && (
+								<p className="aiProcessingText-count">
+									<Tooltip title="no of processed images">
+										<span style={{ color: '#E8E8E8' }}>
+											{imageProcessingStatus?.numberOfImagesGroupedFaces}{' '}
+										</span>
+									</Tooltip>
+									<Tooltip title="no of images with people">
+										<span style={{ color: '#939393' }}>
+											/{imageProcessingStatus?.numberOfImagesPeoples}
+										</span>
+									</Tooltip>
+								</p>
 							)}
 						</div>
-					</div>
-				</div>
-				<div>
-					<div>
-						<div></div>
-						<div>
-							<p>AI still Processing your images </p>
-							<p>Its Usually take some time</p>
+						<br />
+						<div className="aiProcessingButtonContainer">
+							{preRegisteredUsers?.data?.length > 0 && (
+								<button
+									className="notify-all-button"
+									onClick={() => openNotifyPopup('immediate')}
+								>
+									Notify Immediately{' '}
+								</button>
+							)}
+							{/* <button
+								className="notify-all-button"
+								onClick={() => openNotifyPopup('all')}
+							>
+								Notify all at once{' '}
+							</button> */}
 						</div>
-						<div>
-							<p>300/2900</p>
-						</div>
 					</div>
-					<div>
-						<p>Notify Immediately </p>
-						<p>Notify all at once </p>
-					</div>
-				</div>
+				)}
 			</div>
 			<div className="tableWrapper">
 				<InfiniteScroll
@@ -151,6 +237,8 @@ const AiFaceRegistration = ({ link, galleryId }) => {
 					/>
 				</InfiniteScroll>
 			</div>
+
+			<NotifyPopup info={info} setinfo={setinfo} />
 		</div>
 	);
 };
