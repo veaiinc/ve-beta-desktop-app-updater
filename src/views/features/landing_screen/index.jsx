75260378846d54b6ed1e875a763d889490195491
SheshantManure <sheshantmanure@ve.co>
2024-12-20 20:06:24 +0530

    Landing pg ui save prog

--- src/views/features/landing_screen/index.jsx
+++ src/views/features/landing_screen/index.jsx
@@ -4,21 +4,108 @@ import '../../../assets/scss/landingScreen/index.scss';
 import { ReactComponent as VeAiLogo } from '../../../assets/svg/landingScreen/veai-logo.svg';
 import { ReactComponent as VeAiLogoGrey } from '../../../assets/svg/landingScreen/veai-logo-grey.svg';
 import { ReactComponent as ArrowUpBlack } from '../../../assets/svg/landingScreen/arrow-black.svg';
-// import { ReactComponent as DoubleQuote } from '../../../assets/svg/landingScreen/double-quote.svg';
 import { ReactComponent as DownArrow } from '../../../assets/svg/gallery/arrow-down.svg';
 import { VEAI_URL } from '../../../helpers/ConstantUrls';
-import imageSource from '../../../assets/images/golden-gate-bridge (2) 1.png';
-import imageSource2 from '../../../assets/images/Frame 1618873932.png';
-// import { endsWith } from 'lodash';
+import { ReactComponent as GoldenBridge } from '../../../assets/images/landingPage/golden-gate-bridge.svg';
+import Charminar from '../../../assets/images/Frame 1618873932.png';
 import { CHANGELOG_URL } from '../../../helpers/ConstantUrls';
 import { BLOGS_URL } from '../../../helpers/ConstantUrls';
+import { ReactComponent as CarouselDisplayPic1 } from '../../../assets/svg/landingScreen/carousel-display-pic-1.svg';
+import { ReactComponent as RightArrowGrey } from '../../../assets/svg/landingScreen/right-arrow-grey.svg';
 
 const navItems = [
-	{ name: 'Privacy', route: '/privacy-policy' },
-	{ name: 'Terms', route: '/terms-of-service' },
-	{ name: 'Cookies', route: '/cookie-policy' },
-	{ name: 'Blogs', route: BLOGS_URL },
-	{ name: 'Changelog', route: CHANGELOG_URL },
+	{ id: 1, name: 'Privacy', route: '/privacy-policy' },
+	{ id: 2, name: 'Terms', route: '/terms-of-service' },
+	// { name: 'Cookies', route: '/cookie-policy' },
+	// { name: 'Blogs', route: BLOGS_URL },
+	// { name: 'Changelog', route: CHANGELOG_URL },
+];
+
+const carourselData = [
+	{
+		id: 1,
+		displayPic: <CarouselDisplayPic1 />,
+		title: 'Design Builder',
+		description:
+			'An AI-powered gallery driven by an intelligent AI agent takes the experience to the next level by providing proactive, interactive.',
+	},
+	{
+		id: 2,
+		displayPic: <CarouselDisplayPic1 />,
+		title: 'Gallery',
+		description:
+			'An AI-powered gallery driven by an intelligent AI agent takes the experience to the next level by providing proactive, interactive.',
+	},
+	{
+		id: 3,
+		displayPic: <CarouselDisplayPic1 />,
+		title: 'Calendar',
+		description:
+			'Jarvis Syncs with task management tools to link your to-do list with calendar events, ensuring no task is overlooked.',
+	},
+	{
+		id: 4,
+		displayPic: <CarouselDisplayPic1 />,
+		title: 'Sales',
+		description:
+			'An AI-powered gallery driven by an intelligent AI agent takes the experience to the next level by providing proactive, interactive.',
+	},
+	{
+		id: 5,
+		displayPic: <CarouselDisplayPic1 />,
+		title: 'Tasks',
+		description:
+			'An AI-powered gallery driven by an intelligent AI agent takes the experience to the next level by providing proactive, interactive.',
+	},
+];
+
+const socials = [
+	{
+		id: 1,
+		title: 'X',
+		url: 'https://x.com/veai',
+	},
+	{
+		id: 2,
+		title: 'Linkedin',
+		url: 'https://linkedin.com/veai',
+	},
+	{
+		id: 3,
+		title: 'Instagram',
+		url: 'https://instagram.com/veai',
+	},
+	{
+		id: 4,
+		title: 'YouTube',
+		url: 'https://youtube.com/veai',
+	},
+];
+
+const agents = [
+	{
+		id: 1,
+		agentName: 'Da Vinci',
+	},
+	{
+		id: 1,
+		agentName: 'Ace',
+	},
+	{
+		id: 1,
+		agentName: 'Ari',
+	},
+	{
+		id: 1,
+		agentName: 'Jarvis',
+	},
+];
+
+const resources = [
+	{
+		id: 1,
+		title: 'Blogs',
+	},
 ];
 
 const videoSegments = [
@@ -56,6 +143,7 @@ const LandingPage = () => {
 	const [info, setInfo] = useState({
 		activeToggle: 'Path',
 	});
+	const [email, setEmail] = useState('');
 	useEffect(() => {
 		const usertoken = localStorage.getItem('usertoken');
 		const region = localStorage.getItem('region');
@@ -106,25 +194,13 @@ const LandingPage = () => {
 		const currentTime = videoRef?.current?.currentTime;
 		const currentSegmentData = videoSegments[currentSegment];
 		if (currentTime >= currentSegmentData.endTime) {
-			// Simply move to next segment
 			const nextSegment = (currentSegment + 1) % videoSegments.length;
 			setCurrentSegment(nextSegment);
 			videoRef.current.currentTime = videoSegments[nextSegment].startTime;
 		}
 	};
 
-	const handleSegmentChange = (index) => {
-		setCurrentSegment(index);
-		if (videoRef?.current) {
-			videoRef.current.currentTime = videoSegments[index].startTime;
-		}
-	};
-
-	// const handleVideoClick = () => {
-	// 	setIsUserSelectedSegment(false);
-	// };
-
-	const handleNavigation = () => {
+	const handleNavigationToVerifyUser = () => {
 		navigate('/verify-user');
 	};
 
@@ -132,12 +208,16 @@ const LandingPage = () => {
 		window.open(VEAI_URL, '_blank');
 	};
 
+	const handleSubscribeToNewsletter = () => {
+		// need to update after api is ready...
+	};
+
 	return (
 		<div className="landing-page-container">
 			<header className="header-container">
 				<VeAiLogo aria-label="VeAi Logo" />
 				<button
-					onClick={handleNavigation}
+					onClick={handleNavigationToVerifyUser}
 					className="login-button"
 					aria-label="Log in to VeAi"
 				>
@@ -160,7 +240,7 @@ const LandingPage = () => {
 					</p>
 					<div className="cta-container">
 						<button
-							onClick={handleNavigation}
+							onClick={handleNavigationToVerifyUser}
 							className="get-started-button"
 							aria-label="Get started"
 						>
@@ -195,79 +275,113 @@ const LandingPage = () => {
 						<video
 							ref={videoRef}
 							onTimeUpdate={handleTimeUpdate}
-							// onClick={handleVideoClick}
+							muted
 							autoPlay
-							// loop={isUserSelectedSegment}
-							src={
-								'https://ap.assets.ve.ai/logo/final-LandingVideo_lkhiti.mp4'
-								// 'https://res.cloudinary.com/dir4wguav/video/upload/v1734362768/final-LandingVideo_lkhiti.mp4'
-							}
+							loop
+							playsInline
+							src={'https://ap.assets.ve.ai/logo/final-LandingVideo_lkhiti.mp4'}
 							style={{
-								// height: '70vh',
-								// width: '60vw',
 								height: '100%',
 								width: '100%',
 								cursor: 'pointer',
 								borderRadius: '25.625px !important',
 							}}
 						></video>
-						<div className="video-controls">
-							{/* {videoSegments.map((segment, index) => (
-								<label key={segment.id} className="radio-container">
-									<input
-										type="radio"
-										name="video-selector"
-										checked={currentSegment === index}
-										onChange={() => handleSegmentChange(index)}
-										style={{
-											width: '40px',
-											height: '40px',
-											accentColor: '#B8C5F1',
-										}}
-									/>
-									<span className="radio-custom"></span>
-								</label>
-							))} */}
-						</div>
+						<div className="video-controls"></div>
 					</div>
 				</section>
 			</main>
-			<footer className="footer-container">
-				<div
-					className="footer-image-container"
-					style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}
-				>
-					<img src={imageSource} alt="footer image" />
-					<span className="footer-image-text">Designed in San Francisco</span>
+			<div className="caroursel-container">
+				<div className="description-container">
+					<h1 className="title">
+						Bridge the gap to innovation with digital workforce solutions.
+					</h1>
+					<h2 className="subtitle">
+						Advanced AI-powered digital workers integrate effortlessly into your
+						enterprise, bridging the path to innovation while boosting productivity and
+						operational excellence.
+					</h2>
+					<button onClick={handleNavigationToVerifyUser} className="get-started">
+						Get Started
+					</button>
 				</div>
-				<nav>
-					<ul>
-						{navItems.map((item, i) => (
-							<li
-								key={i}
-								onClick={() => {
-									if (item.name === 'Blogs' || item.name === 'Changelog') {
-										window.open(item.route, '_blank');
-									} else {
-										navigate(item.route);
-									}
-								}}
-							>
-								{item?.name}
+				{/* <div className="carousel">
+					{carourselData?.map((carouselItem) => (
+						<div className="carousel-item">
+							<>{carouselItem?.displayPic}</>
+							<h1>{carouselItem?.title}</h1>
+							<p>{carouselItem?.description}</p>
+						</div>
+					))}
+				</div> */}
+			</div>
+			<footer className="footer-container">
+				<div className="main-content">
+					<div className="left-content">
+						<VeAiLogo />
+						<h2 className="description">
+							Stay ahead with the future of AI! Subscribe to get the latest updates,
+							innovations, and insights delivered straight to your inbox.
+						</h2>
+						<div className="subscribe-to-newsletter">
+							<input
+								onChange={(e) => setEmail(e?.target?.value)}
+								className="email"
+								type="email"
+								placeholder="Email Address"
+							/>
+							<button onClick={handleSubscribeToNewsletter} className="subscribe-btn">
+								<RightArrowGrey />
+							</button>
+						</div>
+					</div>
+					<div className="right-content">
+						<ul className="socials-container">
+							<li className="title">Socials</li>
+							{socials?.map((socialData) => (
+								<li
+									className="list-item"
+									onClick={() => navigate(socialData?.url)}
+									key={socialData?.id}
+								>
+									{socialData?.title}
+								</li>
+							))}
+						</ul>
+						<ul className="agents-container">
+							<li className="title">Agents</li>
+							{agents?.map((agentData) => (
+								<li className="list-item" key={agentData?.id}>
+									{agentData?.agentName}
+								</li>
+							))}
+						</ul>
+						<ul className="resources">
+							<li className="title">Resources</li>
+							{resources?.map((resourceData) => (
+								<li className="list-item" key={resourceData?.id}>
+									{resourceData?.title}
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+				<div className="bottom-content">
+					<div className="left">
+						<GoldenBridge />
+						<span>Designed in San Francisco</span>
+					</div>
+					<ul className="middle">
+						{navItems?.map((navItemData) => (
+							<li onClick={() => navigate(navItemData?.route)} key={navItemData?.id}>
+								{navItemData?.name}
 							</li>
 						))}
 					</ul>
-				</nav>
-				<div
-					className="footer-image-container"
-					style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}
-				>
-					<img
-						src={imageSource2}
-						alt="footer image"
-						style={{ height: '26px', width: '26px' }}
-					/>
-					<span className="footer-image-text">Build in Hyderabad</span>
+					<div className="right">
+						<img width={24} height={24} src={Charminar} alt="Charminar" />
+						<span>Built in Hyderabad</span>
+					</div>
 				</div>
 			</footer>
 		</div>
