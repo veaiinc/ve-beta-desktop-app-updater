cb8708385dfaab68c2356460c00969637308ca6c
Your Name <shubham.manohar@ve.co>
2024-12-21 16:34:52 +0530

    changes

--- src/views/components/ai_agents/CreateCards.jsx
+++ src/views/components/ai_agents/CreateCards.jsx
@@ -1,15 +1,68 @@
 import React, { memo } from 'react';
 import '../../../assets/scss/ai_agents/customCards.scss';
-
+import { ReactComponent as Loader } from '../../../assets/svg/ai_agents/loader.svg';
 const createCardsOptions = [
 	{ title: 'Proposal', subText: 'Create a Proposal ', dotColor: '#EDA145' },
-	{ title: 'Proposal', subText: 'Create a Proposal ', dotColor: '#EDA145' },
-	{ title: 'Proposal', subText: 'Create a Proposal ', dotColor: '#EDA145' },
-	{ title: 'Proposal', subText: 'Create a Proposal ', dotColor: '#EDA145' },
-	{ title: 'Proposal', subText: 'Create a Proposal ', dotColor: '#EDA145' },
-	{ title: 'Proposal', subText: 'Create a Proposal ', dotColor: '#EDA145' },
-	{ title: 'Proposal', subText: 'Create a Proposal ', dotColor: '#EDA145' },
-	{ title: 'Proposal', subText: 'Create a Proposal ', dotColor: '#EDA145' },
+	{ title: 'Pitch Deck', subText: 'Create your Brand Pitch Deck ', dotColor: '#F95A2C' },
+	{ title: 'Workflow', subText: 'Create a Workflow ', dotColor: '#07982F' },
+	{ title: 'Notes', subText: 'Create a Note ', dotColor: '#6055EC' },
+	{ title: 'Schedule', subText: 'Check the Schedule ', dotColor: '#FCD7A5' },
+	{ title: 'New Design', subText: 'Check New Designs for and templates ', dotColor: '#4F8E8D' },
+];
+
+const activityCards = [
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
+	{
+		title: 'Contract Signed',
+		subtext: 'I am testing',
+		time: '1d ago',
+	},
 ];
 
 const CreateCards = () => {
@@ -29,7 +82,7 @@ const CreateCards = () => {
 							></div>
 							<span className="createcardOprtionsTitle">{ele?.title}</span>
 						</div>
-						<span className="createOptionsSubTextStyling"></span>
+						<span className="createOptionsSubTextStyling">{ele?.subText}</span>
 					</div>
 				))}
 			</div>
@@ -38,3 +91,68 @@ const CreateCards = () => {
 };
 
 export default memo(CreateCards);
+
+const Activity = memo(() => {
+	return (
+		<div className="aiAgentsAcitivityContainer">
+			<div className="createCardsHeader">
+				<span className="createCardsHeaderTexct">Activity</span>
+				<Loader />
+			</div>
+
+			<div className="aiAgentsactivityCardsholder">
+				{activityCards?.map((ele, index) => (
+					<div className="aiAgentsActivityCards" key={index}>
+						<span className="aiAgentsActivityCardsHeaderText">{ele?.title}</span>
+						<div className="aiAgentsActivityCardsSubTextHolder">
+							<span className="aiAgentsActivityCardssubTextStyling">
+								{ele?.subtext}
+							</span>
+							<span className="aiAgentsActivityCardTime">{ele?.time}</span>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+});
+
+const Drafts = memo(() => {
+	return (
+		<div className="aiAgentsAcitivityContainer">
+			<div className="createCardsHeader">
+				<span className="createCardsHeaderTexct">Drafts</span>
+			</div>
+
+			<div className="aiAgentsactivityCardsholder">
+				{activityCards?.map((ele, index) => (
+					<div
+						className="aiAgentsActivityCards"
+						key={index}
+						style={{ justifyContent: 'center' }}
+					>
+						{/* <span className="aiAgentsActivityCardsHeaderText">{ele?.title}</span> */}
+						<div className="aiAgentsActivityCardsSubTextHolder">
+							<span
+								className="aiAgentsActivityCardssubTextStyling"
+								style={{
+									fontFamily: 'Inter',
+									fontSize: '12px',
+									fontStyle: 'normal',
+									fontWeight: '500',
+									lineHeight: 'normal',
+									color: '#E8E8E8',
+								}}
+							>
+								{ele?.subtext}
+							</span>
+							<span className="aiAgentsActivityCardTime">{ele?.time}</span>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+});
+
+export { Activity, Drafts };
--- src/views/features/ai_agent/index.js
+++ src/views/features/ai_agent/index.js
@@ -3,7 +3,7 @@ import '../../../assets/scss/ai_agents/home.scss';
 import { ReactComponent as CircledCross } from '../../../assets/svg/ai_agents/cross.svg';
 import { ReactComponent as Search } from '../../../assets/svg/ai_agents/search.svg';
 import AiAgentsCards from '../../components/ai_agents/AiAgentsCards';
-import CreateCards from '../../components/ai_agents/CreateCards';
+import CreateCards, { Activity, Drafts } from '../../components/ai_agents/CreateCards';
 const tabs = ['all', 'guidence', 'approval', 'suggestions'];
 const cards = [
 	{ cardType: 'workflow', title: 'Workflow Name - 1', type: 'guidance' },
@@ -112,7 +112,12 @@ const AiAgent = () => {
 					))}
 				</div>
 			</div>
-			<CreateCards />
+
+			<div className="aiagentsCustomCardHolder">
+				<CreateCards />
+				<Activity />
+				<Drafts />
+			</div>
 		</div>
 	);
 };
