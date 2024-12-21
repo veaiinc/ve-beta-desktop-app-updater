85dcb9acb1ccab9e70670f49f5d52dc108321208
Your Name <shubham.manohar@ve.co>
2024-12-21 16:42:53 +0530

    changes

--- src/routes.js
+++ src/routes.js
@@ -29,6 +29,7 @@ import Tasks from './views/features/tasks';
 import ShareAndEarn from './views/features/ShareAndEarn';
 import Notes from './views/features/Notes';
 import Ai_agent from './views/features/ai_agent';
+import AgentsJobs from './views/features/ai_agent/AgentsJobs';
 const routes = [
 	{
 		path: '/',
@@ -259,15 +260,15 @@ const routes = [
 		),
 		exact: true,
 	},
-	// {
-	// 	path: '/ai-agents/jobs/:agent-name',
-	// 	component: (
-	// 		<AuthWrapper title={'Notes'}>
-	// 			<Ai_agent />
-	// 		</AuthWrapper>
-	// 	),
-	// 	exact: true,
-	// },
+	{
+		path: '/ai-agents/jobs/:agent-name',
+		component: (
+			<AuthWrapper title={'Notes'}>
+				<AgentsJobs />
+			</AuthWrapper>
+		),
+		exact: true,
+	},
 	// {
 	// 	path: '/ai-agents/setup/:agent-name',
 	// 	component: (
