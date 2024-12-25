5bff31fb929f49ec61136e8c8b68df44877ce40f
Your Name <shubham.manohar@ve.co>
2024-12-25 14:31:49 +0530

    calender events sync

--- src/views/components/calendar/CalendarAiChat.jsx
+++ src/views/components/calendar/CalendarAiChat.jsx
@@ -17,7 +17,7 @@ const initialState = {
 
 const CalendarAiChat = ({ toggleAskAi }) => {
 	const {
-		calendarInfo: { calendarChat, getCalendarChat, resetCalendarAiChat },
+		calendarInfo: { calendarChat, getCalendarChat, resetCalendarAiChat, getCalendarEventsList },
 	} = useContext(Context);
 
 	const userTypingRef = useRef(null);
@@ -73,6 +73,9 @@ const CalendarAiChat = ({ toggleAskAi }) => {
 					},
 				],
 			}));
+			if (calendarChat?.answer?.includes('successfully')) {
+				getCalendarEventsList();
+			}
 		}
 	}, [calendarChat]);
 
