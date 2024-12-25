c33e64f1428893ebd51b23e94316c9c7be8102bd
KomuravelliKarthikeya <komuravellikarthikeya@ve.co>
2024-12-25 19:28:02 +0530

    Changed the Gallery Share Link

--- src/views/features/gallery/AddGallery.jsx
+++ src/views/features/gallery/AddGallery.jsx
@@ -184,7 +184,7 @@ const AddGallery = () => {
 	};
 
 	const copyGallerySlugFunction = (slug) => {
-		const galleryLink = `https://${info?.workspaceId}.ve.ai/galleries/${slug}`;
+		const galleryLink = `https://${info?.workspaceId}.ve.ai/gallery/${slug}`;
 		navigator?.clipboard
 			?.writeText(galleryLink)
 			.then(() => {
