515cc8a1430c3a12b14791173fc334dc0e28728d
KomuravelliKarthikeya <komuravellikarthikeya@ve.co>
2024-12-31 12:45:15 +0530

    Changed the Stylings of the WorkflowCard.jsx

--- src/views/components/sales/globalWorkflowCard.jsx
+++ src/views/components/sales/globalWorkflowCard.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as RightArrow } from '../../../assets/svg/workflow/right
 import { fetchOriginSelection } from '../../../helpers';
 let origin = fetchOriginSelection();
 
-const GlobalWorkflowCard = ({ data, onClickFunc }) => {
+const GlobalWorkflowCard = ({ data, onClickFunc, isSelected }) => {
 	const [info, setInfo] = useState({
 		formParsedContentHtml: '',
 		publicModules: [],
@@ -43,48 +43,54 @@ const GlobalWorkflowCard = ({ data, onClickFunc }) => {
 	}, [data]);
 
 	return (
-		<div className="globalWorkflowCardContainer" onClick={() => onClickFunc(data)}>
-			<div className="labelContentWrapper">
-				<span className="globalWorkflowTitle">{data?.title}</span>
-				<span className="globalWorkflowSubTitle">
-					Ideal for wedding photography business with multiple events, selectable packages
-					and services, this workflow provides customisable design in enquiry forms,
-					proposals, invoices for multiple payment schedule and hassle contracts with
-					e-sign contracts
-				</span>
-				<div className="actionContainer">
-					<span className="actionsTitle">Actions</span>
-					<div className="actionBTnContainer">
-						{info?.publicModules?.map((ele, index) => (
-							<div className="actionBtnWrapper" key={index}>
-								<div className="actionBtn">
-									<Circled />
-									<span className="actionBtnTitlestyling">{ele?.module}</span>
+		<div
+			className={`globalWorkflowCardContainer ${isSelected ? 'selected' : ''}`}
+			onClick={() => onClickFunc(data)}
+		>
+			<div className="imageContainer">
+				<div className="imageContainer">
+					<iframe
+						src={`${origin}/preview/${data?._id}?module=${data?.moduleTemplates?.[0]?._id}&isPubic=${data?.moduleTemplates?.[0]?.isPublic}&restrictClick=true`}
+						title="Builder Preview"
+						width="100%"
+						height="100%"
+						style={{ zoom: 0.3 }}
+					/>
+				</div>
+				<div className="labelContentWrapper">
+					<div className="labelContent">
+						<div className="globalWorkflowTitle">{data?.title}</div>
+						<div className="globalWorkflowSubTitle">
+							Ideal for wedding photography business with multiple events, selectable
+							packages and services, this workflow provides customisable design in
+							enquiry forms, proposals, invoices for multiple payment schedule and
+							hassle contracts with e-sign contracts
+						</div>
+					</div>
+					<div className="actionContainer">
+						<span className="actionsTitle">Actions</span>
+						<div className="actionBTnContainer">
+							{info?.publicModules?.map((ele, index) => (
+								<div className="actionBtnWrapper" key={index}>
+									<div className="actionBtn">
+										<Circled />
+										<span className="actionBtnTitlestyling">{ele?.module}</span>
+									</div>
+									{info?.privateModules?.length ? <RightArrow /> : ''}
 								</div>
-								{info?.privateModules?.length ? <RightArrow /> : ''}
-							</div>
-						))}
-						{info?.privateModules?.map((ele, index) => (
-							<div className="actionBtnWrapper" key={index}>
-								<div className="actionBtn">
-									<Circled />
-									<span className="actionBtnTitlestyling">{ele?.module}</span>
+							))}
+							{info?.privateModules?.map((ele, index) => (
+								<div className="actionBtnWrapper" key={index}>
+									<div className="actionBtn">
+										<Circled />
+										<span className="actionBtnTitlestyling">{ele?.module}</span>
+									</div>
+									{index < info?.privateModules?.length - 1 ? <RightArrow /> : ''}
 								</div>
-								{index < info?.privateModules?.length - 1 ? <RightArrow /> : ''}
-							</div>
-						))}
+							))}
+						</div>
 					</div>
 				</div>
-			</div>
-
-			<div className="imageContainer">
-				<iframe
-					src={`${origin}/preview/${data?._id}?module=${data?.moduleTemplates?.[0]?._id}&isPubic=${data?.moduleTemplates?.[0]?.isPublic}&restrictClick=true`}
-					title="Builder Preview"
-					width="100%"
-					height="100%"
-					style={{ zoom: 0.3 }}
-				/>
 			</div>
 		</div>
 	);
