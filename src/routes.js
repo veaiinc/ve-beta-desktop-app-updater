234843e52cb8549bb79ba60153effeebea6f697e
Sheshant Manure <sheshant@ve.ai>
2024-12-31 12:29:36 +0530

    Update: add sidebar to early-access pg

--- src/routes.js
+++ src/routes.js
@@ -149,7 +149,11 @@ const routes = [
 	},
 	{
 		path: '/early-access',
-		component: <EarlyAccess />,
+		component: (
+			<AuthWrapper title={'Early Access'}>
+				<EarlyAccess />
+			</AuthWrapper>
+		),
 		exact: true,
 	},
 	{
