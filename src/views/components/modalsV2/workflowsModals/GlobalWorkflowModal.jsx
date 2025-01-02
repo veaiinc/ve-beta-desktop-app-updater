8b9e30850744afdb3b0b252520dd9468cdf678c6
KomuravelliKarthikeya <komuravellikarthikeya@ve.co>
2025-01-02 16:07:50 +0530

    Added proper animation to the Modal

--- src/views/components/modalsV2/workflowsModals/GlobalWorkflowModal.jsx
+++ src/views/components/modalsV2/workflowsModals/GlobalWorkflowModal.jsx
@@ -331,9 +331,9 @@ const GlobalWorkflowModal = ({
 						className="GlobalWorkflowModalParentContainer"
 						style={{
 							width: isExpanded ? '780px' : '420px',
+							transition: 'width 0.7s ease',
 							position: 'fixed',
 							right: '10px',
-							transition: 'width 0.7s ease',
 						}}
 					>
 						<div
@@ -398,22 +398,16 @@ const GlobalWorkflowModal = ({
 											gap: '24px',
 										}}
 									>
-										{!isExpanded && (
-											<span
-												className="svgContainer"
-												onClick={toggleExpand}
-												style={{
-													display:
-														screenWidth < 500 ? 'none' : 'inline-flex',
-												}}
-											>
-												<ArrowsOut />
-											</span>
-										)}
 										<span
 											className="svgContainer"
-											onClick={isExpanded ? toggleExpand : modifiedCloseModal}
+											onClick={toggleExpand}
+											style={{
+												display: screenWidth < 500 ? 'none' : 'inline-flex',
+											}}
 										>
+											<ArrowsOut />
+										</span>
+										<span className="svgContainer" onClick={modifiedCloseModal}>
 											<DoubleBackArrow />
 										</span>
 									</div>
@@ -465,60 +459,9 @@ const GlobalWorkflowModal = ({
 									loading={info?.loading}
 								/>
 							)}
-							<div
-								className="innerContainerHeader"
-								style={{
-									borderBottom: 'none',
-									marginTop: 'auto',
-									paddingTop: '0px',
-								}}
-							>
-								<div
-									className="headerBtnContainer"
-									style={{ justifyContent: 'center', alignItems: 'center' }}
-								>
-									<div className="tabBtnContainer">
-										<div
-											onClick={onCustomiseFunc}
-											className="svgContainer"
-											style={{
-												display: 'flex',
-												width: '368px',
-												padding: '16px 32px',
-												justifyContent: 'center',
-												alignItems: 'center',
-												gap: '16px',
-												borderRadius: '23px',
-												background: '#FAFAFA',
-												cursor: 'pointer',
-											}}
-										>
-											<span
-												style={{
-													color: '#3F3F3F',
-													fontFamily: 'Inter',
-													fontSize: '12px',
-													fontStyle: 'normal',
-													fontWeight: '500',
-													lineHeight: 'normal',
-												}}
-											>
-												Add to workspace
-											</span>
-											{info?.duplicateApiLoading && (
-												<Spinner
-													width={'16px'}
-													height="16px"
-													color={'#6055ec'}
-													borderTopColor="#111"
-												/>
-											)}
-										</div>
-									</div>
-								</div>
-							</div>
 						</div>
 					</div>
+					;
 				</Drawer>
 			) : (
 				<Drawer
@@ -539,10 +482,10 @@ const GlobalWorkflowModal = ({
 						className="GlobalWorkflowModalParentContainer"
 						style={{
 							height: '99dvh',
-							width: isExpanded ? '780px' : '420px',
 							position: 'fixed',
-							right: '10px',
-							transition: 'width 0.7s ease',
+							right: '60',
+							width: isExpanded ? '780px' : '420px',
+							transition: 'width 0.8s ease',
 						}}
 					>
 						<div
