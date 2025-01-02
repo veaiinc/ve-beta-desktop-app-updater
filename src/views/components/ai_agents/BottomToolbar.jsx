e7c22219f7693fd357ec113634c2c3f972415e3c
Your Name <shubham.manohar@ve.co>
2025-01-02 18:10:35 +0530

    quick actions

--- src/views/components/ai_agents/BottomToolbar.jsx
+++ src/views/components/ai_agents/BottomToolbar.jsx
@@ -6,7 +6,7 @@ import { ReactComponent as Settings } from '../../../assets/svg/ai_agents/settin
 import { ReactComponent as Close } from '../../../assets/svg/close.svg';
 import { ReactComponent as Expand } from '../../../assets/svg/bottomToolbar/expand.svg';
 import ToolBarChatContainerModal from '../modalsV2/ToolBarChatContainerModal';
-import { message } from 'antd';
+import { message, Tooltip } from 'antd';
 import ReactMarkdown from 'react-markdown';
 
 const BottomToolbar = ({ outerContainerStyle = {}, chatList = [], onSend, aiChatLoading }) => {
@@ -187,7 +187,29 @@ const BottomToolbar = ({ outerContainerStyle = {}, chatList = [], onSend, aiChat
 						<Home />
 					</div>
 					<div className="quickActionsButtons">
-						<Plus />
+						<Tooltip
+							placement="top"
+							title={
+								<QuickActionsPlusParentContainer />
+								// <ThreeDotsPopUp
+								// 	data={ele}
+								// 	setSelectedEventsPreset={setSelectedEventsPreset}
+								// 	index={index}
+								// />
+							}
+							color={'#202020'}
+							arrow={true}
+							trigger="click"
+							overlayClassName="quickActionsTooltipContainer"
+							// open={info?.threeDotsPopUp?.[index]}
+							// onOpenChange={(open) => {
+							// 	if (!open) {
+							// 		closeThreeDotsPopup(index);
+							// 	}
+							// }}
+						>
+							<Plus />
+						</Tooltip>
 					</div>
 					<div className="quickActionsButtons">
 						<Settings />
@@ -211,3 +233,7 @@ const BottomToolbar = ({ outerContainerStyle = {}, chatList = [], onSend, aiChat
 };
 
 export default memo(BottomToolbar);
+
+const QuickActionsPlusParentContainer = () => {
+	return <div className="QuickActionsPlusParentContainer"></div>;
+};
