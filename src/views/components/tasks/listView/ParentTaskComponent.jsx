b9ea3f16c41580bd8548436ba0a280b1e0d67a66
sabith <preetam@ve.ai>
2025-01-06 18:59:47 +0530

    Add title for all components

--- src/views/components/tasks/listView/ParentTaskComponent.jsx
+++ src/views/components/tasks/listView/ParentTaskComponent.jsx
@@ -2,13 +2,21 @@ import React, { memo } from 'react';
 import '../../../../assets/scss/tasks/listItems.scss';
 
 import { ReactComponent as ChevronRightThinSvg } from '../../../../assets/svg/tasks/chevronRightThin.svg';
+import { Tooltip } from 'antd';
 
-const ParentTaskComponent = ({ value }) => {
+const ParentTaskComponent = ({ value, showTitle = false }) => {
 	return (
-		<div className="parent-task-component">
-			<ChevronRightThinSvg />
-			<span className="parent-task-title">{value?.title || ''}</span>
-		</div>
+		<Tooltip
+			title={showTitle ? <div className="tooltip-inner">{'Parent task'}</div> : ''}
+			placement="bottom"
+			overlayClassName="tooltip-overlay-container"
+			color="transparent"
+		>
+			<div className="parent-task-component">
+				<ChevronRightThinSvg />
+				<span className="parent-task-title">{value?.title || ''}</span>
+			</div>
+		</Tooltip>
 	);
 };
 
