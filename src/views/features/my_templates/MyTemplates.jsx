3b7056f0cba74894ec30ca1199a5e13354bdfa53
Sheshant Manure <sheshant@ve.ai>
2025-01-11 13:15:05 +0530

    ui: my-templates header

--- src/views/features/my_templates/MyTemplates.jsx
+++ src/views/features/my_templates/MyTemplates.jsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import '../../../assets/scss/my_templates/myTemplates.scss';
 import { ReactComponent as Stars } from '../../../assets/svg/my_templates/stars.svg';
 import { ReactComponent as Plus } from '../../../assets/svg/my_templates/plus.svg';
@@ -81,6 +81,10 @@ const ctaItems = [
 ];
 
 const MyTemplates = () => {
+	const [info, setInfo] = useState({
+		activeNav: 1,
+	});
+
 	return (
 		<div className="myTemplatesContainer">
 			<div className="headerContainer">
@@ -99,7 +103,13 @@ const MyTemplates = () => {
 				<nav className="navContainer">
 					<div className="navItemsContainer">
 						{navItems?.map((navItem) => (
-							<div className="navItem" key={navItem?.id}>
+							<div
+								className={`navItem ${
+									navItem?.id === info?.activeNav ? 'active' : ''
+								}`}
+								key={navItem?.id}
+								onClick={() => setInfo({ activeNav: navItem?.id })}
+							>
 								{navItem?.title}
 							</div>
 						))}
