a337970d9c268b591f17ec41bb4f0cb9f193bc74
Sheshant Manure <sheshant@ve.ai>
2025-01-11 19:10:27 +0530

    update: add title field inside getTemplates payload function

--- src/context/Templates/state.js
+++ src/context/Templates/state.js
@@ -514,7 +514,7 @@ export const TemplatesState = (props) => {
 		}
 	};
 
-	const getTemplatesListForDocs = async (page = 1, limit = 10) => {
+	const getTemplatesListForDocs = async (page = 1, limit = 10, searchValue = '') => {
 		let workspaceId = localStorage.getItem('workspaceId');
 		let usertoken = localStorage.getItem('usertoken');
 
@@ -524,6 +524,7 @@ export const TemplatesState = (props) => {
 				page,
 				type: 'workspace',
 				status: 'published',
+				title: searchValue,
 			},
 		};
 		const response = await service.query(
