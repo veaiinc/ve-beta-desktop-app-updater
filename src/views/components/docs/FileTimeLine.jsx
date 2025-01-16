8e1ed5e3c61283f6a16efda6922808e33d426ae3
preetam <preetamsingh@ve.co>
2025-01-16 20:07:50 +0530

    Fixed timeline in activty tab

--- src/views/components/docs/FileTimeLine.jsx
+++ src/views/components/docs/FileTimeLine.jsx
@@ -1,57 +1,68 @@
 import React, { memo } from 'react';
+import '../../../assets/scss/docs/fileTimeLine.scss';
+import moment from 'moment';
 
 import { ReactComponent as FileArrowUp } from '../../../assets/svg/activity/fileArrowUp.svg';
 import { ReactComponent as EventUser } from '../../../assets/svg/activity/eventUserImg.svg';
 
 const FileTimeLine = ({ data }) => {
+	console.log('dataaaa', data);
 	return (
-		<div className="timelineViewParentContainer">
-			<div className="timelineEventContainer">
-				<div className="timeLineEventBlock">
-					<div className="timelineParentEventBlock">
-						{data?.timeline?.map((item) => (
-							<div className="eventBlock">
-								<div className="eventIcon">
-									<div className="eventIconContainer">
-										<FileArrowUp />
-									</div>
+		<div className="fileTimeLineParentContainer">
+			{data?.timeline?.map((item) => (
+				<>
+					<div className="timelineEventContainer">
+						<div className="eventInfoBlock">
+							<div className="eventIcon">
+								<div className="eventIconContainer">
+									<FileArrowUp />
 								</div>
+							</div>
 
-								<div className="eventDetails">
-									<div className="eventType">{item?.interactionType}</div>
-									<div className="eventUserDetails">
-										<div className="eventUserImg">
-											<EventUser />
-										</div>
-										{item?.name || item?.email ? (
-											<div className="userDetails">
-												<div className="userLabel">
-													<span className="labelKey">Name : </span>
-													<span className="labelValue">
-														{item?.name || ''}
-													</span>
-												</div>
-												<div className="userLabel">
-													<span className="labelKey">Email : </span>
-													<span className="labelValue">
-														{item?.email || ''}
-													</span>
-												</div>
-											</div>
-										) : (
+							<div className="eventDetails">
+								<div className="eventType">{item?.interactionType}</div>
+								<div className="eventUserDetails">
+									<div className="eventUserImg">
+										<EventUser />
+									</div>
+									{item?.name || item?.email ? (
+										<div className="userDetails">
 											<div className="userLabel">
-												<span className="labelKey">Anonymous</span>
+												<span className="labelKey">Name : </span>
+												<span className="labelValue">
+													{item?.name || ''}
+												</span>
 											</div>
-										)}
-									</div>
+											<div className="userLabel">
+												<span className="labelKey">Email : </span>
+												<span className="labelValue">
+													{item?.email || ''}
+												</span>
+											</div>
+										</div>
+									) : (
+										<div className="userLabel">
+											<span className="labelKey">Anonymous</span>
+										</div>
+									)}
 								</div>
+							</div>
 
+							<div className="eventActionButton">
 								<div className="eventActions">View Response</div>
 							</div>
-						))}
+						</div>
+
+						<div className="eventTimeBlock">
+							<div className="eventTime">
+								{item?.createdAt
+									? moment.unix(item?.createdAt).format('ddd, D MMM YYYY hh:mm a')
+									: ''}
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+				</>
+			))}
 		</div>
 	);
 };
