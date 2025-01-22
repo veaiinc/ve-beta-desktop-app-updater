e727189f837acfe7ed1e210bc19885b456bcb000
preetam <preetamsingh@ve.co>
2025-01-22 15:09:29 +0530

    WIP: Form Leads

--- src/views/features/forms/FormLeads.jsx
+++ src/views/features/forms/FormLeads.jsx
@@ -1,9 +1,29 @@
 import React, { memo } from 'react';
 import '../../../assets/scss/forms/formLeads.scss';
 import { ReactComponent as BackArrowSvg } from '../../../assets/svg/workflow/backarrow.svg';
+import { ReactComponent as CurlyBracesSvg } from '../../../assets/svg/docs/curly-bracess.svg';
+import { ReactComponent as LinkSvg } from '../../../assets/svg/activity/link.svg';
+import { ReactComponent as LinkShareSvg } from '../../../assets/svg/docs/link-share.svg';
 import { useNavigate } from 'react-router-dom';
+
 const FormLeads = () => {
 	const navigate = useNavigate();
+
+	const metricsData = [
+		{
+			value: '100',
+			title: 'Total Submissions',
+		},
+		{
+			value: '100',
+			title: 'Completed Entries',
+		},
+		{
+			value: '100',
+			title: 'Partial Entries',
+		},
+	];
+
 	return (
 		<div className="formLeadsParentContainer">
 			<div className="headerContainer">
@@ -11,6 +31,42 @@ const FormLeads = () => {
 					<BackArrowSvg />
 					<span>Back</span>
 				</span>
+			</div>
+
+			<div className="formEnquiryContainer">
+				<header className="headerContainer">
+					<h1 className="headerTitle">Student Application Form</h1>
+				</header>
+
+				<div className="formSummaryContainer">
+					<div className="imgContainer"></div>
+					<div className="detailsContainer">
+						<div className="formMetricsContainer">
+							{metricsData?.map((metric, index) => (
+								<div key={index} className="metricsCard">
+									<span className="value">{metric?.value}</span>
+									<span className="title">{metric?.title}</span>
+								</div>
+							))}
+						</div>
+						<div className="formCTAContainer">
+							<span className="ctaBtn">
+								<LinkSvg />
+								<span>Download</span>
+							</span>
+							<div className="divider"></div>
+							<span className="ctaBtn">
+								<CurlyBracesSvg />
+								<span>Embed Form</span>
+							</span>
+							<div className="divider"></div>
+							<span className="ctaBtn">
+								<LinkShareSvg />
+								<span>Share as Template</span>
+							</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
