c973b17e4e768833453cd9664fdbc0d55ed83823
Your Name <shubham.manohar@ve.co>
2025-01-23 11:01:28 +0530

    commented tasks and docs

--- src/views/components/sidebar/sidebarindex.js
+++ src/views/components/sidebar/sidebarindex.js
@@ -62,8 +62,8 @@ export const veAiModulesItemsList = [
 		// ],
 	},
 
-	{ name: 'Tasks', moduleRoute: '/tasks', route: '/tasks', icon: '' },
-	{ name: 'Docs', moduleRoute: '/docs', route: '/docs', icon: '' },
+	// { name: 'Tasks', moduleRoute: '/tasks', route: '/tasks', icon: '' },
+	// { name: 'Docs', moduleRoute: '/docs', route: '/docs', icon: '' },
 	// {
 	// 	name: 'Da-Vinci',
 	// 	moduleRoute: '/ai-agents/home/da-vinici',
