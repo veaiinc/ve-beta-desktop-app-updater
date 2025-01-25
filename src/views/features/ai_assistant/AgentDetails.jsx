7b281d6e9b50820b7791e95e5b345034b50b6155
preetam <preetamsingh@ve.co>
2025-01-25 11:55:54 +0530

    WIP: AI Assistant Routes SetUP

---
+++ src/views/features/ai_assistant/AgentDetails.jsx
@@ -0,0 +1,7 @@
+import React, { memo } from 'react';
+
+const AgentDetails = () => {
+	return <div style={{ color: 'red' }}>AgentDetails</div>;
+};
+
+export default memo(AgentDetails);
---
+++ src/views/features/ai_assistant/index.jsx
@@ -0,0 +1,7 @@
+import React, { memo } from 'react';
+
+const AiAssistants = () => {
+	return <div style={{ color: 'red' }}>AiAssistants</div>;
+};
+
+export default memo(AiAssistants);
