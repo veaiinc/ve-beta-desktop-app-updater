5b12fa3276636839493923df331a859b4cf69e12
Sheshant Manure <sheshant@ve.ai>
2025-01-27 14:01:14 +0530

    refactor: restore chat box to previous version

--- src/views/components/ai_agents/BottomToolbar.jsx
+++ src/views/components/ai_agents/BottomToolbar.jsx
@@ -1,13 +1,8 @@
 import React, { memo, useCallback, useState, useRef, useEffect, useContext } from 'react';
 import '../../../assets/scss/ai_agents/bottomToolbar.scss';
-// import { ReactComponent as Plus } from '../../../assets/svg/ai_agents/Plus.svg';
-// import { ReactComponent as Home } from '../../../assets/svg/ai_agents/home.svg';
-// import { ReactComponent as Settings } from '../../../assets/svg/ai_agents/settings.svg';
-import { ReactComponent as Filter } from '../../../assets/svg/ai_agents/filter.svg';
-import { ReactComponent as Arroba } from '../../../assets/svg/ai_agents/arroba.svg';
-import { ReactComponent as PaperClip } from '../../../assets/svg/ai_agents/paper-clip.svg';
-import { ReactComponent as Mic } from '../../../assets/svg/ai_agents/mic.svg';
-import { ReactComponent as ExpandChatIcon } from '../../../assets/svg/ai_agents/expand-chat-icon.svg';
+import { ReactComponent as Plus } from '../../../assets/svg/ai_agents/Plus.svg';
+import { ReactComponent as Home } from '../../../assets/svg/ai_agents/home.svg';
+import { ReactComponent as Settings } from '../../../assets/svg/ai_agents/settings.svg';
 import { ReactComponent as Close } from '../../../assets/svg/close.svg';
 import { ReactComponent as Expand } from '../../../assets/svg/bottomToolbar/expand.svg';
 import ToolBarChatContainerModal from '../modalsV2/ToolBarChatContainerModal';
@@ -18,18 +13,10 @@ import { Upload } from 'antd';
 import Context from '../../../context/context';
 import ObjectID from 'bson-objectid';
 import { useLocation } from 'react-router-dom';
-import Spinner from '../loaders/Spinner';
 
 const moduleHelper = {
 	'/tasks': 'tasks',
 };
-
-const chatIcons = [
-	{ icon: <Filter />, type: 'filter' },
-	{ icon: <Arroba />, type: 'arroba' },
-	{ icon: <PaperClip />, type: 'fileUpload' },
-	{ icon: <Mic />, type: 'mic' },
-];
 
 const BottomToolbar = ({
 	outerContainerStyle = {},
@@ -41,21 +28,18 @@ const BottomToolbar = ({
 }) => {
 	const {
 		templates: { handleGlobalChatMessages, globalChatMessages, updateStateValues },
-		aiSetup: { checkFileUploadStatus },
 	} = useContext(Context);
 
 	const location = useLocation();
+
 	const [info, setInfo] = useState({
 		expanded: false,
 		inputExpanded: false,
 		chatModalIsOpen: false,
 		chatQuery: '',
-		position: { x: -325, y: 0 },
+		position: { x: 0, y: 0 },
 		addQuickAction: false,
-		chatSessionId: ObjectID()?.toString(),
-		uploadedFiles: [],
-		uploadedFilesStatus: [],
-		uploadTimeoutIds: [],
+		chatSessionId: ObjectID().toString(),
 	});
 
 	const toolbarRef = useRef(null);
@@ -64,15 +48,15 @@ const BottomToolbar = ({
 	const chatContentRef = useRef(null);
 
 	// Add and remove event listeners
-	// useEffect(() => {
-	// 	document.addEventListener('mousemove', handleMouseMove);
-	// 	document.addEventListener('mouseup', handleMouseUp);
+	useEffect(() => {
+		document.addEventListener('mousemove', handleMouseMove);
+		document.addEventListener('mouseup', handleMouseUp);
 
-	// 	return () => {
-	// 		document.removeEventListener('mousemove', handleMouseMove);
-	// 		document.removeEventListener('mouseup', handleMouseUp);
-	// 	};
-	// }, []);
+		return () => {
+			document.removeEventListener('mousemove', handleMouseMove);
+			document.removeEventListener('mouseup', handleMouseUp);
+		};
+	}, []);
 	// Add this useEffect for auto-scrolling
 	useEffect(() => {
 		if (chatContentRef.current) {
@@ -80,93 +64,43 @@ const BottomToolbar = ({
 		}
 	}, [chatList]); // Scroll whenever chatList changes
 
-	useEffect(() => {
-		info?.uploadedFiles?.forEach((uploadedFile) => {
-			getFileUploadStatus(uploadedFile);
-		});
-	}, [info?.uploadedFiles]);
-
-	const getFileUploadStatus = async (uploadedFile) => {
-		const response = await checkFileUploadStatus(
-			uploadedFile?.uploadBatchId,
-			uploadedFile?._id,
-		);
-		if (response?.[0] === true) {
-			const fileUploadStatus = {
-				fileId: uploadedFile?._id,
-				status:
-					response?.[1]?.successCount === 1
-						? 'success'
-						: response?.[1]?.errorCount === 1
-						? 'failed'
-						: 'pending',
-			};
-
-			setInfo((prev) => {
-				const existingIndex = prev?.uploadedFilesStatus.findIndex(
-					(file) => file.fileId === fileUploadStatus.fileId,
-				);
-
-				let updatedFilesStatus = [...prev.uploadedFilesStatus];
-
-				if (existingIndex !== -1) {
-					// Update the existing object
-					updatedFilesStatus[existingIndex] = fileUploadStatus;
-				} else {
-					// Append the new object
-					updatedFilesStatus = [...updatedFilesStatus, fileUploadStatus];
-				}
-
-				return {
-					...prev,
-					uploadedFilesStatus: updatedFilesStatus,
-				};
-			});
-
-			if (fileUploadStatus?.status === 'pending') {
-				const timeoutId = setTimeout(() => {
-					getFileUploadStatus(uploadedFile);
-				}, 1000);
-				setInfo((prev) => ({
-					...prev,
-					uploadTimeoutIds: [...prev?.uploadTimeoutIds, timeoutId],
-				}));
-			} else {
-				info?.uploadTimeoutIds?.forEach((timeoutId) => clearTimeout(timeoutId));
-			}
-		}
-	};
-
 	const handleMouseDown = useCallback(
 		(e) => {
-			if (e.target?.closest('.quickActionsButtons, input, button')) return;
+			if (e.target.closest('.quickActionsButtons, input, button')) return;
 
 			isDraggingRef.current = true;
 			startPosRef.current = {
-				x: e.clientX - info?.position?.x,
-				y: e.clientY - info?.position?.y,
+				x: e.clientX - info.position.x,
+				y: e.clientY - info.position.y,
 			};
 		},
-		[info?.position],
+		[info.position],
 	);
 
-	// const handleMouseMove = useCallback((e) => {
-	// 	if (!isDraggingRef?.current) return;
+	const handleMouseMove = useCallback((e) => {
+		if (!isDraggingRef.current) return;
 
-	// 	const newX = e.clientX - startPosRef?.current?.x;
-	// 	const newY = e.clientY - startPosRef?.current?.y;
+		const newX = e.clientX - startPosRef.current.x;
+		const newY = e.clientY - startPosRef.current.y;
 
-	// 	setInfo((prev) => ({
-	// 		...prev,
-	// 		position: { x: newX, y: newY },
-	// 	}));
-	// }, []);
+		setInfo((prev) => ({
+			...prev,
+			position: { x: newX, y: newY },
+		}));
+	}, []);
 
 	const handleMouseUp = useCallback(() => {
 		isDraggingRef.current = false;
 	}, []);
 
 	//function definitions
+	const handleInputFocus = useCallback(() => {
+		setInfo((prev) => ({
+			...prev,
+			expanded: true,
+			inputExpanded: true,
+		}));
+	}, [info]);
 
 	const handleClose = useCallback(() => {
 		setInfo((prev) => ({
@@ -188,29 +122,12 @@ const BottomToolbar = ({
 		setInfo((prev) => ({ ...prev, chatModalIsOpen: false }));
 	}, [info]);
 
-	const handleInputFocus = useCallback(() => {
-		if (globalChatMessages?.length > 1 || chatList?.length > 0) {
-			setInfo((prev) => ({
-				...prev,
-				expanded: true,
-				inputExpanded: true,
-			}));
-		}
-	}, [globalChatMessages, chatList]);
 	const handleSendMessageFunc = useCallback(
 		(e) => {
 			if (e.key === 'Enter') {
 				// If Shift+Enter, allow new line
 				if (e.shiftKey) {
 					return;
-				}
-
-				if (info?.expanded === false) {
-					setInfo((prev) => ({
-						...prev,
-						expanded: true,
-						inputExpanded: true,
-					}));
 				}
 
 				// Prevent default to avoid unwanted new line
@@ -220,7 +137,7 @@ const BottomToolbar = ({
 					return message.error('Please wait for the AI response');
 				}
 
-				if (info?.chatQuery?.trim()?.length) {
+				if (info?.chatQuery?.trim().length) {
 					if (customChatActions) {
 						onSend(info?.chatQuery);
 					} else {
@@ -243,32 +160,17 @@ const BottomToolbar = ({
 	);
 
 	const handleChange = useCallback(
-		async ({ file }) => {
-			const { _id, uploadBatchId, sessionId } = await handleAiUploadImage(file);
+		({ file }) => {
+			handleAiUploadImage(file);
 			setInfo((prev) => ({
 				...prev,
 				addQuickAction: false,
 				expanded: true,
 				inputExpanded: true,
-				uploadedFiles: [
-					...prev?.uploadedFiles,
-					{ _id, uploadBatchId, sessionId, file, fileType: file?.type },
-				],
 			}));
 		},
 		[handleAiUploadImage],
 	);
-
-	const handleChatIconClick = (e, type) => {
-		if (type === 'fileUpload') {
-			const files = e?.target?.files ?? null;
-			if (files) {
-				Array?.from(files)?.forEach((file) => {
-					handleChange({ file });
-				});
-			}
-		}
-	};
 
 	return (
 		<div
@@ -277,8 +179,8 @@ const BottomToolbar = ({
 			style={{
 				...outerContainerStyle,
 				position: 'fixed',
-				transform: `translate(${info?.position?.x}px, ${info?.position?.y}px)`,
-				cursor: isDraggingRef?.current ? 'grabbing' : 'grab',
+				transform: `translate(${info.position.x}px, ${info.position.y}px)`,
+				cursor: isDraggingRef.current ? 'grabbing' : 'grab',
 			}}
 			onMouseDown={handleMouseDown}
 		>
@@ -288,12 +190,10 @@ const BottomToolbar = ({
 				} bottomToolbarChatContainer`}
 			>
 				<div className="bottomToolBarChatHeader">
+					<span>AI Assistant</span>
 					<div style={{ display: 'flex', alignItems: 'center' }}>
 						<button className="closeButton" onClick={handleChatExpand}>
 							<Expand />
-						</button>
-						<button className="closeButton">
-							<ExpandChatIcon />
 						</button>
 						<button className="closeButton" onClick={handleClose}>
 							<Close />
@@ -304,7 +204,7 @@ const BottomToolbar = ({
 					{(!customChatActions ? globalChatMessages : chatList).map((chat, index) =>
 						chat?.content ? (
 							<div
-								className={`chat-message ${chat?.type?.toLowerCase()}-message`}
+								className={`chat-message ${chat.type.toLowerCase()}-message`}
 								key={index}
 							>
 								{chat?.content}
@@ -312,10 +212,10 @@ const BottomToolbar = ({
 						) : (
 							<div
 								key={index}
-								className={`chat-message ${chat?.type?.toLowerCase()}-message`}
+								className={`chat-message ${chat.type.toLowerCase()}-message`}
 							>
 								<div className="message-content">
-									<ReactMarkdown>{chat?.message}</ReactMarkdown>
+									<ReactMarkdown>{chat.message}</ReactMarkdown>
 								</div>
 							</div>
 						),
@@ -325,80 +225,19 @@ const BottomToolbar = ({
 
 			{/* bottom toolBarContent */}
 			{!info?.chatModalIsOpen ? (
-				<div className={`bottomToolbar ${info?.expanded ? 'update-border-radius' : ''}`}>
-					{info?.uploadedFiles?.length > 0 && (
-						<div className="uploaded-files-container">
-							{info?.uploadedFiles?.map((uploadedFile) => {
-								const fileStatus = info?.uploadedFilesStatus?.find(
-									(status) => status?.fileId === uploadedFile?._id,
-								)?.status;
-
-								return (
-									<div key={uploadedFile?._id} className="uploaded-file-item">
-										<p className="uploaded-file-name">
-											{uploadedFile?.file?.name}
-										</p>
-										{uploadedFile?.fileType?.includes('image') && (
-											<img
-												src={URL?.createObjectURL(uploadedFile?.file)}
-												alt={uploadedFile?.file?.name}
-												className="uploaded-file-preview"
-												onLoad={(e) => URL?.revokeObjectURL(e?.target?.src)} // Clean up object URL after loading
-											/>
-										)}
-										{console.log(uploadedFile?.fileType)}
-										{uploadedFile?.fileType?.includes('pdf') && (
-											<iframe
-												src={URL?.createObjectURL(uploadedFile?.file)}
-												className="uploaded-file-preview"
-												onLoad={(e) => URL?.revokeObjectURL(e?.target?.src)} // Clean up object URL after loading
-											/>
-										)}
-										{fileStatus === 'pending' && (
-											<span className="uploaded-file-status-loader">
-												<Spinner width={'16px'} height={'16px'} />
-											</span>
-										)}
-									</div>
-								);
-							})}
-						</div>
-					)}
+				<div className="bottomToolbar">
 					<textarea
-						className={`bottomToolbarInputs ${info?.inputExpanded ? 'expanded' : ''}`}
-						placeholder="Hey! Need help? Ask me anything."
+						className={`bottomToolbarInputs ${info.inputExpanded ? 'expanded' : ''}`}
+						placeholder="Ask AI"
+						onFocus={handleInputFocus}
 						value={info?.chatQuery}
 						onChange={(e) =>
-							setInfo((prev) => ({ ...prev, chatQuery: e?.target?.value }))
+							setInfo((prev) => ({ ...prev, chatQuery: e.target.value }))
 						}
-						onFocus={handleInputFocus}
 						onKeyDown={handleSendMessageFunc}
+						style={{ resize: 'none' }}
 					/>
-					<div className="chat-icons-container">
-						{chatIcons?.map((chatIcon, idx) => (
-							<span
-								className="icon-container"
-								{...(chatIcon?.type !== 'fileUpload' && {
-									onClick: (e) => handleChatIconClick(e, chatIcon?.type),
-								})}
-								key={idx}
-							>
-								{chatIcon?.type === 'fileUpload' && (
-									<span className="file-upload-container">
-										<input
-											type="file"
-											accept=".pdf,.docx,.md,.txt,.jpg,.jpeg,.png,.json"
-											className="file-upload"
-											onChange={(e) => handleChatIconClick(e, chatIcon?.type)}
-											multiple
-										/>
-									</span>
-								)}
-								{chatIcon?.icon}
-							</span>
-						))}
-					</div>
-					{/* <div className="quickActionsButtons">
+					<div className="quickActionsButtons">
 						<Home />
 					</div>
 					<div className="quickActionsButtons">
@@ -421,7 +260,7 @@ const BottomToolbar = ({
 					</div>
 					<div className="quickActionsButtons">
 						<Settings />
-					</div> */}
+					</div>
 				</div>
 			) : (
 				''
--- src/views/layouts/authWrapper.jsx
+++ src/views/layouts/authWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, memo, useContext, useState } from 'react';
+import React, { useEffect, memo } from 'react';
 import '../../assets/scss/authWrapper.scss';
 import { Helmet } from 'react-helmet';
 import useActiveWorkspace from '../hooks/useActiveWorkspace';
@@ -9,41 +9,17 @@ import useAuth from '../hooks/useAuth';
 import useSubscription from '../hooks/useSubscription';
 import useTokenExpiry from '../hooks/useTokenExpiry';
 import BottomToolbar from '../components/ai_agents/BottomToolbar';
-import { message } from 'antd';
-import Context from '../../context/context';
 
 const AuthWrapper = ({ title, children, maxWidth = '', showBottomToolbar = true }) => {
 	const [workspaceId, setActiveWorkspaceId] = useActiveWorkspace();
-	const [uploadedFiles, setUploadedFiles] = useState([]);
 
 	const checkAuth = useAuth();
 	// const data = useSubscription();
 	const tokenData = useTokenExpiry();
-	const {
-		aiSetup: { uploadImageToKnowledgeBase },
-	} = useContext(Context);
 
 	useEffect(() => {
 		checkAuth();
 	}, []);
-
-	const handleAiUploadImage = async (uploadedImage) => {
-		try {
-			const response = await uploadImageToKnowledgeBase(uploadedImage);
-			if (response?.[0]) {
-				const { _id, uploadBatchId, sessionId } = response?.[1];
-				setUploadedFiles([
-					...uploadedFiles,
-					{ _id, uploadBatchId, sessionId, filename: uploadedImage?.name },
-				]);
-				return { _id, uploadBatchId, sessionId };
-			} else {
-				message.error('An error occurred while uploading the image.');
-			}
-		} catch (error) {
-			console.log('Something went wrong! ', error);
-		}
-	};
 
 	return (
 		<div className="authParentContainer">
@@ -81,7 +57,7 @@ const AuthWrapper = ({ title, children, maxWidth = '', showBottomToolbar = true 
 					// chatList={info?.chatList}
 					// onSend={handleSendMessage}
 					// aiChatLoading={info?.aiChatLoading}
-					handleAiUploadImage={handleAiUploadImage}
+					// handleAiUploadImage={handleAiUploadImage}
 				/>
 			) : (
 				''
