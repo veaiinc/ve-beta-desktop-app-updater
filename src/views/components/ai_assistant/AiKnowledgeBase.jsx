08bbaaea167c22b29825e5c503809d04ee957b9f
preetam <preetamsingh@ve.co>
2025-01-27 16:14:18 +0530

    WIP: DeleteAgentModal

--- src/views/components/ai_assistant/AiKnowledgeBase.jsx
+++ src/views/components/ai_assistant/AiKnowledgeBase.jsx
@@ -1,14 +1,21 @@
 import React, { memo, useState } from 'react';
 import '../../../assets/scss/ai_assistant/knowledgeBase.scss';
 import { ReactComponent as Link } from '../../../assets/svg/smartFiles/formResponse/link.svg';
+import AddKnowledgeModal from '../../components/modalsV2/settings/ai_setup/AddKnowledgeModal';
 
 const AiKnowledgeBase = () => {
-	const [toggleStates, setToggleStates] = useState({});
+	const [info, setInfo] = useState({
+		toggleStates: {},
+		knowledgeModalOpen: false,
+	});
 
 	const handleToggleChange = (toggleId) => {
-		setToggleStates((prevStates) => ({
-			...prevStates,
-			[toggleId]: !prevStates[toggleId],
+		setInfo((prev) => ({
+			...prev,
+			toggleStates: {
+				...prev.toggleStates,
+				[toggleId]: !prev.toggleStates[toggleId],
+			},
 		}));
 	};
 
@@ -22,46 +29,60 @@ const AiKnowledgeBase = () => {
 	];
 
 	return (
-		<div className="aiKnowledgeBaseParentContainer">
-			<div className="knowledgeBaseHeaderContainer">
-				<div className="knowledgeBaseHeader">
-					<span className="lineone">Knowledge Base</span>
-					<span className="linetwo">
-						Make files available to this AI assistant so it can use them as a source of
-						knowledge for chats.
-					</span>
-				</div>
-
-				<div className="addKnowledge">Add a Knowledge</div>
-			</div>
-
-			<div className="knowledgeBaseListContainer">
-				<div className="header">
-					<span>Title</span>
-					<span>Last edit</span>
-					<span>Active</span>
-				</div>
-				{knowledgeBaseItems?.map((item) => (
-					<div key={item?.id} className="knowledgeBaseItem">
-						<span>
-							<Link />
-							{item?.title}
-						</span>
-						<span style={{ color: '#7C7C84' }}>{item?.lastEdit}</span>
-						<span className="toggleSwitch">
-							<input
-								type="checkbox"
-								id={item?.id}
-								className="toggle"
-								checked={toggleStates[item?.id] || false}
-								onChange={() => handleToggleChange(item.id)}
-							/>
-							<label htmlFor={item?.id}></label>
+		<>
+			<div className="aiKnowledgeBaseParentContainer">
+				<div className="knowledgeBaseHeaderContainer">
+					<div className="knowledgeBaseHeader">
+						<span className="lineone">Knowledge Base</span>
+						<span className="linetwo">
+							Make files available to this AI assistant so it can use them as a source
+							of knowledge for chats.
 						</span>
 					</div>
-				))}
+
+					<div
+						className="addKnowledge"
+						onClick={() => setInfo((prev) => ({ ...prev, knowledgeModalOpen: true }))}
+					>
+						Add a Knowledge
+					</div>
+				</div>
+
+				<div className="knowledgeBaseListContainer">
+					<div className="header">
+						<span>Title</span>
+						<span>Last edit</span>
+						<span>Active</span>
+					</div>
+					{knowledgeBaseItems?.map((item) => (
+						<div key={item?.id} className="knowledgeBaseItem">
+							<span>
+								<Link />
+								{item?.title}
+							</span>
+							<span style={{ color: '#7C7C84' }}>{item?.lastEdit}</span>
+							<span className="toggleSwitch">
+								<input
+									type="checkbox"
+									id={item?.id}
+									className="toggle"
+									checked={info?.toggleStates[item?.id] || false}
+									onChange={() => handleToggleChange(item.id)}
+								/>
+								<label htmlFor={item?.id}></label>
+							</span>
+						</div>
+					))}
+				</div>
 			</div>
-		</div>
+			<AddKnowledgeModal
+				isOpen={info?.knowledgeModalOpen}
+				toggleModal={() => {
+					setInfo((prev) => ({ ...prev, knowledgeModalOpen: false }));
+				}}
+				onActionClick={() => {}}
+			/>
+		</>
 	);
 };
 
--- src/views/features/ai_assistant/CreateAgent.jsx
+++ src/views/features/ai_assistant/CreateAgent.jsx
@@ -1,9 +1,12 @@
 import React, { memo, useCallback, useState } from 'react';
 import '../../../assets/scss/ai_assistant/CreateAgent.scss';
-import CreateAgentHeader from '../../components/ai_assistant/CreateAgentHeader';
+import '../../../assets/scss/ai_assistant/createAgentHeader.scss';
+// import CreateAgentHeader from '../../components/ai_assistant/CreateAgentHeader';
 import TabHeader from '../../components/ai_assistant/TabHeader';
 import { useNavigate } from 'react-router-dom';
 import { ReactComponent as AgentIcon } from '../../../assets/svg/ai_assistant/agent.svg';
+import { ReactComponent as BackSvg } from '../../../assets/svg/sidebar/leftarrowwhite.svg';
+import { ReactComponent as Delete } from '../../../assets/svg/ai_assistant/delete.svg';
 import AiPersonality from '../../components/ai_assistant/AiPersonality';
 import AiInstructions from '../../components/ai_assistant/AiInstructions';
 import AiActions from '../../components/ai_assistant/AiActions';
@@ -11,12 +14,15 @@ import AiKnowledgeBase from '../../components/ai_assistant/AiKnowledgeBase';
 import AiPrompt from '../../components/ai_assistant/AiPrompt';
 import AiShare from '../../components/ai_assistant/AiShare';
 import AiLinkFile from '../../components/ai_assistant/AiLinkFile';
+import DeleteAgentModal from '../../components/modalsV2/ai_assistant/DeleteAgentModal';
 
-import AddKnowledgeModal from '../../components/modalsV2/settings/ai_setup/AddKnowledgeModal';
 const CreateAgent = () => {
 	const navigate = useNavigate();
 	const [info, setInfo] = useState({
 		activeTab: 'personality', // personality, instructions, actions, knowledgeBase, prompt, share, linkeafile
+		selectedAgent: null,
+		publishAgent: false,
+		deleteAgentModal: false,
 	});
 
 	const tabs = {
@@ -47,27 +53,81 @@ const CreateAgent = () => {
 		console.log('Action Clicked');
 	}, []);
 
+	const onDeleteClick = useCallback(() => {
+		setInfo((prev) => ({ ...prev, deleteAgentModal: true }));
+	}, []);
+	const handleDeleteAgent = useCallback(() => {
+		console.log('Delete Agent Clicked');
+	}, []);
+
 	return (
-		<div className="create-agent">
-			<div style={{ flexShrink: 0 }}>
-				<CreateAgentHeader
-					name="Assistant#2"
-					onBack={onBack}
-					onActionClick={onActionClick}
-					status="Saved"
-					backText="Back to AI Assistants"
-					actionText="Publish"
-					agentIcon={<AgentIcon width={18} height={18} />}
-				/>
-				<TabHeader
-					activeTab={info?.activeTab}
-					onTabChange={onTabChange}
-					tabs={Object?.values(tabs)}
-				/>
+		<>
+			<div className="create-agent">
+				<div style={{ flexShrink: 0 }}>
+					<div className="create-agent-header">
+						<div className="create-agent-header-left">
+							<div className="create-agent-header-left-back" onClick={onBack}>
+								<div className="create-agent-header-left-back-icon">
+									<BackSvg />
+								</div>
+								<div className="create-agent-header-left-back-text">
+									Back to AI Assistants
+								</div>
+							</div>
+							<div className="create-agent-header-left-agent-name">
+								<AgentIcon width={18} height={18} />
+								{info?.agentName || 'Assistant'}
+							</div>
+						</div>
+
+						<div className="create-agent-header-right">
+							{info?.selectedAgent ? (
+								<>
+									<span className="create-agent-header-right-status">
+										<div className="create-agent-header-right-status-icon" />
+										{info?.selectedAgent?.status}
+									</span>
+									<button
+										className={`create-agent-header-right-button`}
+										onClick={onActionClick}
+									>
+										Publish
+									</button>
+								</>
+							) : (
+								<div
+									className={`create-agent-header-delete-button`}
+									onClick={onDeleteClick}
+								>
+									<Delete width={18} height={18} />
+									Delete
+								</div>
+							)}
+						</div>
+					</div>
+
+					<TabHeader
+						activeTab={info?.activeTab}
+						onTabChange={onTabChange}
+						tabs={Object?.values(tabs)}
+					/>
+				</div>
+
+				<div className="tabSection">{tabs[info?.activeTab]?.component}</div>
 			</div>
-			<div className="tabSection">{tabs[info?.activeTab]?.component}</div>
-			<AddKnowledgeModal isOpen={true} onClose={() => {}} onActionClick={() => {}} />
-		</div>
+			<DeleteAgentModal
+				open={info?.deleteAgentModal}
+				closeModal={() => setInfo((prev) => ({ ...prev, deleteAgentModal: false }))}
+				deleteChatBot={() => {
+					console.log('Delete Chat Bot Clicked');
+					navigate(-1);
+				}}
+				deleteConversations={() => {
+					console.log('Delete Conversations Clicked');
+					navigate(-1);
+				}}
+			/>
+		</>
 	);
 };
 
