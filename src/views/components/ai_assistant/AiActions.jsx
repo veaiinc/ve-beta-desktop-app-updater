f3557616360c51dbc7e298eb32af9106807dc16e
preetam <preetamsingh@ve.co>
2025-01-31 17:39:45 +0530

    WIP: Actions Modal And Page

--- src/views/components/ai_assistant/AiActions.jsx
+++ src/views/components/ai_assistant/AiActions.jsx
@@ -1,69 +1,81 @@
 import React, { memo, useState } from 'react';
 import '../../../assets/scss/ai_assistant/aiInstructions.scss';
-
+import ToggleSwitch from '../input/slider';
+import ActionsModal from '../modalsV2/ai_assistant/ActionsModal';
 const AiActions = () => {
-	const [toggleStates, setToggleStates] = useState({
+	const [info, setInfo] = useState({
 		instruction_toggle_1: false,
 		instruction_toggle_2: false,
+		actionModal: false,
 	});
 
 	const handleToggleChange = (toggleId) => {
-		setToggleStates((prevStates) => ({
+		setInfo((prevStates) => ({
 			...prevStates,
 			[toggleId]: !prevStates[toggleId],
 		}));
 	};
 
+	const handleAddAction = () => {
+		setInfo((prevStates) => ({
+			...prevStates,
+			actionModal: !prevStates.actionModal,
+		}));
+	};
+
 	return (
-		<div className="aiInstructionsParentContainer">
-			<div className="instructionsHeaderContainer">
-				<div className="instructionsHeader">
-					<span className="lineone">Action</span>
-					<span className="linetwo">
-						Automates tasks like triggering APIs or connecting with external systems.
-					</span>
+		<div style={{ width: '100%' }}>
+			<div className="aiInstructionsParentContainer">
+				<div className="instructionsHeaderContainer">
+					<div className="instructionsHeader">
+						<span className="lineone">Action</span>
+						<span className="linetwo">
+							Automates tasks like triggering APIs or connecting with external
+							systems.
+						</span>
+					</div>
+
+					<div className="addInstruction" onClick={handleAddAction}>
+						Add action
+					</div>
 				</div>
 
-				<div className="addInstruction">Add action</div>
-			</div>
-
-			<div className="instructionsListContainer">
-				<div className="header">
-					<span>Title</span>
-					<span>Last edit</span>
-					<span>Active</span>
-				</div>
-				<div className="instructionItem">
-					<span>Speak about customer</span>
-					<span style={{ color: '#7C7C84' }}>Jul, 26 2024</span>
-
-					<span className="toggleSwitch">
-						<input
-							type="checkbox"
-							id="instruction_toggle_1"
-							className="toggle"
-							checked={toggleStates.instruction_toggle_1}
-							onChange={() => handleToggleChange('instruction_toggle_1')}
-						/>
-						<label htmlFor="instruction_toggle_1"></label>
-					</span>
-				</div>
-				<div className="instructionItem">
-					<span>Speak about customer</span>
-					<span style={{ color: '#7C7C84' }}>Jul, 26 2024</span>
-
-					<span className="toggleSwitch">
-						<input
-							type="checkbox"
-							id="instruction_toggle_2"
-							className="toggle"
-							checked={toggleStates.instruction_toggle_2}
-							onChange={() => handleToggleChange('instruction_toggle_2')}
-						/>
-						<label htmlFor="instruction_toggle_2"></label>
-					</span>
+				<div className="instructionsListContainer">
+					<div className="header">
+						<span>Title</span>
+						<span>Last edit</span>
+						<span>Active</span>
+					</div>
+					<div className="instructionItem">
+						<span>Speak about customer</span>
+						<span style={{ color: '#7C7C84' }}>Jul, 26 2024</span>
+						<span className="aiToggleSwitch">
+							<ToggleSwitch
+								id="instruction_toggle_1"
+								value={info.instruction_toggle_1}
+								onChange={() => handleToggleChange('instruction_toggle_1')}
+							/>
+						</span>
+					</div>
+					<div className="instructionItem">
+						<span>Speak about customer</span>
+						<span style={{ color: '#7C7C84' }}>Jul, 26 2024</span>
+						<span className="aiToggleSwitch">
+							<ToggleSwitch
+								id="instruction_toggle_2"
+								value={info.instruction_toggle_2}
+								onChange={() => handleToggleChange('instruction_toggle_2')}
+							/>
+						</span>
+					</div>
 				</div>
 			</div>
+
+			<ActionsModal
+				isOpen={info?.actionModal}
+				// isOpen={true}
+				onClose={handleAddAction}
+			/>
 		</div>
 	);
 };
--- src/views/components/modalsV2/ai_assistant/ActionsModal.jsx
+++ src/views/components/modalsV2/ai_assistant/ActionsModal.jsx
@@ -1,16 +1,19 @@
 import React, { memo, useState } from 'react';
+import '../../../../assets/scss/ai_assistant/modal/actionsModal.scss';
 import ReactModal from '../index';
 import { ReactComponent as CloseSvg } from '../../../../assets/svg/close.svg';
 import { ReactComponent as DeleteSvg } from '../../../../assets/svg/tasks/dustBin.svg';
-import '../../../../assets/scss/ai_assistant/modal/actionsModal.scss';
 import ActionButton from '../../ai_assistant/ActionButton';
 import InputComponent from '../../ai_assistant/InputComponent';
 import TextareaComponent from '../../ai_assistant/TextareaComponent';
 import { ReactComponent as DownSvg } from '../../../../assets/svg/activity/down.svg';
 import { Tooltip } from 'antd';
+import { ReactComponent as TrashSvg } from '../../../../assets/svg/tasks/dustBin.svg';
+import { ReactComponent as PlusSvg } from '../../../../assets/svg/tasks/plus.svg';
 
 const methodsOptions = ['GET', 'POST', 'PUT', 'DELETE'];
 const apiUsesOptions = ['JSON'];
+const variableTypes = ['Text', 'Number', 'Float', 'Boolean'];
 
 const ActionsModal = ({
 	isOpen,
@@ -26,13 +29,103 @@ const ActionsModal = ({
 	isDeletebtnLoading,
 }) => {
 	const [info, setInfo] = useState({
-		activeTab: 'endpoint',
+		activeTab: 'endpoint', // endpoint, headers, body
 		method: 'GET',
 		apiUses: 'JSON',
+		url: '',
+		isMethodDropdownOpen: false,
+		isApiUsesDropdownOpen: false,
+		variables: [],
+		openVariableTypeId: null,
 	});
+
+	const handleTabChange = (tab) => {
+		setInfo((prevInfo) => ({ ...prevInfo, activeTab: tab }));
+	};
 
 	const updateInfo = (updateValue) => {
 		setInfo((prevInfo) => ({ ...prevInfo, ...updateValue }));
+	};
+
+	const handleUrlChange = (e) => {
+		updateInfo({ url: e.target.value });
+	};
+
+	const handleMethodChange = (method) => {
+		updateInfo({ method, isMethodDropdownOpen: false });
+	};
+
+	const handleApiUsesChange = (apiUses) => {
+		updateInfo({ apiUses, isApiUsesDropdownOpen: false });
+	};
+
+	const handleMethodDropdownVisibility = (visible) => {
+		updateInfo({ isMethodDropdownOpen: visible });
+	};
+
+	const handleApiUsesDropdownVisibility = (visible) => {
+		updateInfo({ isApiUsesDropdownOpen: visible });
+	};
+
+	const handleVariableTypeDropdownVisibility = (id, visible) => {
+		updateInfo({ openVariableTypeId: visible ? id : null });
+	};
+
+	const handleVariableTypeChange = (id, type) => {
+		const updatedVariables = info.variables.map((variable) =>
+			variable.id === id ? { ...variable, type } : variable,
+		);
+		updateInfo({ variables: updatedVariables, openVariableTypeId: null });
+	};
+
+	const handleAddVariable = () => {
+		const newVariable = {
+			id: Date.now(),
+			name: '',
+			type: 'Text',
+		};
+		updateInfo({ variables: [...info.variables, newVariable] });
+	};
+
+	const handleVariableNameChange = (id, name) => {
+		const updatedVariables = info.variables.map((variable) =>
+			variable.id === id ? { ...variable, name } : variable,
+		);
+		updateInfo({ variables: updatedVariables });
+	};
+
+	const handleVariableDelete = (id) => {
+		const updatedVariables = info.variables.filter((variable) => variable.id !== id);
+		updateInfo({ variables: updatedVariables });
+	};
+
+	const tabs = {
+		endpoint: {
+			label: 'Endpoint',
+			component: (
+				<EndpointTab
+					url={info.url}
+					method={info.method}
+					apiUses={info.apiUses}
+					isMethodDropdownOpen={info.isMethodDropdownOpen}
+					isApiUsesDropdownOpen={info.isApiUsesDropdownOpen}
+					onUrlChange={handleUrlChange}
+					onMethodChange={handleMethodChange}
+					onApiUsesChange={handleApiUsesChange}
+					onMethodDropdownVisibility={handleMethodDropdownVisibility}
+					onApiUsesDropdownVisibility={handleApiUsesDropdownVisibility}
+					variables={info.variables}
+					openVariableTypeId={info.openVariableTypeId}
+					onVariableTypeDropdownVisibility={handleVariableTypeDropdownVisibility}
+					onVariableTypeChange={handleVariableTypeChange}
+					onVariableNameChange={handleVariableNameChange}
+					onVariableDelete={handleVariableDelete}
+					onAddVariable={handleAddVariable}
+				/>
+			),
+		},
+		headers: { label: 'Headers', component: <MethodTab /> },
+		body: { label: 'Body', component: <ApiUsesTab /> },
 	};
 
 	return (
@@ -40,7 +133,10 @@ const ActionsModal = ({
 			isOpen={isOpen}
 			closeModal={onClose}
 			modalType={'center'}
-			customStyles={{ content: { borderRadius: '15px' } }}
+			customStyles={{
+				overlay: { zIndex: 1001 },
+				content: { borderRadius: '15px', zIndex: 1002 },
+			}}
 		>
 			<div className="actions-modal">
 				<div className="actions-modal-header">
@@ -61,103 +157,32 @@ const ActionsModal = ({
 					<p>Build the API call for Action, including inputs from chats, variable.</p>
 				</div>
 
-				<div className="actions-modal-tabs">
-					<div className="actions-modal-tabs-header">
-						<span
-							className={`actions-modal-tabs-header-item ${
-								info?.activeTab === 'endpoint' ? 'active' : ''
-							}`}
-							onClick={() => updateInfo({ activeTab: 'endpoint' })}
-						>
-							Endpoint
-						</span>
-						<span
-							className={`actions-modal-tabs-header-item ${
-								info?.activeTab === 'headers' ? 'active' : ''
-							}`}
-							onClick={() => updateInfo({ activeTab: 'headers' })}
-						>
-							Headers
-						</span>
-					</div>
-					<div className="actions-modal-tabs-body">
-						<InputComponent
-							placeholder="Add URL"
-							value={info?.endpoint}
-							onChange={() => {}}
-						/>
-						<div className="actions-modal-tabs-body-dropdown-wrapper">
-							<div className="actions-modal-dropdown">
-								<span className="actions-modal-dropdown-label">Method</span>
-								<Tooltip
-									placement="bottom"
-									title={
-										<div className="actions-dropdown">
-											{methodsOptions?.map((option) => (
-												<div
-													className="actions-dropdown-item"
-													onClick={() => updateInfo({ method: option })}
-												>
-													{option}
-												</div>
-											))}
-										</div>
-									}
-									arrow={false}
-									trigger={'click'}
-									color={'transparent'}
-									overlayStyle={{ minWidth: 'fit-content', padding: '0' }}
-								>
-									<div className="actions-dropdown-selected">
-										{info?.method}{' '}
-										<DownSvg
-											className={`${info?.isSelectVoiceOpen ? 'open' : ''}`}
-										/>
-									</div>
-								</Tooltip>
+				<div className="actionsTabContainer">
+					<div className="actionTabs">
+						{Object.keys(tabs)?.map((tab) => (
+							<div
+								key={tab}
+								className={`actionTab ${info?.activeTab === tab ? 'active' : ''}`}
+								onClick={() => handleTabChange(tab)}
+							>
+								{tabs?.[tab]?.label}
 							</div>
-							<div className="actions-modal-dropdown">
-								<span className="actions-modal-dropdown-label">This API uses</span>
-								<Tooltip
-									placement="bottom"
-									title={
-										<div className="actions-dropdown">
-											{apiUsesOptions?.map((option) => (
-												<div
-													className="actions-dropdown-item"
-													onClick={() => updateInfo({ apiUses: option })}
-												>
-													{option}
-												</div>
-											))}
-										</div>
-									}
-									arrow={false}
-									trigger={'click'}
-									color={'transparent'}
-									overlayStyle={{ minWidth: 'fit-content', padding: '0' }}
-								>
-									<div className="actions-dropdown-selected">
-										{info?.apiUses}{' '}
-										<DownSvg
-											className={`${info?.isSelectVoiceOpen ? 'open' : ''}`}
-										/>
-									</div>
-								</Tooltip>
-							</div>
-						</div>
+						))}
 					</div>
 				</div>
+
+				{tabs?.[info?.activeTab]?.component}
+
 				<div className="actions-modal-footer">
 					<div>
 						{showDelete && (
-							<button
+							<div
 								onClick={onDeleteClick ? onDeleteClick : onClose}
 								className="actions-modal-footer-delete-button"
 								disabled={isDeletebtnLoading}
 							>
 								<DeleteSvg />
-							</button>
+							</div>
 						)}
 					</div>
 					<ActionButton
@@ -177,3 +202,193 @@ const ActionsModal = ({
 };
 
 export default memo(ActionsModal);
+
+const EndpointTab = ({
+	url,
+	method,
+	apiUses,
+	isMethodDropdownOpen,
+	isApiUsesDropdownOpen,
+	onUrlChange,
+	onMethodChange,
+	onApiUsesChange,
+	onMethodDropdownVisibility,
+	onApiUsesDropdownVisibility,
+	variables,
+	openVariableTypeId,
+	onVariableTypeDropdownVisibility,
+	onVariableTypeChange,
+	onVariableNameChange,
+	onVariableDelete,
+	onAddVariable,
+}) => {
+	return (
+		<div className="endpointTabContainer">
+			<div className="addUrlContainer">
+				<input
+					type="text"
+					className="url"
+					placeholder="Add URL"
+					value={url}
+					onChange={onUrlChange}
+				/>
+				<div className="urlMethodCotainer">
+					<div className="method">
+						<div className="method-label">Method</div>
+						<Tooltip
+							open={isMethodDropdownOpen}
+							onOpenChange={onMethodDropdownVisibility}
+							placement="bottom"
+							title={
+								<div className="actions-dropdown">
+									{methodsOptions?.map((option) => (
+										<div
+											key={option}
+											className="actions-dropdown-item"
+											onClick={() => onMethodChange(option)}
+										>
+											{option}
+										</div>
+									))}
+								</div>
+							}
+							arrow={false}
+							trigger={'click'}
+							color={'transparent'}
+							overlayStyle={{ minWidth: 'fit-content', padding: '0' }}
+						>
+							<div className="method-dropdown">
+								{method}
+								<DownSvg className={`${isMethodDropdownOpen ? 'open' : ''}`} />
+							</div>
+						</Tooltip>
+					</div>
+					<div className="method">
+						<div className="method-label">API body Type</div>
+						<Tooltip
+							open={isApiUsesDropdownOpen}
+							onOpenChange={onApiUsesDropdownVisibility}
+							placement="bottom"
+							title={
+								<div className="actions-dropdown">
+									{apiUsesOptions?.map((option) => (
+										<div
+											key={option}
+											className="actions-dropdown-item"
+											onClick={() => onApiUsesChange(option)}
+										>
+											{option}
+										</div>
+									))}
+								</div>
+							}
+							arrow={false}
+							trigger={'click'}
+							color={'transparent'}
+							overlayStyle={{ minWidth: 'fit-content', padding: '0' }}
+						>
+							<div className="method-dropdown">
+								{apiUses}
+								<DownSvg className={`${isApiUsesDropdownOpen ? 'open' : ''}`} />
+							</div>
+						</Tooltip>
+					</div>
+				</div>
+			</div>
+
+			<div className="addVariablesContainer">
+				<div className="header">
+					<h2>Get inputs from chat</h2>
+					<p>
+						List any info your AI Agent needs to find in the conversation for this
+						Action's API call.
+					</p>
+				</div>
+
+				<div className="variablesContent">
+					{variables.map((variable) => (
+						<div key={variable.id} className="variableRow">
+							<input
+								type="text"
+								className="variableInput"
+								placeholder="Input"
+								value={variable.name}
+								onChange={(e) => onVariableNameChange(variable.id, e.target.value)}
+							/>
+							<div className="variableType">
+								<Tooltip
+									open={openVariableTypeId === variable.id}
+									onOpenChange={(visible) =>
+										onVariableTypeDropdownVisibility(variable.id, visible)
+									}
+									placement="bottomLeft"
+									title={
+										<div className="actions-dropdown">
+											{variableTypes.map((type) => (
+												<div
+													key={type}
+													className="actions-dropdown-item"
+													onClick={() =>
+														onVariableTypeChange(variable.id, type)
+													}
+												>
+													{type}
+												</div>
+											))}
+										</div>
+									}
+									arrow={false}
+									trigger={'click'}
+									color={'transparent'}
+									overlayStyle={{
+										minWidth: 'fit-content',
+										padding: '0',
+									}}
+									overlayInnerStyle={{
+										padding: 0,
+										backgroundColor: 'transparent',
+									}}
+								>
+									<div className="method-dropdown">
+										{variable.type}
+										<DownSvg
+											className={`${
+												openVariableTypeId === variable.id ? 'open' : ''
+											}`}
+										/>
+									</div>
+								</Tooltip>
+							</div>
+							<div
+								className="deleteVariable"
+								onClick={() => onVariableDelete(variable.id)}
+							>
+								<TrashSvg className="trash" />
+							</div>
+						</div>
+					))}
+
+					{variables.length === 0 && (
+						<div className="emptyState">
+							<p>No inputs added</p>
+							<p>Add inputs to extract from chat</p>
+						</div>
+					)}
+
+					<div className="addVariableButton" onClick={onAddVariable}>
+						<PlusSvg />
+						Add
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const MethodTab = () => {
+	return <div>MethodTab</div>;
+};
+
+const ApiUsesTab = () => {
+	return <div>ApiUsesTab</div>;
+};
