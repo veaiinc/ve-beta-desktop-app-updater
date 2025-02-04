4b064ee5fa456d7a2e1a42d9704064ee9aab08ac
Sheshant Manure <sheshant@ve.ai>
2025-02-04 18:34:38 +0530

    commented tasks in dashboard

--- src/views/features/home_page/HomePage.jsx
+++ src/views/features/home_page/HomePage.jsx
@@ -26,7 +26,7 @@ const navbarOptions = {
 	],
 	dashboard: [
 		{ id: 1, title: 'Priority', value: 'Priority' },
-		{ id: 2, title: 'Tasks', value: 'Tasks' },
+		// { id: 2, title: 'Tasks', value: 'Tasks' },
 		{ id: 3, title: 'Workflows', value: 'Workflows' },
 		// { id: 4, title: 'Recent Chats', value: 'Recent Chats' },
 		{ id: 5, title: 'Drafts & Activity', value: 'Drafts & Activity' },
