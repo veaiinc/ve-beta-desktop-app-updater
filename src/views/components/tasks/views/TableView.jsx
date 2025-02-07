be0e62e81048c93a16fc9687bcfd5ae8ce54d21c
sabith <sabith@ve.ai>
2025-02-07 17:24:46 +0530

    Update table view scrolling height property

--- src/views/components/tasks/views/TableView.jsx
+++ src/views/components/tasks/views/TableView.jsx
@@ -217,7 +217,7 @@ const TableView = ({
 								overflow: 'auto',
 								width: '100%',
 							}}
-							maxHeight="calc(100vh - 160px)"
+							height="calc(100vh - 160px)"
 							scrollThreshold="90%"
 						>
 							<TableBody
