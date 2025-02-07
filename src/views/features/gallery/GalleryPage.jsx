0db4f954991f70dfff25497afe6b11454dd57c7d
Sheshant Manure <sheshant@ve.ai>
2025-02-07 18:47:21 +0530

    Chnaged the api call

--- src/views/features/gallery/GalleryPage.jsx
+++ src/views/features/gallery/GalleryPage.jsx
@@ -2997,7 +2997,7 @@ const GalleryPage = () => {
 				info.selectedImages.length === 0
 			) {
 				const payload = {
-					imageType: 'optimized',
+					imageType: 'original',
 				};
 
 				const response = await downloadImagesForClientSelection(
