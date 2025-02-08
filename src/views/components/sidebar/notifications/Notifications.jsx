9b2279b2bc24c52298e90554376565171aadb749
Sheshant Manure <sheshant@ve.ai>
2025-02-08 16:59:47 +0530

    update: integrate activity logs api inside notifications

--- src/views/components/sidebar/notifications/Notifications.jsx
+++ src/views/components/sidebar/notifications/Notifications.jsx
@@ -1,9 +1,14 @@
 import { Drawer } from 'antd';
-import React from 'react';
+import React, { useContext, useEffect, useState, useCallback } from 'react';
 import { ReactComponent as Back } from '../../../../assets/svg/sidebar/notifications/back.svg';
 import { ReactComponent as Search } from '../../../../assets/svg/sidebar/notifications/search.svg';
 import { ReactComponent as Filter } from '../../../../assets/svg/sidebar/notifications/filter.svg';
 import { ReactComponent as Menu } from '../../../../assets/svg/sidebar/notifications/menu.svg';
+import { ReactComponent as WhiteDot } from '../../../../assets/svg/sidebar/notifications/white-dot.svg';
+import Context from '../../../../context/context';
+import moment from 'moment';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { FetchMoreLoaderComp } from '../../../../helpers';
 
 const ctaMapper = [
 	{
@@ -24,7 +29,81 @@ const ctaMapper = [
 	},
 ];
 
+const infiniteScrollHeight = 'calc(100vh - 72px)';
+
 const Notifications = ({ showNotificationsDrawer, setShowNotificationsDrawer }) => {
+	let {
+		templates: { getActivityLogs, activityLogs, moreActivityLogs },
+	} = useContext(Context);
+
+	const [info, setInfo] = useState({
+		loading: true,
+		activityLogsData: [],
+		currentPage: 1,
+		hasNextPage: false,
+	});
+
+	useEffect(() => {
+		if (showNotificationsDrawer && !activityLogs) {
+			getActivityLogsData(1);
+		}
+	}, [showNotificationsDrawer, activityLogs]);
+
+	useEffect(() => {
+		if (activityLogs) {
+			handleActivityLogsData(activityLogs);
+		}
+	}, [activityLogs]);
+
+	useEffect(() => {
+		if (moreActivityLogs) {
+			handleActivityLogsData(moreActivityLogs, true);
+		}
+	}, [moreActivityLogs]);
+
+	const handleActivityLogsData = useCallback(
+		(incomingData, fetchMore = false) => {
+			const { currentPage, data, hasNextPage } = incomingData;
+			let activityLogsData = data;
+			if (fetchMore) {
+				activityLogsData = [...info?.activityLogsData, ...data];
+			}
+
+			setInfo((prev) => ({
+				...prev,
+				loading: false,
+				activityLogsData: activityLogsData,
+				hasNextPage,
+				currentPage,
+			}));
+		},
+		[info],
+	);
+
+	const getActivityLogsData = useCallback(
+		(page, fetchMore = false) => {
+			const payload = {
+				filters: {
+					limit: 30,
+					page: page,
+				},
+			};
+
+			getActivityLogs(payload, fetchMore);
+		},
+		[info?.hasNextPage, info?.loading],
+	);
+
+	const fetchMoreActivityLogs = useCallback(() => {
+		if (info?.hasNextPage) {
+			getActivityLogsData(info?.currentPage + 1, true);
+		}
+	}, [info?.currentPage, info?.hasNextPage]);
+
+	const formatTimestamp = (timestamp) => {
+		return moment.unix(timestamp).fromNow();
+	};
+
 	return (
 		<Drawer
 			title={null}
@@ -44,7 +123,49 @@ const Notifications = ({ showNotificationsDrawer, setShowNotificationsDrawer }) 
 						))}
 					</div>
 				</div>
-				<div className="body"></div>
+				<div className="body">
+					{info?.loading && (
+						<div className="loading-state">
+							<p className="message">Loading notifications...</p>
+						</div>
+					)}
+					<InfiniteScroll
+						dataLength={info?.activityLogsData?.length || 0}
+						next={fetchMoreActivityLogs}
+						hasMore={info?.hasNextPage}
+						loader={<FetchMoreLoaderComp />}
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'flex-start',
+							gap: '8px',
+							flex: '1 0 0',
+							alignSelf: 'stretch',
+						}}
+						height={infiniteScrollHeight}
+					>
+						{info?.activityLogsData?.map((activityLog) => (
+							<div key={activityLog?._id} className="activity-log">
+								<WhiteDot />
+								<p className="summary">
+									{activityLog?.summary || ''}
+									<br />
+									<span className="action">{activityLog?.action}</span>
+								</p>
+								<p className="time">
+									{activityLog?.timestamp
+										? formatTimestamp(activityLog?.timestamp)
+										: ''}
+								</p>
+							</div>
+						))}
+						{info?.activityLogsData?.length === 0 && !info?.loading && (
+							<div className="empty-state">
+								<p className="message">No notifications yet!</p>
+							</div>
+						)}
+					</InfiniteScroll>
+				</div>
 			</div>
 		</Drawer>
 	);
