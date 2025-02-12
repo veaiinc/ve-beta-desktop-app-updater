2240d01c2412349f664ae4cd147c406672d1d512
Sheshant Manure <sheshant@ve.ai>
2025-02-12 18:43:16 +0530

    wip: brand voice

--- src/views/features/settings/Workspace.jsx
+++ src/views/features/settings/Workspace.jsx
@@ -4,6 +4,7 @@ import Context from '../../../context/context';
 import WorkspaceHandleComponent from '../../components/settings/workspace/WorkspaceHandle';
 import TimeZoneCurrencyComponent from '../../components/settings/workspace/TimezoneCurrency';
 import DeleteWorkpsaceComponent from '../../components/settings/workspace/DeleteWorkspace';
+import { Tooltip } from 'antd';
 
 const SettingsWorkspace = () => {
 	// # Contexts
@@ -35,7 +36,12 @@ const SettingsWorkspace = () => {
 			<div className="settingsBoxContainer workspaceHandleComponent">
 				<WorkspaceHandleComponent overviewState={overviewState} />
 			</div>
-
+			<div className="brandVoiceContainer">
+				<h1 className="title">Brand Voice</h1>
+				<Tooltip title="Description">
+					<BrandVoiceDescription />
+				</Tooltip>
+			</div>
 			<div className="settingsBoxContainer timezoneCurrencyComponent">
 				<TimeZoneCurrencyComponent overviewState={overviewState} />
 			</div>
@@ -49,3 +55,15 @@ const SettingsWorkspace = () => {
 };
 
 export default memo(SettingsWorkspace);
+
+const BrandVoiceDescription = memo(() => {
+	return (
+		<div className="brandVoiceDescription">
+			<p className="description">
+				The voice description defines the core characteristics of your brand's voice. This
+				should be detailed and specific enough for someone unfamiliar with your brand to
+				successfully emulate your voice.
+			</p>
+		</div>
+	);
+});
