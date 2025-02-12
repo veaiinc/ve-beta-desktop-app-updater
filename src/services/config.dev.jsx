c3cc2a4056557590965b7259561c53aca628215d
Your Name <shubham.manohar@ve.co>
2025-02-12 16:28:47 +0530

    config changes

--- src/services/config.dev.jsx
+++ src/services/config.dev.jsx
@@ -7,7 +7,7 @@ module.exports = {
 	ve_conversations_api: 'https://api.ve.co/ve-conversations/1.0',
 	proposals_api: 'https://ap.api.ve.ai/proposals/1.0',
 	workflows_Api: 'https://ap.api.ve.ai/workflows/1.0',
-	auth_Api: 'https://auth.ve.ai',
+	auth_Api: 'https://ap.api.ve.ai/auth/dev',
 	activity_api: 'https://ap.api.ve.ai/workflow-user-analytics/1.0',
 	// galleries: 'https://ap.api.ve.ai/galleries/1.0',
 	galleries: 'https://ap.api.ve.ai/galleries/dev',
@@ -18,6 +18,7 @@ module.exports = {
 	slack_api: 'https://slack.com/api',
 
 	//outside India
+	auth_Api_US: 'https://us.api.ve.ai/auth/dev',
 	tenant_api_US: 'https://us.api.ve.ai/tenants/1.0',
 	tenant_users_api_US: 'https://us.api.ve.ai/tenant-users/1.0',
 	ve_conversations_api_US: 'https://us.api.ve.ai/ve-conversations/1.0',
