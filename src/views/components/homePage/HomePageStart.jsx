ee23740b2a3edd4a4d1315a5ff1f44aa90f1b44d
Krishna Sai <krishnasai@ve.ai>
2025-02-13 15:57:33 +0530

    Ui changes

--- src/views/components/homePage/HomePageStart.jsx
+++ src/views/components/homePage/HomePageStart.jsx
@@ -2,24 +2,43 @@ import React, { memo } from 'react';
 import '../../../assets/scss/home_page/homepage.scss';
 const HomePageStart = ({ setInfo, promptsData }) => {
 	return (
-		<div className="home-page-cards-container">
-			{promptsData?.data?.map((card) => (
+		<>
+			<div className="home-page-cards-container">
+				{promptsData?.data?.map((card) => (
+					<div
+						key={card?.id}
+						className="home-page-cards-container-card"
+						onClick={() => {
+							setInfo((prev) => ({
+								...prev,
+								showPromptPopup: true,
+								selectedCard: card,
+							}));
+						}}
+					>
+						<div className="home-page-cards-container-card-sub-title">
+							{card?.category}
+						</div>
+						<div className="home-page-cards-container-card-title">{card?.title}</div>
+					</div>
+				))}
+			</div>
+			{!promptsData?.data?.length && (
 				<div
-					key={card?.id}
-					className="home-page-cards-container-card"
-					onClick={() => {
-						setInfo((prev) => ({
-							...prev,
-							showPromptPopup: true,
-							selectedCard: card,
-						}));
+					className="home-page-cards-container-no-data"
+					style={{
+						marginTop: '16px',
+						color: '#f2f2f3',
+						textAlign: 'center',
+						fontSize: '16px',
+						fontWeight: '500',
+						width: '100%',
 					}}
 				>
-					<div className="home-page-cards-container-card-sub-title">{card?.category}</div>
-					<div className="home-page-cards-container-card-title">{card?.title}</div>
+					No data found
 				</div>
-			))}
-		</div>
+			)}
+		</>
 	);
 };
 
