1417fc7a7e0a99f2c38c8833cff945517ce150da
preetam <preetamsingh@ve.co>
2025-02-18 19:39:05 +0530

    FIX: Playground Chat Size

--- src/views/components/ai_assistant/AiPlayGround.jsx
+++ src/views/components/ai_assistant/AiPlayGround.jsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 
 const AiPlayGround = ({ assistant }) => {
 	if (!assistant?._id) return null;
-	const aiChatUrl = `https://widget.ve.ai/${assistant._id}`;
+	const aiChatUrl = `https://widget.ve.ai/${assistant._id}?modeType=small`;
 	return (
 		<div className="aiPlayGroundParentContainer">
 			<div className="ai-playground-container">
