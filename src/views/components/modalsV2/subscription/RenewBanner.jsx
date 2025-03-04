edd2069fb182c78f66318a058da350a6bfa72d6c
Krishna Sai <krishnasai@ve.ai>
2025-03-04 13:08:43 +0530

    Added the Subscriptions popup

--- src/views/components/modalsV2/subscription/RenewBanner.jsx
+++ src/views/components/modalsV2/subscription/RenewBanner.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import '../../../../assets/scss/subscriptions/renewBanner.scss';
 import { useLocation } from 'react-router-dom';
+import Context from '../../../../context/context';
+import AddOnPlans from '../../../components/settings/planbilling/addOnCards';
 
 const MappedApp = {
-	docs: 'Workflow',
+	docs: 'Documents',
 	'ai-assistant': 'Conversational Agent',
 	galleries: 'Classic Gallery',
 	'lite-gallery': 'Lite Gallery',
@@ -16,19 +18,52 @@ const MappedApp = {
 };
 
 const RenewBanner = () => {
+	const {
+		subscriptionInfo: { getAllSubscriptionPlan },
+	} = useContext(Context);
+	const [info, setInfo] = useState({
+		isOpen: false,
+		subscriptionLoading: false,
+		subscriptionState: '',
+	});
 	const location = useLocation();
 	const route = location.pathname.split('/')[1];
 	const currentRoute = MappedApp[route];
 
+	const handleUpgradeSubscriptionClick = async () => {
+		setInfo((prev) => ({ ...prev, subscriptionLoading: true }));
+		await getAllSubscriptionPlan();
+		setInfo((prev) => ({
+			...prev,
+			isOpen: true,
+			subscriptionState: 'upgradeSubscription',
+			subscriptionLoading: false,
+		}));
+	};
+
 	return (
-		<div className="renew-banner-container">
-			<div className="renew-banner">
-				<div className="renew-banner-title">
-					Upgrade to unlock {currentRoute} full potential.
+		<>
+			<div className="renew-banner-container">
+				<div className="renew-banner">
+					<div className="renew-banner-title">
+						Upgrade to unlock {currentRoute} full potential.
+					</div>
+					<button
+						className="renew-banner-button"
+						onClick={handleUpgradeSubscriptionClick}
+					>
+						Subscribe
+					</button>
 				</div>
-				<button className="renew-banner-button">Subscribe</button>
 			</div>
-		</div>
+			<AddOnPlans
+				isOpen={info?.isOpen}
+				closeModal={() =>
+					setInfo((prev) => ({ ...prev, isOpen: false, subscriptionState: '' }))
+				}
+				subscriptionState={info?.subscriptionState}
+			/>
+		</>
 	);
 };
 
