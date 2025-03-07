5fb89f87d518a933868786c96756db64ac1cd45d
Sheshant Manure <sheshant@ve.ai>
2025-03-07 17:29:46 +0530

    fix: close recent chats when sidebar minimizes

--- src/views/components/sidebar/OpenedSidebar.jsx
+++ src/views/components/sidebar/OpenedSidebar.jsx
@@ -356,6 +356,7 @@ const OpenedSideBarItemsComponent = ({
 		// e.stopPropagation();
 		setsidebarStates({ ...sidebarStates, navStyle: 'close' });
 		setIsOpen(false);
+		setShowChatsDrawer(false);
 	};
 
 	const handleChatSelect = (chatName) => {
