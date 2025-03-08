8cf8c0097af2c4aebc8f8faf1d7dd0a5cd24696d
Your Name <shubham.manohar@ve.co>
2025-03-08 17:06:24 +0530

    removed from templates

--- src/context/Templates/reducer.js
+++ src/context/Templates/reducer.js
@@ -192,11 +192,7 @@ const actionHandlers = {
 		...state,
 		llmModels: action?.payload,
 	}),
-	GET_NOTES_SUCCESS: (state, action) => ({ ...state, notes: action?.payload }),
-	GET_MORE_NOTES_SUCCESS: (state, action) => ({
-		...state,
-		moreNotes: action?.payload,
-	}),
+
 	RESET_STATE: () => intialState,
 };
 
