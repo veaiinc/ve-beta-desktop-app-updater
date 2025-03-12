ce80e14c28f7d4d632df916455f39b37f682e2b3
Sheshant Manure <sheshant@ve.ai>
2025-03-12 12:12:19 +0530

    wip: remove expired filter till backend is ready

--- src/views/features/docs/index.jsx
+++ src/views/features/docs/index.jsx
@@ -147,7 +147,7 @@ export const statusTextmapper = {
 };
 
 const statusList = Object.values(statusTextmapper)
-	// .filter((status) => status?.label !== 'Expired') // Exclude the status with label 'Expired'
+	.filter((status) => status?.label !== 'Expired') // Exclude the status with label 'Expired'
 	.map((status) => ({
 		name: status?.label,
 		_id: status?.id,
