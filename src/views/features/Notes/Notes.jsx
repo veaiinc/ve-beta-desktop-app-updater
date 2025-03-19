3b9830c44a53da06479693978e1b650828655fab
Sheshant Manure <sheshant@ve.ai>
2025-03-19 16:38:33 +0530

    upd: hide share btn till api is ready

--- src/views/features/Notes/Notes.jsx
+++ src/views/features/Notes/Notes.jsx
@@ -96,7 +96,7 @@ const NotesEditor = ({ outerContainerStyle, innerContainerStyle }) => {
 			<div className="notes-nav-menu">
 				<span className="notes-nav-menu-item-last-edited">Edited 5 min ago</span>
 				{/* <button className="notes-nav-menu-item-comment">Comment</button> */}
-				<button className="notes-nav-menu-item-share">Share</button>
+				{/* <button className="notes-nav-menu-item-share">Share</button>  // Todo: uncomment when share api is ready */}
 				{/* <button className="notes-nav-menu-item-more">...</button> */}
 			</div>
 			<div className="notes-editor-container">
