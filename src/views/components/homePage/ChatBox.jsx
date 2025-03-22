3a0bf46f37a5e9eb3f7bf453f4566256d2d045d5
Rupesh Macharla <rupesh@ve.ai>
2025-03-22 20:08:43 +0530

    updates

--- src/views/components/homePage/ChatBox.jsx
+++ src/views/components/homePage/ChatBox.jsx
@@ -206,6 +206,14 @@ const ChatBox = ({
 	}, [currentSessionId]);
 
 	useEffect(() => {
+		if (info?.recentFiles?.length > 0 || info?.uploadedImages?.length > 0) {
+			updateStateValues({
+				chatInfo: { ...chatInfo, workspaceSearch: true },
+			});
+		}
+	}, [info?.recentFiles, info?.uploadedImages]);
+
+	useEffect(() => {
 		if (followUpQuery) {
 			setInfo((prev) => ({
 				...prev,
@@ -458,7 +466,7 @@ const ChatBox = ({
 
 					setInfo((prev) => ({
 						...prev,
-						uploadedImages: [],
+						// uploadedImages: [],
 						chatQuery: '',
 						// recentFiles: [],// not clearing the recent files , because they want like sana
 						chatFilters: initialChatFilters,
