8182483784bc384022a38501c05ccbca2592c09d
Gowtham <gowtham@ve.ai>
2025-03-24 13:25:07 +0530

    invite user issue

--- src/views/components/settings/ReusableButtonSettings.jsx
+++ src/views/components/settings/ReusableButtonSettings.jsx
@@ -27,7 +27,9 @@ const ReusableButtonSettings = ({
 		}
 	};
 
-	const handleConnectToThirdParty = async () => {};
+	const handleConnectToThirdParty = async () => {
+		await func();
+	};
 
 	// const styles = {
 	// 	base: {
