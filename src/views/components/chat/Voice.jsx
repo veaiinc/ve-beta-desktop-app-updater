1dacf5d3ea7565e2f8d19ca3df02297dfccf7c06
Shubham Manohar <shubham.manohar@ve.co>
2025-03-26 19:01:01 +0530

    added transcriptions

--- src/views/components/chat/Voice.jsx
+++ src/views/components/chat/Voice.jsx
@@ -80,7 +80,6 @@ const Voice = ({ shouldConnect, token, serverUrl, handleDisconnect }) => {
 		setTransScriptMessages(allMessages);
 	}, [voiceAssistant, localParticipant, agentMessages?.segments, localMessages?.segments]);
 
-	// console.log('transScriptMessages==>', transScriptMessages);
 	return (
 		<div className="voiceIntegrationContainer">
 			<div className="voiceIntegrationIconsContainer">
