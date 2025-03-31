313643011a0e47933718a0dc70846c5ef8cc4f63
Sheshant Manure <sheshant@ve.ai>
2025-03-31 15:47:00 +0530

    upd: animate func

--- src/views/features/onboarding/Onboarding.jsx
+++ src/views/features/onboarding/Onboarding.jsx
@@ -4,9 +4,9 @@ import Stages from '../../components/onboarding/Stages';
 
 const aboutVe = `Hi! Welcome to VEAI, you can give me access to everything from your Slack, Google Drive files, Calendar, Notion documents, and Salesforce. I'll help you get answers from data you don't have the time or energy to go through, help find connections between points in multiple different documents, record and summarize meetings you join (or the ones you skip).`;
 export const animateFadeIn = (selector) => {
-	const mainContent = document.querySelector(selector);
-	if (mainContent) {
-		mainContent.style.opacity = '1';
+	const element = document.querySelector(selector);
+	if (element) {
+		element.style.opacity = '1';
 	}
 };
 
