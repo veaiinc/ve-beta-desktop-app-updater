24cc21007e9d2301644a59c29013d5b19d9f6dfd
gowtham <gowtham@ve.ai>
2025-04-05 09:34:48 +0530

    actions styling update

--- src/views/features/docs/index.jsx
+++ src/views/features/docs/index.jsx
@@ -266,10 +266,10 @@ const Docs = () => {
 		{
 			id: 0,
 			title: 'Create new Document',
-			value: 'form-submission',
-			controlValue: 'form',
+			value: '',
+			controlValue: 'workflow',
 			action: ({ setInfo, info }) => {
-				setInfo({ ...info, openProposalPopup: true, commonState: 'form-submission' });
+				setInfo({ ...info, openProposalPopup: true, commonState: '' });
 			},
 		},
 		{
@@ -306,6 +306,15 @@ const Docs = () => {
 			controlValue: 'workflow',
 			action: ({ setInfo, info }) => {
 				setInfo({ ...info, openProposalPopup: true, commonState: 'contract' });
+			},
+		},
+		{
+			id: 5,
+			title: 'Create new Presentation',
+			value: 'presentation',
+			controlValue: 'workflow',
+			action: ({ setInfo, info }) => {
+				setInfo({ ...info, openProposalPopup: true, commonState: 'presentation' });
 			},
 		},
 	];
