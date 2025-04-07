2dd49b521476483a56f11b1ecfcce66f933d7d9e
Sheshant Manure <sheshant@ve.ai>
2025-04-07 12:50:05 +0530

    feat: add elastic search setup and routing

--- src/routes.js
+++ src/routes.js
@@ -52,6 +52,7 @@ import DocsFullView from './views/components/docs/DocsFullView';
 import FormFullView from './views/components/forms/FormFullView';
 import TaskFullView from './views/features/tasks/TaskFullView';
 import ExpandedClientView from './views/features/contacts/ExpandedClientView';
+import ElasticSearch from './views/features/elastic_search';
 
 const routes = [
 	{
@@ -501,6 +502,14 @@ const routes = [
 		component: (
 			<AuthWrapper title="Contact Details">
 				<ExpandedClientView />
+			</AuthWrapper>
+		),
+	},
+	{
+		path: '/search',
+		component: (
+			<AuthWrapper title="Search">
+				<ElasticSearch />
 			</AuthWrapper>
 		),
 	},
---
+++ src/views/features/elastic_search/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../assets/scss/elastic_search/index.scss';
+
+const ElasticSearch = () => {
+	return (
+		<div className="ElasticSearchContainer">
+			<h1 className="title">Search</h1>
+		</div>
+	);
+};
+
+export default ElasticSearch;
