3cd131da298863107d585a40b319a4607036fb91
Gowtham <gowtham@ve.ai>
2025-04-19 01:31:30 +0530

    auth wrapper padding issue

--- src/views/layouts/authWrapper.jsx
+++ src/views/layouts/authWrapper.jsx
@@ -47,7 +47,7 @@ const AuthWrapper = ({
 				style={{
 					display: 'flex',
 					height: renewBanner ? 'calc(100dvh - 41px)' : '100dvh',
-					padding: '32px 32px 0 32px',
+					padding: '32px 32px 0',
 					...outerContainerStyle,
 				}}
 			>
