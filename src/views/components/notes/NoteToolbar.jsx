564ad649707000bb119d0c043b52ae6050c83c10
sabith <sabith@ve.ai>
2025-04-22 19:43:19 +0530

    changed the toolbar styles

--- src/views/components/notes/NoteToolbar.jsx
+++ src/views/components/notes/NoteToolbar.jsx
@@ -21,13 +21,10 @@ const NoteToolbar = memo(({ sendMessage, aiResonse, resetAiResponse }) => {
 		<FormattingToolbarController
 			formattingToolbar={() => (
 				<FormattingToolbar>
-					<AskAiButton
-						key={'askAiButton'}
-						sendMessage={sendMessage}
-						aiResonse={aiResonse}
-						resetAiResponse={resetAiResponse}
-					/>
 					<BlockTypeSelect key={'blockTypeSelect'} />
+					<span className="bn-divider" />
+					<ColorStyleButton key={'colorStyleButton'} />
+					<span className="bn-divider" />
 
 					<FileCaptionButton key={'fileCaptionButton'} />
 					<FileReplaceButton key={'replaceFileButton'} />
@@ -39,19 +36,25 @@ const NoteToolbar = memo(({ sendMessage, aiResonse, resetAiResponse }) => {
 						key={'underlineStyleButton'}
 					/>
 					<BasicTextStyleButton basicTextStyle={'strike'} key={'strikeStyleButton'} />
+					<span className="bn-divider" />
 					{/* Extra button to toggle code styles */}
 					<BasicTextStyleButton key={'codeStyleButton'} basicTextStyle={'code'} />
-
-					{/* <TextColorPicker key={'textColorPicker'} /> */}
-					<ColorStyleButton key={'colorStyleButton'} />
+					<span className="bn-divider" />
 
 					<TextAlignButton textAlignment={'left'} key={'textAlignLeftButton'} />
 					<TextAlignButton textAlignment={'center'} key={'textAlignCenterButton'} />
 					<TextAlignButton textAlignment={'right'} key={'textAlignRightButton'} />
+					<span className="bn-divider" />
 					<NestBlockButton key={'nestBlockButton'} />
 					<UnnestBlockButton key={'unnestBlockButton'} />
 
 					<CreateLinkButton key={'createLinkButton'} />
+					<AskAiButton
+						key={'askAiButton'}
+						sendMessage={sendMessage}
+						aiResonse={aiResonse}
+						resetAiResponse={resetAiResponse}
+					/>
 				</FormattingToolbar>
 			)}
 		/>
