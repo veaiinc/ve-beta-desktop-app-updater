ff9fb793c9fe835bbdd86e50272212865e2d4590
Rupesh Macharla <rupesh@ve.ai>
2025-04-23 19:32:22 +0530

    changes

--- src/context/Templates/reducer.js
+++ src/context/Templates/reducer.js
@@ -212,12 +212,12 @@ const actionHandlers = {
 				}
 
 				if (
-					payload?.refined_sub_query_id &&
+					payload?.sub_query_id &&
 					payload?.reading &&
 					payload?.reading?.refined_sub_query
 				) {
 					let index = cot_refined?.findIndex(
-						(item) => item?.refined_sub_query_id === payload?.refined_sub_query_id,
+						(item) => item?.sub_query_id === payload?.sub_query_id,
 					);
 					if (index !== -1) {
 						let readings = cot_refined[index]?.readings || [];
@@ -228,7 +228,7 @@ const actionHandlers = {
 						};
 					} else {
 						cot_refined?.push({
-							refined_sub_query_id: payload?.refined_sub_query_id,
+							sub_query_id: payload?.sub_query_id,
 							readings: [{ reading: payload?.reading }],
 						});
 					}
