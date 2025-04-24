1aa24b59d4fccbfd5000858e6b6f4838e069c1e9
Rupesh Macharla <rupesh@ve.ai>
2025-04-24 16:07:47 +0530

    updates

--- src/context/Templates/state.js
+++ src/context/Templates/state.js
@@ -88,23 +88,7 @@ export const intialState = {
 	draftStateWorkflowtemplates: null,
 	moreDraftStateWorkflowtemplates: null,
 	createLeadModalContextState: false,
-	globalChatMessages: [
-		{
-			type: 'user',
-			message: 'Hello, how can I help you today?',
-		},
-		{
-			type: 'AI',
-			message: 'Hello, how can I help you today?',
-			deepResearch: {
-				cot: [
-					{
-						step: 'Hello, how can I help you today?',
-					},
-				],
-			},
-		},
-	], // { type: 'AI', message: 'Hello, how can I help you today?' }
+	globalChatMessages: [], // { type: 'AI', message: 'Hello, how can I help you today?' }
 	currentSessionId: null,
 	citations: null,
 	docsFilesList: null,
