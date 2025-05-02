b098ba86f530e739b4152893fbe894c19629ff81
Krishna Sai <krishnasai@ve.ai>
2025-05-02 19:28:19 +0530

    Updated the light gallery hasFullAccess

--- src/views/features/files/Files.jsx
+++ src/views/features/files/Files.jsx
@@ -314,10 +314,16 @@ const Files = () => {
 			if (!isAdmin && tenantUserAccessControls?.accessControls) {
 				const enabledApps = new Set(
 					tenantUserAccessControls?.accessControls
-						.filter((permission) => permission.isEnabled)
-						.map((permission) => permission.app),
+						?.filter((permission) => {
+							if (permission?.app === 'liteGallery') {
+								return permission?.isEnabled && permission?.hasFullAccess;
+							}
+							return permission?.isEnabled;
+						})
+						?.map((permission) => permission?.app),
 				);
-				filteredOptions = options?.filter((option) => enabledApps.has(option?.value));
+
+				filteredOptions = options?.filter((option) => enabledApps?.has(option?.value));
 			}
 
 			setInfo((prevInfo) => ({
