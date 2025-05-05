9c3780f3e8d028fbf956f29aec5ad99e791e82ca
sivave <sivakrishna@ve.ai>
2025-05-05 19:37:13 +0530

    [Contact new ui]

---
+++ src/views/components/contacts/ActivityContact.jsx
@@ -0,0 +1,15 @@
+import '../../../assets/scss/contacts/activityContact.scss';
+
+import React from 'react';
+
+const ActivityContact = () => {
+	return (
+		<div className="activity-contact">
+			<div className="activity-contact-header">
+				<h1>Activity</h1>
+			</div>
+		</div>
+	);
+};
+
+export default ActivityContact;
