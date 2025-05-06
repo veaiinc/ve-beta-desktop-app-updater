6484a5d2f3e40e2edc0e67416b6d6fd92f08a747
Krishna Sai <krishnasai@ve.ai>
2025-05-06 17:27:24 +0530

    updated the logic for the view report

--- src/views/features/homePage/ProactiveSuggestions.jsx
+++ src/views/features/homePage/ProactiveSuggestions.jsx
@@ -492,32 +492,32 @@ const ProactiveSuggestions = ({ selectedOption }) => {
 	};
 
 	const handleViewReportClick = useCallback((card) => {
+		const { chain_of_thought } = card;
+		const report = handleCombinedChainOfThought(chain_of_thought || []);
+
+		const messages = [
+			{
+				type: 'user',
+				moduleType: 'ai_suggestion_report',
+				data: card,
+				message: card?.title,
+			},
+			{
+				type: 'AI',
+				moduleType: 'ai_suggestion_report',
+				data: {
+					research_report: card?.research_report,
+				},
+				processing: 'Report',
+				report,
+				follow_up_query: card?.suggested_prompts,
+				stream_end: true,
+			},
+		];
+		updateStateValues({ globalChatMessages: messages });
 		if (card?.sessionId) {
 			navigate(`/chat/${card?.sessionId}`);
 		} else {
-			const { chain_of_thought } = card;
-			const report = handleCombinedChainOfThought(chain_of_thought || []);
-
-			const messages = [
-				{
-					type: 'user',
-					moduleType: 'ai_suggestion_report',
-					data: card,
-					message: card?.title,
-				},
-				{
-					type: 'AI',
-					moduleType: 'ai_suggestion_report',
-					data: {
-						research_report: card?.research_report,
-					},
-					processing: 'Report',
-					report,
-					follow_up_query: card?.suggested_prompts,
-					stream_end: true,
-				},
-			];
-			updateStateValues({ globalChatMessages: messages });
 			navigate(`/chat/${ObjectID()?.toString()}`);
 		}
 	}, []);
