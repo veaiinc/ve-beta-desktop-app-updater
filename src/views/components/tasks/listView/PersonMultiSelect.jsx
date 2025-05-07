a016a522c1dc16cc3d8a6bf52f7de3ec60551498
sabith <sabith@ve.ai>
2025-05-07 20:20:30 +0530

    updated the person component

--- src/views/components/tasks/listView/PersonMultiSelect.jsx
+++ src/views/components/tasks/listView/PersonMultiSelect.jsx
@@ -18,7 +18,7 @@ const PersonMultiSelect = ({
 
 	useEffect(() => {
 		setInfo((prev) => ({ ...prev, selected: value }));
-	}, [value]);
+	}, []);
 
 	const handleOptionClick = (option) => {
 		const isSelected = info?.selected?.some((item) => item._id === option._id);
