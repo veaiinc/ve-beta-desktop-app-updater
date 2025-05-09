edc0a10a43ee8fc2b994b8a3f35b59b6469955ec
Krishna Sai <krishnasai@ve.ai>
2025-05-09 19:24:58 +0530

    Updated the Aipeople bug where infintieScroll is not working

--- src/views/components/gallery/aiSelections/AiPeopleContainer.jsx
+++ src/views/components/gallery/aiSelections/AiPeopleContainer.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect, useState, memo } from 'react';
+import { useContext, useEffect, useState, memo } from 'react';
 import Context from '../../../../context/context';
-import InfiniteScroll from 'react-infinite-scroll-component';
+
 import PeopleCard from '../galleryView/PeopleCard';
 import { ReactComponent as DownArrow } from '../../../../assets/svg/gallery/arrow-down.svg';
 import { ReactComponent as SearchIcon } from '../../../../assets/svg/workflow/search.svg';
 import { useLocation } from 'react-router-dom';
+import InfiniteScroll from '../../globalComponents/InfiniteScroll';
 
 const AiPeopleContainer = ({
 	galleryId,
@@ -87,8 +88,11 @@ const AiPeopleContainer = ({
 							next={() => getAiFace(galleryId, aiFace?.currentPage + 1, 65)}
 							hasMore={info?.hasNextPage || false}
 							// loader={<h4 style={{ textAlign: 'center', color: '#fff' }}>Loading...</h4>}
-							scrollableTarget="galleryScrollTarget_aiPeople"
-							scrollThreshold={0.5}
+							height={'100%'}
+							style={{
+								maxHeight: info?.showMore ? '390px' : '195px',
+								overflowY: info?.showMore ? 'scroll' : 'hidden',
+							}}
 						>
 							<div
 								className={`aiPeople ${info?.showMore ? 'aiPeople-showMore' : ''}`}
