27f25faa32ec72ef26746102de7c8b5d14475863
Kapil Motwani <kapil@ve.ai>
2025-05-24 17:17:01 +0530

    some paths updated

--- src/views/components/docs/ProposalsPopup.jsx
+++ src/views/components/docs/ProposalsPopup.jsx
@@ -389,7 +389,7 @@ const ProposalPopup = ({ open, closeModal, clientDetails = null, commonState }) 
 								>
 									<div className="docsTemplateImageContainer">
 										<iframe
-											src={`${origin}/preview/short/${template?._id}?module=${template?.moduleTemplates?.[0]?._id}&isPubic=${template?.moduleTemplates?.[0]?.isPublic}&restrictClick=true`}
+											src={`/builder/preview/short/${template?._id}?module=${template?.moduleTemplates?.[0]?._id}&isPubic=${template?.moduleTemplates?.[0]?.isPublic}&restrictClick=true`}
 											title="Builder Preview"
 											width="100%"
 											height="100%"
