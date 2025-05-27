17126323ea94585617efebed24d84ba3b9b240b4
Shah Nawaz <shahnawaz@vi.ai>
2025-05-27 16:33:45 +0530

    google calender calendar view, events popup, task widget, contacts->create document, files remove notes, automation hide

--- src/views/features/calendar/CalendarMainPage.jsx
+++ src/views/features/calendar/CalendarMainPage.jsx
@@ -26,6 +26,7 @@ const initialState = {
 	selectedSession: null,
 	sessionFilter: [],
 	showEditScheduler: false,
+	showGoogleEvents: true,
 };
 
 const aiSuggestions = [
@@ -144,18 +145,22 @@ const Calendar = () => {
 			...prevInfo,
 			showEditScheduler: false,
 			selectedSession: null,
-			isEventSelected: false,
-			selectedSlot: null,
 		}));
 	}, []);
 
 	const updateCategoryList = useCallback(() => {
-		if (calendarCategoriesList) {
-			setInfo((prevInfo) => ({
-				...prevInfo,
-				categoryList: [...calendarCategoriesList],
-			}));
-		}
+		const categories = calendarCategoriesList?.map((category) => ({
+			...category,
+			_id: category?._id,
+			name: category?.name,
+			color: category?.color,
+			type: category?.type,
+		}));
+
+		setInfo((prevInfo) => ({
+			...prevInfo,
+			categoryList: categories,
+		}));
 	}, [calendarCategoriesList]);
 
 	// Get Week Days array for <WeekDayHeader /> component
@@ -194,6 +199,7 @@ const Calendar = () => {
 						schedulerList={info?.schedulerList}
 						selectedSession={info?.selectedSession}
 						sessionFilter={info?.sessionFilter}
+						showGoogleEvents={info?.showGoogleEvents}
 					/>
 				</ChatLeftBarComponent>
 
@@ -218,6 +224,7 @@ const Calendar = () => {
 						updateCalendarInfo={updateCalendarInfo}
 						selectedWorkflowId={info?.selectedWorkflowId}
 						selectedSlot={info?.selectedSlot}
+						showGoogleEvents={info?.showGoogleEvents}
 					/>
 				)}
 			</div>
