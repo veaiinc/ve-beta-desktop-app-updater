6b64f9445662fbe30b21c366a87ad8ae24d43b61
Krishna Sai <krishnasai@ve.ai>
2025-06-09 18:40:01 +0530

    Chores small changes

--- src/views/components/settings/planbilling/addOnCards.jsx
+++ src/views/components/settings/planbilling/addOnCards.jsx
@@ -160,6 +160,10 @@ const AddOnPlans = ({
 		try {
 			let response;
 			if (subscriptionState === 'upgradeSubscription') {
+				if (data?.[0]?.quantity < currentPlan?.tenantUsers) {
+					message.error('You cannot purchase less than tenant users');
+					return;
+				}
 				response = await purchaseSubscriptionPlan(payload);
 			} else {
 				response = await purchaseAddOnPlan(payload);
