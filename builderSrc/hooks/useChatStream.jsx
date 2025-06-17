db2ebde701e5a64da1fc34c25b5c82ba36455377
Gowtham <gowtham@ve.ai>
2025-06-17 20:12:41 +0530

    update to chat_streaming

--- builderSrc/hooks/useChatStream.jsx
+++ builderSrc/hooks/useChatStream.jsx
@@ -102,7 +102,7 @@ const useChatStream = () => {
 			messageHandlerRef.current = onMessageFunc;
 			isPublicChatRef.current = isPublicChat;
 
-			const agent = agentTypeMap[agentType] || 'multi_achat_streaming';
+			const agent = agentTypeMap[agentType] || 'chat_streaming';
 			agentTypeRef.current = agent;
 
 			const usertoken = localStorage.getItem('usertoken');
