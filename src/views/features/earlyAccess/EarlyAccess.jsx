f89062d80a15158cafdcff3a87aa7b8fe4437724
Krishna Sai <krishnasai@ve.ai>
2025-06-27 17:01:32 +0530

    fixed the position Styles in the early-access page

--- src/views/features/earlyAccess/EarlyAccess.jsx
+++ src/views/features/earlyAccess/EarlyAccess.jsx
@@ -194,6 +194,7 @@ const EarlyAccess = () => {
 			return prevEmails.filter((_, i) => i !== index);
 		});
 	};
+	console.log(onboardPosition, 'onboardPosition');
 	return (
 		<div className="landing-page-container2">
 			<header className="header-container2">
@@ -209,7 +210,7 @@ const EarlyAccess = () => {
 				</section>
 				<section className="hero-section-7">
 					<div className="content-container2">
-						{onboardPosition?.onboardPosition && (
+						{onboardPosition && (
 							<div className="top-container">
 								<div>
 									<div className="top-container-text">Your place</div>
