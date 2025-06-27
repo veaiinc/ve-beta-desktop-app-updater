80793db73d93ddaef802e213a46161335418c300
Krishna Sai <krishnasai@ve.ai>
2025-06-27 18:06:55 +0530

    Placeholder styles

--- src/views/components/chat/ChatBoxPlaceholder.jsx
+++ src/views/components/chat/ChatBoxPlaceholder.jsx
@@ -8,7 +8,7 @@ const ChatBoxPlaceholder = ({ chatboxPlaceholders, activePlaceholderIndex }) => 
 				className="placeholderList"
 				style={{
 					transform: `translateY(-${
-						activePlaceholderIndex * 20 + activePlaceholderIndex
+						activePlaceholderIndex * 35 + activePlaceholderIndex
 					}px)`,
 					transition:
 						activePlaceholderIndex === 0
