241f40444936b3b8296bc885e24a6fe94351f926
Krishna Sai <krishnasai@ve.ai>
2025-06-28 17:03:12 +0530

    updated the chat box container

--- src/views/features/homePage/ProactiveSuggestions.jsx
+++ src/views/features/homePage/ProactiveSuggestions.jsx
@@ -22,6 +22,7 @@ import ChatBox from '../../components/chat/ChatBox';
 import Suggestions from './Suggestions';
 import PromptsWidget from '../../components/globalComponents/PromptsWidget';
 import BuildOptions from './BuildOptions';
+import { ReactComponent as CommandSvg } from '../../../assets/svg/files/command.svg';
 
 const payload = {
 	page: 1,
@@ -932,19 +933,13 @@ const ProactiveSuggestions = ({ previousOption = null, option = null }) => {
 								>
 									{(info?.cards.length || info?.searchQuery?.length !== 0) && (
 										<div className="right-container">
-											<div
-												style={{
-													display: 'flex',
-													flexDirection: 'row',
-													gap: '6px',
-												}}
-											>
+											<div className="searchMainContainer">
 												<div
 													className="search-wrapper"
 													data-tooltip="Search"
 												>
 													<div className="search-icon">
-														<SearchSvg />
+														<SearchSvg stroke="var(--primary-font)" />
 													</div>
 													<input
 														className="search-input"
@@ -958,170 +953,186 @@ const ProactiveSuggestions = ({ previousOption = null, option = null }) => {
 														}
 													/>
 												</div>
-												<Tooltip
-													open={info?.openFilter}
-													onOpenChange={() =>
-														setInfo((prev) => ({
-															...prev,
-															openFilter: false,
-														}))
-													}
-													placement="top"
-													title={
-														<div className="filter-container">
-															<div className="filter-items">
-																{filterGroups?.map((group, idx) => (
-																	<div
-																		key={group?.title}
-																		style={{ width: '100%' }}
-																	>
-																		<div className="filter-item">
-																			<div className="filter-item-title">
-																				{group?.title || ''}
-																			</div>
-																			<div className="filter-item-options">
-																				{group?.options?.map(
-																					(item) => {
-																						const itemWithGroup =
-																							{
-																								...item,
-																								group: group?.title,
-																							};
-
-																						const isSelected =
-																							info?.selectedFilters?.some(
-																								(
-																									option,
-																								) =>
-																									option?.title ===
-																										itemWithGroup?.title &&
-																									option?.group ===
-																										itemWithGroup?.group,
-																							);
-																						return (
-																							<div
-																								key={
-																									item?.id
-																								}
-																								className="eachOption"
-																								onClick={() =>
-																									handleFilterClick(
-																										itemWithGroup,
-																										group?.title,
-																									)
-																								}
-																							>
-																								{group?.title ===
-																									'Priority Level' && (
-																									<div
-																										className="indicator"
-																										style={{
-																											backgroundColor:
-																												item?.bgColor ||
-																												'',
-																										}}
-																									></div>
-																								)}
-																								<div className="option-text">
-																									<span className="option-text-content">
-																										{item?.title ||
-																											''}
-																									</span>
-																									{isSelected && (
-																										<TickIcon
-																											style={{
-																												marginLeft:
-																													'8px',
-																											}}
-																										/>
-																									)}
-																								</div>
-																							</div>
-																						);
-																					},
-																				)}
-																			</div>
-																		</div>
-																		{idx <
-																			filterGroups?.length -
-																				1 && (
-																			<hr
-																				style={{
-																					width: '100%',
-																					height: '1px',
-																					backgroundColor:
-																						'var(--stroke)',
-																					border: 'none',
-																					marginTop:
-																						'10px',
-																				}}
-																			/>
-																		)}
-																	</div>
-																))}
-															</div>
-														</div>
-													}
-													color={'transparent'}
-													style={{
-														cursor: 'pointer',
-														userSelect: 'none',
-													}}
-													trigger={'click'}
-												>
-													<div
-														className="action-left"
-														onClick={() => {
-															if (info?.openFilter) {
-																return;
-															}
-															setInfo((prev) => ({
-																...prev,
-																openFilter: true,
-															}));
-														}}
-													>
-														<button
-															className={`filter-btn ${
-																info?.openFilter ? 'active' : ''
-															}`}
-															data-tooltip="Filter"
-														>
-															<FilterIcon />
-														</button>
+												<div className="searchOptionsContainer">
+													<div className="command-icon">
+														<span>
+															<CommandSvg />
+														</span>
 													</div>
-												</Tooltip>
+													<div className="command-icon">
+														<span>K</span>
+													</div>
+												</div>
 											</div>
 										</div>
 									)}
 									{info?.cards?.length > 0 && (
-										<div className="action-right">
-											<button
-												className="card-change-btn"
-												onClick={(e) => {
-													e.stopPropagation();
-													e.preventDefault();
-													handleLeft();
+										<div className="optionsRightMainContainer">
+											<Tooltip
+												open={info?.openFilter}
+												onOpenChange={() =>
+													setInfo((prev) => ({
+														...prev,
+														openFilter: false,
+													}))
+												}
+												placement="top"
+												title={
+													<div className="filter-container">
+														<div className="filter-items">
+															{filterGroups?.map((group, idx) => (
+																<div
+																	key={group?.title}
+																	style={{ width: '100%' }}
+																>
+																	<div className="filter-item">
+																		<div className="filter-item-title">
+																			{group?.title || ''}
+																		</div>
+																		<div className="filter-item-options">
+																			{group?.options?.map(
+																				(item) => {
+																					const itemWithGroup =
+																						{
+																							...item,
+																							group: group?.title,
+																						};
+
+																					const isSelected =
+																						info?.selectedFilters?.some(
+																							(
+																								option,
+																							) =>
+																								option?.title ===
+																									itemWithGroup?.title &&
+																								option?.group ===
+																									itemWithGroup?.group,
+																						);
+																					return (
+																						<div
+																							key={
+																								item?.id
+																							}
+																							className="eachOption"
+																							onClick={() =>
+																								handleFilterClick(
+																									itemWithGroup,
+																									group?.title,
+																								)
+																							}
+																						>
+																							{group?.title ===
+																								'Priority Level' && (
+																								<div
+																									className="indicator"
+																									style={{
+																										backgroundColor:
+																											item?.bgColor ||
+																											'',
+																									}}
+																								></div>
+																							)}
+																							<div className="option-text">
+																								<span className="option-text-content">
+																									{item?.title ||
+																										''}
+																								</span>
+																								{isSelected && (
+																									<TickIcon
+																										style={{
+																											marginLeft:
+																												'8px',
+																										}}
+																									/>
+																								)}
+																							</div>
+																						</div>
+																					);
+																				},
+																			)}
+																		</div>
+																	</div>
+																	{idx <
+																		filterGroups?.length -
+																			1 && (
+																		<hr
+																			style={{
+																				width: '100%',
+																				height: '1px',
+																				backgroundColor:
+																					'var(--stroke)',
+																				border: 'none',
+																				marginTop: '10px',
+																			}}
+																		/>
+																	)}
+																</div>
+															))}
+														</div>
+													</div>
+												}
+												color={'transparent'}
+												style={{
+													cursor: 'pointer',
+													userSelect: 'none',
 												}}
+												trigger={'click'}
 											>
-												<ChevronRightThinSvg className="left-chevron" />
-											</button>
-											<div className="card-number">
-												<span>{currentIndexRef?.current + 1}</span>/
-												<span>
-													{aiSuggestedPendingActions?.metaInfo?.totalDocs}
-												</span>
-											</div>
-											<button
-												className="card-change-btn"
-												onClick={(e) => {
-													e.stopPropagation();
-													e.preventDefault();
-													handleRight();
-												}}
-											>
-												<ChevronRightThinSvg />
-											</button>
+												<div
+													className="action-left"
+													onClick={() => {
+														if (info?.openFilter) {
+															return;
+														}
+														setInfo((prev) => ({
+															...prev,
+															openFilter: true,
+														}));
+													}}
+												>
+													<button
+														className={`filter-btn ${
+															info?.openFilter ? 'active' : ''
+														}`}
+														data-tooltip="Filter"
+													>
+														<FilterIcon /> Filter
+													</button>
+												</div>
+											</Tooltip>
+											{info?.cards?.length > 0 && (
+												<div className="action-right">
+													<button
+														className="card-change-btn"
+														onClick={(e) => {
+															e.stopPropagation();
+															e.preventDefault();
+															handleLeft();
+														}}
+													>
+														<ChevronRightThinSvg className="left-chevron" />
+													</button>
+													<div className="card-number">
+														<span>{currentIndexRef?.current + 1}</span>/
+														<span className="total-docs">
+															{
+																aiSuggestedPendingActions?.metaInfo
+																	?.totalDocs
+															}
+														</span>
+													</div>
+													<button
+														className="card-change-btn"
+														onClick={(e) => {
+															e.stopPropagation();
+															e.preventDefault();
+															handleRight();
+														}}
+													>
+														<ChevronRightThinSvg />
+													</button>
+												</div>
+											)}
 										</div>
 									)}
 								</div>
