a2bd2b2c33543ea81653c5f5a9ee9b85e2e337be
Sheshant Manure <sheshant@ve.ai>
2025-06-30 15:15:01 +0530

    dynamic import for initial load time optimisation

--- src/views/hooks/useWorkspaceMode.jsx
+++ src/views/hooks/useWorkspaceMode.jsx
@@ -1,13 +1,20 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Context from '../../context/context';
 import { matchPath, useLocation } from 'react-router-dom';
 import useLogout from './useLogout';
 
 // routes
-import publicRoutes, { publicRoutesList } from '../../routes/publicRoutes';
-import stableRoutes from '../../routes/stableRoutes';
-import betaRoutes from '../../routes/betaRoutes';
+import { publicRoutesList } from '../../routes/publicRoutes';
+// import publicRoutes, { publicRoutesList } from '../../routes/publicRoutes';
+// import stableRoutes from '../../routes/stableRoutes';
+// import betaRoutes from '../../routes/betaRoutes';
 import fallbackRoute from '../../routes/fallbackRoute';
+
+// const routeMap = {
+// 	stable: 'stableRoutes',
+// 	beta: 'betaRoutes',
+// 	null: 'fallbackRoute',
+// };
 
 const useWorkspaceMode = () => {
 	const { pathname } = useLocation();
@@ -17,19 +24,26 @@ const useWorkspaceMode = () => {
 		profileInfo: { tennantSettingsData, getTenantSettings },
 	} = useContext(Context);
 
+	const [routesInfo, setRoutesInfo] = useState({
+		publicRoutes: null,
+		stableRoutes: null,
+		betaRoutes: null,
+		fallbackRoute,
+	});
+
 	const workspaceMode = tennantSettingsData?.workspaceMode ?? null; // stable, beta, internal
 	const isPublicRoute = publicRoutesList.some((routePath) =>
 		matchPath({ path: routePath, end: true }, pathname),
 	);
-	const loading = isPublicRoute ? false : workspaceMode === null; // since public routes don't have workspace mode. Until workspace mode becomes stable/beta, loading is true.
-	const routes = isPublicRoute
-		? publicRoutes
+	const routeType = isPublicRoute
+		? 'publicRoutes'
 		: workspaceMode === 'stable'
-		? stableRoutes
+		? 'stableRoutes'
 		: workspaceMode === 'beta'
-		? betaRoutes
-		: fallbackRoute; // handles reload in protected routes
-
+		? 'betaRoutes'
+		: 'fallbackRoute';
+	const routes = routesInfo[routeType] ?? routesInfo['fallbackRoute'];
+	const loading = isPublicRoute ? false : workspaceMode === null; // since public routes don't have workspace mode. Until workspace mode becomes stable/beta, loading is true.
 	const fetchWorkspaceMode = async () => {
 		try {
 			if (workspaceMode === null) {
@@ -45,11 +59,58 @@ const useWorkspaceMode = () => {
 		}
 	};
 
+	const importRoutes = async (routeType) => {
+		switch (routeType) {
+			case 'publicRoutes':
+				if (routesInfo['publicRoutes'] === null) {
+					const { default: publicRoutes } = await import('../../routes/publicRoutes');
+					setRoutesInfo((prev) => ({
+						...prev,
+						publicRoutes,
+					}));
+				}
+				break;
+
+			case 'stableRoutes':
+				if (routesInfo['stableRoutes'] === null) {
+					const { default: stableRoutes } = await import('../../routes/stableRoutes');
+					setRoutesInfo((prev) => ({
+						...prev,
+						stableRoutes,
+					}));
+				}
+				break;
+			case 'betaRoutes':
+				if (routesInfo['betaRoutes'] === null) {
+					const { default: betaRoutes } = await import('../../routes/betaRoutes');
+					setRoutesInfo((prev) => ({
+						...prev,
+						betaRoutes,
+					}));
+				}
+				break;
+			case 'fallbackRoute':
+				return;
+				// if (routesInfo['fallbackRoute'] === null) {
+				// 	const { default: fallbackRoute } = await import('../../routes/fallbackRoute');
+				// 	setRoutesInfo((prev) => ({
+				// 		...prev,
+				// 		fallbackRoute,
+				// 	}));
+				// }
+				break;
+		}
+	};
+
 	useEffect(() => {
 		// Fetch workspaceMode only for protected routes
 		if (isPublicRoute) return;
 		fetchWorkspaceMode();
 	}, [isPublicRoute]);
+
+	useEffect(() => {
+		importRoutes(routeType);
+	}, [routeType]);
 
 	return { loading, routes, workspaceMode };
 };
