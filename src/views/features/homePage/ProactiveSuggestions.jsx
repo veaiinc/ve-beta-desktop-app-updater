3e56402edb741c0feb957a8636dab58f277d936e
Krishna Sai <krishnasai@ve.ai>
2025-07-02 15:44:34 +0530

    Chores

--- src/views/features/homePage/ProactiveSuggestions.jsx
+++ src/views/features/homePage/ProactiveSuggestions.jsx
@@ -759,8 +759,7 @@ const ProactiveSuggestions = ({ previousOption = null, option = null }) => {
 				</div>
 			)}
 			{(aiSuggestedPendingActions?.pendingActions?.length > 0 ||
-				info?.searchQuery?.length !== 0 ||
-				info?.filters?.length !== 0) &&
+				(info?.searchQuery?.length !== 0 && info?.filters?.length !== 0)) &&
 				!info?.showExploreMore && (
 					<div className="options-wrapper">
 						<div
@@ -819,8 +818,8 @@ const ProactiveSuggestions = ({ previousOption = null, option = null }) => {
 							<>
 								{info?.selectedOption === firstOption ? (
 									(info?.cards?.length > 0 ||
-										info?.searchQuery?.length !== 0 ||
-										info?.filters?.length !== 0) && (
+										(info?.searchQuery?.length !== 0 &&
+											info?.filters?.length !== 0)) && (
 										<div
 											className="cards-container"
 											// style={{
