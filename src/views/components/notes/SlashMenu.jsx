7f0b90a7c02c250ec300d6ed0b08f3b8c0bc9c18
Sheshant Manure <sheshant@ve.ai>
2025-07-02 13:40:48 +0530

    remove react import

--- src/views/components/notes/SlashMenu.jsx
+++ src/views/components/notes/SlashMenu.jsx
@@ -1,5 +1,5 @@
 import { getDefaultReactSlashMenuItems, SuggestionMenuController } from '@blocknote/react';
-import React, { memo } from 'react';
+import { memo } from 'react';
 import { insertImage } from './ImageComponent';
 import { insertDatabase } from './Database';
 import { filterSuggestionItems } from '@blocknote/core';
