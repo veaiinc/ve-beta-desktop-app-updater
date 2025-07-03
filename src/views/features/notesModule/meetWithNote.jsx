1c74e5e8460276eca27178d694f1160afe214b64
Sheshant Manure <sheshant@ve.ai>
2025-07-03 18:29:12 +0530

    changes

--- src/views/features/notesModule/meetWithNote.jsx
+++ src/views/features/notesModule/meetWithNote.jsx
@@ -31,7 +31,7 @@ import { ReactComponent as CrossIcon } from '../../../assets/svg/notes/cross.svg
 import { message } from '../../components/globalComponents/CustomToast';
 import { Helmet } from 'react-helmet';
 import Skeleton from 'react-loading-skeleton';
-import useChatStream from '../../hooks/useChatStream';
+// import useChatStream from '../../../hooks/useChatStream';
 import ObjectID from 'bson-objectid';
 import jwtDecode from 'jwt-decode';
 import { ReactComponent as DustBinIcon } from '../../../assets/svg/tasks/dustBin.svg';
@@ -43,13 +43,13 @@ import IconUploadPopup from '../../components/notes/IconUploadPopup';
 import { ReactComponent as BackArrowSvg } from '../../../assets/svg/workflow/backarrow.svg';
 import { ImageBlock, insertImage } from '../../components/notes/ImageComponent';
 import { BlockNoteSchema, defaultBlockSpecs, filterSuggestionItems } from '@blocknote/core';
-import useWorkspaceMode from '../../hooks/useWorkspaceMode';
+import useWorkspaceMode from '../../../hooks/useWorkspaceMode';
 import { isEqual } from 'lodash';
 import { Database, insertDatabase } from '../../components/notes/Database';
 import DatabaseSidebar from '../../components/modalsV2/notes/DatabaseSidebar';
 import MeetTranscript from './MeetTranscript';
-import useRecallStream from '../../../hooks/useRecallStream';
 import useLiveIntelligenceStream from '../../../hooks/useLiveIntelligenceStream';
+import useRecallStream from '../../../hooks/useRecallStream';
 
 export const NotesRefContext = createContext(null);
 
