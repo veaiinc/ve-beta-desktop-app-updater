e9a6ece40bcc22f36d961eec4f85451706d437fa
Sheshant Manure <sheshant@ve.ai>
2025-07-08 19:30:31 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import useWorkspaceMode from './hooks/useWorkspaceMode';
-import CustomToast from './views/components/globalComponents/CustomToast';
 
 const App = () => {
 	const { routes } = useWorkspaceMode();
 
 	return (
-		<>
-			<CustomToast />
-			<Routes>
-				{routes?.map((route) => (
-					<Route key={route.path} path={route.path} element={route.element} />
-				))}
-			</Routes>
-		</>
+		<Routes>
+			{routes?.map((route) => (
+				<Route key={route.path} path={route.path} element={route.element} />
+			))}
+		</Routes>
 	);
 };
 
