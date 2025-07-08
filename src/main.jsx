c442d7d8183f4081314dedc852dfa108f99dda61
Sheshant Manure <sheshant@ve.ai>
2025-07-08 20:00:11 +0530

    add pg loader

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ContextState from './context/ContextStates';
 import { Suspense } from 'react';
+import PageLoader from './views/features/app/PageLoader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<ContextState>
 		<BrowserRouter>
-			<Suspense fallback={<p>loading</p>}>
+			<Suspense fallback={<PageLoader />}>
 				<App />
 			</Suspense>
 		</BrowserRouter>
