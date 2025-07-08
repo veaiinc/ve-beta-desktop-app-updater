4e2f433948cea4ec12e6275dfcf28bc05afb0866
sabith <sabith@ve.ai>
2025-07-08 19:16:59 +0530

    moved the toast component to root

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import useWorkspaceMode from './hooks/useWorkspaceMode';
+import CustomToast from './views/components/globalComponents/CustomToast';
 
 const App = () => {
 	const { routes } = useWorkspaceMode();
 
 	return (
-		<Routes>
-			{routes?.map((route) => (
-				<Route key={route.path} path={route.path} element={route.element} />
-			))}
-		</Routes>
+		<>
+			<CustomToast />
+			<Routes>
+				{routes?.map((route) => (
+					<Route key={route.path} path={route.path} element={route.element} />
+				))}
+			</Routes>
+		</>
 	);
 };
 
