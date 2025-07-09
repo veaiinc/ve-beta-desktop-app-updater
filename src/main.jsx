40d35a6e6558c747cba3ce51818c558746e06edd
Sheshant Manure <sheshant@ve.ai>
2025-07-09 13:01:20 +0530

    optimisation updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ContextState from './context/ContextStates';
-import { Suspense } from 'react';
-import PageLoader from './views/features/app/PageLoader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<ContextState>
 		<BrowserRouter>
-			<Suspense fallback={<PageLoader />}>
-				<App />
-			</Suspense>
+			<App />
 		</BrowserRouter>
 	</ContextState>,
 );
