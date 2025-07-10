d750bf2dbf456bddb1ad7994d88d759ca837ba4d
jeevanPalapati10 <jeevan@ve.ai>
2025-07-10 12:01:52 +0530

    resoved navbar crop issue

--- builderSrc/views/components/library/elementPopups/NavImagePopup.jsx
+++ builderSrc/views/components/library/elementPopups/NavImagePopup.jsx
@@ -624,7 +624,7 @@ class NavImagePopup extends Images {
 									<input
 										type="range"
 										min={1}
-										max={5}
+										max={2}
 										step={0.1}
 										value={
 											this.props.isMobileNavbar
