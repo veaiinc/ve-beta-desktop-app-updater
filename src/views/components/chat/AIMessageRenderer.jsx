352b42737893453b665bfe4d4a6948510f4fc619
Rupesh Macharla <rupesh@ve.ai>
2025-07-12 11:03:12 +0530

    changes

--- src/views/components/chat/AIMessageRenderer.jsx
+++ src/views/components/chat/AIMessageRenderer.jsx
@@ -24,7 +24,7 @@ const AIMessageRenderer = ({
 				<>
 					{(messageData?.processing === 'Deep Search' ||
 						messageData?.processing === 'Deep Research' ||
-						messageData?.processing === 'normal_search' ||
+						messageData?.processing === 'Normal Search' ||
 						messageData?.memory_thinking) && (
 						<ChainOfThoughtWidget messageData={messageData} />
 					)}
