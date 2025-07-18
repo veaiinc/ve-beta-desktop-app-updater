f22ef5953df8fa126d9ecd0a8104a5c618cef98f
Shahnawaz <shahnawaz@ve.ai>
2025-07-18 16:02:24 +0530

    files bugfix

--- builderSrc/views/components/ViewDocument/viewDocument.jsx
+++ builderSrc/views/components/ViewDocument/viewDocument.jsx
@@ -498,7 +498,7 @@ const ViewDocument = ({ workflowId, templateID }) => {
 	};
 	return (
 		<div className="viewDocumentContainer">
-			<div className="back-to-files" onClick={() => navigate(`/files?activeTab=Documents`)}>
+			<div className="back-to-files" onClick={() => window.history.back()}>
 				<span className="back-arrow">&#8592;</span> Back to Files
 			</div>
 			<MainDocumentSection workflowId={workflowId} templateID={templateID} />
