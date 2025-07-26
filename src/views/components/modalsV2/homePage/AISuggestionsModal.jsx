dc87d1801fa0a877fc7e26957b4b09208c904a54
Rupesh Macharla <rupesh@ve.ai>
2025-07-26 18:55:54 +0530

    changes

--- src/views/components/modalsV2/homePage/AISuggestionsModal.jsx
+++ src/views/components/modalsV2/homePage/AISuggestionsModal.jsx
@@ -54,7 +54,7 @@ const AISuggestionsModal = ({
 			getAISuggestedPendingActions,
 			handleGlobalChatMessages,
 		},
-		companyInfo: { getTeamMembers, tenantsUserList },
+		profileInfo: { getTenantUserAccessControls, tenantUserAccessControls },
 	} = useContext(Context);
 	const [info, setInfo] = useState({
 		isAIResultsExpanded: true,
@@ -69,6 +69,7 @@ const AISuggestionsModal = ({
 		isDeleting: false,
 		tabOptions: [],
 		accessType: 'view',
+		hasFullAccess: false,
 	});
 
 	const resizableContainerRef = useRef(null);
@@ -83,20 +84,26 @@ const AISuggestionsModal = ({
 		const { user_id } = jwtDecode(token);
 		setInfo((prev) => ({ ...prev, currentUserId: user_id }));
 
-		if (!tenantsUserList) {
-			getTeamMembers();
+		if (!tenantUserAccessControls) {
+			getTenantUserAccessControls();
 		}
 	}, []);
+
 	useEffect(() => {
-		if (info?.currentUserId && tenantsUserList) {
-			const currentUserIndex = tenantsUserList?.findIndex(
-				(user) => user?._id === info?.currentUserId,
-			);
-			if (currentUserIndex !== -1 && tenantsUserList?.[currentUserIndex]?.role === 'admin') {
-				setInfo((prev) => ({ ...prev, isAdmin: true }));
+		if (info?.currentUserId && tenantUserAccessControls) {
+			let hasFullAccess = false;
+			if (tenantUserAccessControls?.role === 'admin') {
+				hasFullAccess = true;
+			} else if (tenantUserAccessControls?.accessControls) {
+				tenantUserAccessControls?.accessControls?.forEach((access) => {
+					if (access?.app === 'insights' && access?.hasFullAccess && access?.isEnabled) {
+						hasFullAccess = true;
+					}
+				});
 			}
+			setInfo((prev) => ({ ...prev, hasFullAccess }));
 		}
-	}, [info?.currentUserId, tenantsUserList]);
+	}, [info?.currentUserId, tenantUserAccessControls]);
 
 	useEffect(() => {
 		if (!data) return;
@@ -295,7 +302,7 @@ const AISuggestionsModal = ({
 	};
 
 	const handleDeleteCard = useCallback(async () => {
-		if (info?.accessType === 'view' && !info?.isAdmin) {
+		if (info?.accessType === 'view' && !info?.hasFullAccess) {
 			message.error('You do not have access to delete this insight');
 			return;
 		}
@@ -317,11 +324,11 @@ const AISuggestionsModal = ({
 		pendingActionsUpdate,
 		info.isDeleting,
 		info?.accessType,
-		info?.isAdmin,
+		info?.hasFullAccess,
 	]);
 
 	const handleOpenFeedbackPopup = () => {
-		if (info?.accessType == 'view' && !info?.isAdmin) {
+		if (info?.accessType == 'view' && !info?.hasFullAccess) {
 			message.error('You do not have access to give feedback');
 			return;
 		}
@@ -413,7 +420,7 @@ const AISuggestionsModal = ({
 										Teach me
 									</div>
 								)}
-								{(info?.accessType !== 'view' || info?.isAdmin) && (
+								{(info?.accessType !== 'view' || info?.hasFullAccess) && (
 									<ProactiveAIShare
 										proactiveAiId={data?._id}
 										proactiveAiData={data}
