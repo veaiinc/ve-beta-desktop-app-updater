d5df6816e03e22b75fee7c634f39eb84c8b06900
Sheshant Manure <sheshant@ve.ai>
2025-07-28 19:17:02 +0530

    loading

--- src/views/features/gallery/GalleryPage.jsx
+++ src/views/features/gallery/GalleryPage.jsx
@@ -5173,7 +5173,7 @@ const GalleryPage = () => {
 															<p>Add Photos</p>
 														</div>
 													</div>
-													{info?.imagesList?.docs && info?.albumLoading
+													{info?.imagesList?.docs && !info?.albumLoading
 														? info?.imagesList?.docs?.map(
 																(image, index) => {
 																	const params = `Key-Pair-Id=${galleryCredentials?.['Key-Pair-Id']}&Signature=${galleryCredentials?.Signature}&Policy=${galleryCredentials?.Policy}`;
