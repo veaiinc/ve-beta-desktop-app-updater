85219017dab90266fed7a7285bc232fb0c4f01b1
Md-Abdullah-ve <abdullah@ve.ai>
2025-08-04 15:33:28 +0530

    updated width in invoice in mobile view

--- builderSrc/views/components/library/layouts/invoices/index.jsx
+++ builderSrc/views/components/library/layouts/invoices/index.jsx
@@ -2396,6 +2396,11 @@ class Invoice extends Component {
 	}
 
 	render() {
+		const isiOS =
+			this.props?.client && this.props?.previewType === 'm'
+				? /iP(hone|ad|od)/.test(navigator.platform) ||
+				  (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
+				: true;
 		return (
 			<div
 				className="block"
@@ -2405,6 +2410,7 @@ class Invoice extends Component {
 							? '0px 0px'
 							: '0px 0px',
 					width: this.state?.previewType === 'm' && '400px',
+					width: this.state?.previewType === 'm' && (isiOS ? '400px' : '375'),
 				}}
 				onClick={(e) => {
 					// e.stopPropagation();
