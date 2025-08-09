e3504b67054ea4a0604517359900d177ad2028e0
Gowtham <gowtham@ve.ai>
2025-08-09 20:13:38 +0530

    changes

--- builderSrc/views/components/library/layouts/NabarWrapper/NavbarComponent.jsx
+++ builderSrc/views/components/library/layouts/NabarWrapper/NavbarComponent.jsx
@@ -1,8 +1,12 @@
 import React, { Component, useEffect } from 'react';
 import ImageItem from '../../elements/image';
-import { EditNavbar as Edit } from '../../../builder_client_common';
 import './NavbarCompStyles.scss';
-import { ElementSidebar, DownloadPDF, DropDownSvg } from '../../../builder_client_common';
+import {
+	ElementSidebar,
+	DownloadPDF,
+	Dropdown as DropDownSvg,
+	EditNavbar as Edit,
+} from '../../../builder_client_common';
 import CartIcons from '../NabarWrapper/CartIcons';
 import _ from 'lodash';
 import { Tooltip } from 'antd';
