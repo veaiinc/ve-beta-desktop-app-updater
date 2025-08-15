eaf85f054cdcef2de3a1200fbee10ddd55c63cf3
Krishna Sai <krishnasai@ve.ai>
2025-08-15 13:21:42 +0530

    updated the desktopApp Intimaiton logic

--- src/views/components/gallery/galleryPage/DesktopAppIntimation.jsx
+++ src/views/components/gallery/galleryPage/DesktopAppIntimation.jsx
@@ -3,8 +3,27 @@ import s from '../../../../assets/scss/gallery/desktopAppIntimation.module.scss'
 import ReactModal from '../../modalsV2';
 
 const desktopAppDownloadUrl = import.meta.env.VITE_APP_DESKTOP_APP_DOWNLOAD_URL || null;
+const deepLinkUrl = 'veai://open';
 
 const DesktopPopup = ({ open, closeModal, onStandardUploadClick }) => {
+	const handleInstallOrOpen = () => {
+		window.location.href = deepLinkUrl;
+
+		const timer = setTimeout(() => {
+			if (desktopAppDownloadUrl) {
+				window.open(desktopAppDownloadUrl, '_blank');
+			}
+		}, 2000);
+
+		// If user switches focus (e.g., app opened), cancel fallback
+		window.addEventListener(
+			'blur',
+			() => {
+				clearTimeout(timer);
+			},
+			{ once: true },
+		);
+	};
 	return (
 		<ReactModal
 			isOpen={open}
@@ -20,11 +39,12 @@ const DesktopPopup = ({ open, closeModal, onStandardUploadClick }) => {
 				</div>
 				<button
 					className={s.desktopDownloadButton}
-					onClick={() => {
-						if (desktopAppDownloadUrl) {
-							window.open(desktopAppDownloadUrl, '_blank');
-						}
-					}}
+					// onClick={() => {
+					// 	if (desktopAppDownloadUrl) {
+					// 		window.open(desktopAppDownloadUrl, '_blank');
+					// 	}
+					// }}
+					onClick={handleInstallOrOpen}
 				>
 					Install
 				</button>
