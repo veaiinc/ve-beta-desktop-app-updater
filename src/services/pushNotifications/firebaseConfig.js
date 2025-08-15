68f70d1514c3de055cb2bf117963ff8fe2cc6570
Sheshant Manure <sheshant@ve.ai>
2025-08-15 20:07:02 +0530

    feat: receive push notifications

--- src/services/pushNotifications/firebaseConfig.js
+++ src/services/pushNotifications/firebaseConfig.js
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
+
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
 	authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
@@ -8,4 +11,8 @@ const firebaseConfig = {
 	measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+export { messaging };
 export default firebaseConfig;
