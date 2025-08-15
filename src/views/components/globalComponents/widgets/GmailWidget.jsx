93a722d99220776f64fe96723bb9116cd3f95cde
Rupesh Macharla <rupesh@ve.ai>
2025-08-15 12:05:04 +0530

    changes

--- src/views/components/globalComponents/widgets/GmailWidget.jsx
+++ src/views/components/globalComponents/widgets/GmailWidget.jsx
@@ -10,31 +10,68 @@ Best,
 Brandn`;
 
 const GmailWidget = () => {
-	const [info, setInfo] = useState({ data: {}, editEnabled: false });
+	const [info, setInfo] = useState({
+		data: { body: emailBody, to: 'test@test.com' },
+		editEnabled: false,
+	});
 
 	const handleEditClick = useCallback(() => {
-		setInfo((prev) => ({ ...prev, editEnabled: true }));
+		setInfo((prev) => {
+			return { ...prev, editEnabled: !prev?.editEnabled };
+		});
+	}, []);
+
+	const handleContentChange = useCallback((e, type) => {
+		setInfo((prev) => {
+			if (type === 'body') {
+				return { ...prev, data: { ...prev?.data, body: e.target.value } };
+			} else if (type === 'to') {
+				return { ...prev, data: { ...prev?.data, to: e.target.value } };
+			}
+			return prev;
+		});
 	}, []);
 
 	return (
 		<div className={styles.gmailWidgetContainer}>
 			<div className={styles.gmailWidgetTitle}>Draft Email Preview </div>
-			<div className={styles.gmailWidgetBody}>
-				<span>{emailBody}</span>
-			</div>
-			<div className={styles.buttonsContainer}>
-				{/* <button className={styles.eachButton}>
+			<div className={styles.gmailContent}>
+				<div className={styles.to}>
+					<div className={styles.toTitle}>To</div>
+					<input
+						type="text"
+						className={styles.toInput}
+						value={info?.data?.to || ''}
+						disabled={!info?.editEnabled}
+						onChange={(e) => handleContentChange(e, 'to')}
+					/>
+				</div>
+				<div className={styles.body}>
+					<div className={styles.bodyHeader}>Body</div>
+					<div className={styles.gmailWidgetBody}>
+						<textarea
+							className={styles.textarea}
+							value={info?.data?.body || ''}
+							rows={8}
+							disabled={!info?.editEnabled}
+							onChange={(e) => handleContentChange(e, 'body')}
+						/>
+					</div>
+				</div>
+				<div className={styles.buttonsContainer}>
+					{/* <button className={styles.eachButton}>
 					<CrossSvg />
 					Dismiss
 				</button> */}
-				<button className={styles.eachButton} onClick={handleEditClick}>
-					<EditSvg />
-					Edit First
-				</button>
-				<button className={`${styles.eachButton} ${styles.sendSvg}`}>
-					<SendSvg />
-					Send Now
-				</button>
+					<button className={styles.eachButton} onClick={handleEditClick}>
+						<EditSvg />
+						{info?.editEnabled ? 'Stop Editing' : 'Edit First'}
+					</button>
+					<button className={`${styles.eachButton} ${styles.sendSvg}`}>
+						<SendSvg />
+						Send Now
+					</button>
+				</div>
 			</div>
 		</div>
 	);
