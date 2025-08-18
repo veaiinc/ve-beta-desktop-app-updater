ca17bb349ecac3a6bc076d890d87dc85b61f9aef
Sheshant Manure <sheshant@ve.ai>
2025-08-18 19:52:47 +0530

    updates

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js');
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-	measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
+	apiKey: 'AIzaSyCoWbQyV42a2GNWIAcHPf3PBbm9D3glDJU',
+	authDomain: 'veai-notifications.firebaseapp.com',
+	projectId: 'veai-notifications',
+	storageBucket: 'veai-notifications.firebasestorage.app',
+	messagingSenderId: '719556623749',
+	appId: '1:719556623749:web:5834eb840431480e5318ec',
+	measurementId: 'G-B7K087ZV97',
 };
 
 firebase.initializeApp(firebaseConfig);
