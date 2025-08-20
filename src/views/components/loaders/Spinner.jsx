b78e3cd67e556074f5670e4388b84e66a3e2294f
Rakeshveai <rakesh@ve.ai>
2025-08-20 20:09:58 +0530

    changes in create doc

--- src/views/components/loaders/Spinner.jsx
+++ src/views/components/loaders/Spinner.jsx
@@ -1,20 +1,3 @@
-// import React, { memo } from 'react';
-
-// const Spinner = ({ width, height, color, cssstyle = {}, borderTopColor ,borderWidth=3}) => {
-// 	const style = {
-// 		width: width || '18px',
-// 		height: height || '18px',
-// 		border: `${borderWidth}px solid ${color || 'var(--primary-font)'}`,
-// 		borderTop: `4px solid ${borderTopColor || 'transparent'}`,
-// 		borderRadius: '50%',
-// 		animation: 'spin 1s linear infinite',
-// 		...cssstyle,
-// 	};
-// 	return <div style={style}></div>;
-// };
-
-// export default memo(Spinner);
-
 import React from 'react';
 import { Alert, Flex, Spin } from 'antd';
 import './App.scss'; // Include this CSS file for custom styling
@@ -22,19 +5,24 @@ import useWindowSize from '../../../hooks/useWindowSize';
 
 const contentStyle = {
 	padding: 30,
-	// borderRadius: 4,
-	background: ' #121212 !important',
+	backgroundColor: ' #121212 !important',
 };
 
 const content = <div style={contentStyle} />;
 
 const App = ({ size }) => {
 	const { width, height } = useWindowSize();
+	let theme = localStorage.getItem('theme');
+	theme = 'light';
+	// console.log(theme, 'theme');
 	return (
 		<Flex gap="middle" vertical>
 			<Flex gap="middle">
 				<Spin
 					size={size ? size : width > 768 ? 'large' : 'small'}
+					style={{
+						background: theme == 'dark' ? '#121212 !important' : '#ffffff !important',
+					}}
 					className="custom-spinner"
 				>
 					{content}
