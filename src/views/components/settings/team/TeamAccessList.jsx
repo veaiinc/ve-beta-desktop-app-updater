573d1c07dffece41d7f1a5129989d860dbf2cddf
Sheshant Manure <sheshant@ve.ai>
2025-08-21 19:00:00 +0530

    remove react import

--- src/views/components/settings/team/TeamAccessList.jsx
+++ src/views/components/settings/team/TeamAccessList.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useState, useEffect } from 'react';
+import { memo, useState, useEffect } from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { getInitials } from '../../../../helpers/index';
