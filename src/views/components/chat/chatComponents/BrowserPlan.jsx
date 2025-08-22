49c2ae07a3cc490bd1dbe08351d1ecf9f876b989
Rupesh Macharla <rupesh@ve.ai>
2025-08-22 17:57:19 +0530

    ui updates browser

--- src/views/components/chat/chatComponents/BrowserPlan.jsx
+++ src/views/components/chat/chatComponents/BrowserPlan.jsx
@@ -4,16 +4,23 @@ import s from '../../../../assets/scss/chat/chatComponents/browserPlan.module.sc
 const BrowserPlan = ({ browserPlan = null }) => {
 	return (
 		<div className={s.browserPlanContainer}>
-			<div className={s.title}>{browserPlan?.task || ''}</div>
-			<ul className={s.actions}>
-				{browserPlan?.action?.map((action, index) => {
-					return (
-						<li className={s.action} key={index}>
-							{action || ''}
-						</li>
-					);
-				})}
-			</ul>
+			<div className={s.titleContainer}>
+				<div className={s.header}>My Plan is:</div>
+				<div className={s.title}>{browserPlan?.task || ''}</div>
+			</div>
+
+			<div className={s.actionsContainer}>
+				<div className={s.actionsTitle}>I will do the following:</div>
+				<ul className={s.actions}>
+					{browserPlan?.action?.map((action, index) => {
+						return (
+							<li className={s.action} key={index}>
+								{action || ''}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 };
--- src/views/components/chat/chatComponents/BrowserTools.jsx
+++ src/views/components/chat/chatComponents/BrowserTools.jsx
@@ -7,22 +7,43 @@ const textMapper = {
 	switch_tab: 'Switching to different tab',
 	extract_page_content: 'Extracting page content',
 	screenshot_and_html: 'Capturing screenshot',
-	scroll_mouse_wheel: 'Scrolling down',
+	scroll_mouse_wheel: 'Scrolling',
 	wait: 'Waiting for the page to load',
+	input_text: 'Typing',
+	go_back: 'Going back to previous page in history',
+	click_element: 'Clicking element',
+	hover_to_element: 'Hovering on element',
+	scroll_to_element: 'Scrolling to an element',
+	select_option: 'Selecting element',
+	get_select_options: 'Retrieving available options for selected element',
+	navigate_to: 'Browsing',
 };
 
 const BrowserTools = ({ data = [] }) => {
+	const getBrowserTools = (tool) => {
+		const { toolName, params } = tool || {};
+
+		if (toolName === 'navigate_to') {
+			return (
+				<>
+					{textMapper[toolName] ?? ''}
+					<span>{params?.url}</span>
+				</>
+			);
+		} else if (toolName === 'input_text') {
+			<>
+				{textMapper[toolName] ?? ''}
+				<span>{params?.text}</span>
+			</>;
+		} else {
+			return textMapper[toolName] ?? '';
+		}
+	};
 	return (
 		<div className={s.browserTools}>
 			{data?.map((tool, index) => (
 				<div key={index} className={s.tool}>
-					{tool?.toolName === 'navigate_to' ? (
-						<>
-							Browsing : <span>{tool?.params?.url}</span>
-						</>
-					) : (
-						textMapper[tool?.toolName] ?? ''
-					)}
+					{getBrowserTools(tool)}
 				</div>
 			))}
 		</div>
