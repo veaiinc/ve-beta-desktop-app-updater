31abd966256a54eadd2d10c8d2ad51c2056e4c48
Rupesh Macharla <rupesh@ve.ai>
2025-08-23 14:09:46 +0530

    changes

--- src/views/features/homePage/NewUi.jsx
+++ src/views/features/homePage/NewUi.jsx
@@ -490,7 +490,10 @@ const NewUi = ({ handleActiveChatChange }) => {
 						<div className="loader-container">
 							{skeletonArray?.map((s, index) => {
 								return (
-									<div className={`skeleton-container skeleton-${index + 1}`}>
+									<div
+										key={index}
+										className={`skeleton-container skeleton-${index + 1}`}
+									>
 										<Skeleton
 											height={'100%'}
 											width={'100%'}
