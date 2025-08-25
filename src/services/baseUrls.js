4aa6ef3cabf83208d5d1e31692e4306ddb814419
Sheshant Manure <sheshant@ve.ai>
2025-08-25 20:03:39 +0530

    wip

--- src/services/baseUrls.js
+++ src/services/baseUrls.js
@@ -4,12 +4,21 @@ const environment = import.meta.env.VITE_APP_DEV_ENVIRONMENT ?? 'development';
 const region = Cookies.get('region') ?? localStorage.getItem('region') ?? 'us-east-1';
 
 const globalBaseUrls = {
+	// Auth
 	auth_Api: 'https://auth.ve.ai',
+
+	// Socket
 	chat_ws_api_US: 'wss://ai.us-east-1.ve.ai',
 	guest_chat_ws_api_US: 'wss://guestsearch.us-east-1.ve.ai',
 	meeting_ws_api_US: 'wss://recall.us-east-1.ve.ai',
 	voice_agent_api_US: 'wss://ve-voice-agent-g4ptyv6v.livekit.cloud',
 	browser_ws_api_US: 'wss://browser.us-east-1.ve.ai',
+
+	// Voice Agent
+	generate_voice_agent_token_api: 'https://voice.us-east-1.ve.ai',
+
+	// Browser
+	browser_ws_api: 'wss://browser.us-east-1.ve.ai',
 };
 
 const regionBaseUrls = {
