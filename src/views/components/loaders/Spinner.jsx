9d02885950d5eee77792f01e79029a7fb29dd6db
Sheshant Manure <sheshant@ve.ai>
2025-08-29 19:14:39 +0530

    feat: add new VeLoader component and update Spinner to use it; enhance theme handling in useTheme hook

--- src/views/components/loaders/Spinner.jsx
+++ src/views/components/loaders/Spinner.jsx
@@ -1,16 +1,18 @@
 import React, { memo } from 'react';
+import VeLoader from './VeLoader';
 
 const Spinner = ({ width, height, color, cssstyle = {}, borderTopColor, borderWidth = 3 }) => {
-	const style = {
-		width: width || '32px',
-		height: height || '32px',
-		border: `${borderWidth}px solid ${color || 'var(--primary-button)'}`,
-		borderTop: `1.5px solid ${borderTopColor || 'transparent'}`,
-		borderRadius: '50%',
-		animation: 'spin 1s linear infinite',
-		...cssstyle,
-	};
-	return <div style={style}></div>;
+	// const style = {
+	// 	width: width || '32px',
+	// 	height: height || '32px',
+	// 	border: `${borderWidth}px solid ${color || 'var(--primary-button)'}`,
+	// 	borderTop: `1.5px solid ${borderTopColor || 'transparent'}`,
+	// 	borderRadius: '50%',
+	// 	animation: 'spin 1s linear infinite',
+	// 	...cssstyle,
+	// };
+	// return <div style={style}></div>;
+	return <VeLoader />;
 };
 
 export default memo(Spinner);
