7ad7612de1bdd5486a5faecdbb86e37dad5cde58
Krishna Sai <krishnasai@ve.ai>
2025-09-18 19:39:50 +0530

    Chores

--- src/views/features/gallery/GalleryPage.jsx
+++ src/views/features/gallery/GalleryPage.jsx
@@ -74,7 +74,8 @@ import { ReactComponent as ChevronLeft } from '../../../assets/svg/tasks/chevron
 import { ReactComponent as MoveToIcon } from '../../../assets/svg/gallery/moveToIcon.svg';
 import Spinner from '../../components/loaders/Spinner';
 import UploadPhotosDesktop from './UploadPhotosDesktop';
-import InfiniteScroll from '../../components/globalComponents/InfiniteScroll';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 // const workspaceId = localStorage.getItem('workspaceId');
 
 const dummyImagesArray = Array.from({ length: 10 }, () => ({ isPlaceholderImg: true }));
