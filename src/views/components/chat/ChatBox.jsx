2977901b86f61ee5eca17e46c8914eaeebc3e1e5
Rupesh Macharla <rupesh@ve.ai>
2025-09-19 18:41:40 +0530

    changes

--- src/views/components/chat/ChatBox.jsx
+++ src/views/components/chat/ChatBox.jsx
@@ -515,6 +515,12 @@ const ChatBox = ({
 				message.error('Please wait, AI is already generating a response');
 				return;
 			}
+			if (totalCreditsUsed >= totalCreditsLimit) {
+				updateStateValues({ activePayloadForChat: null });
+				message.error('You have reached your limit of credits');
+				return;
+			}
+
 			const { payload, localPayload, currentQuery, recentFiles = [] } = activePayloadForChat;
 			if (handleSendWebsocketMessage) {
 				handleSendWebsocketMessage(payload, currentQuery, '', info?.chatSessionId);
