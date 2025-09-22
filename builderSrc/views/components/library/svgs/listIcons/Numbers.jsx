baa211946d8ebd062727bd5f32f65826cd55e4e9
gowtham <gowtham@ve.ai>
2025-09-23 00:42:15 +0530

    ran npm run format

--- builderSrc/views/components/library/svgs/listIcons/Numbers.jsx
+++ builderSrc/views/components/library/svgs/listIcons/Numbers.jsx
@@ -10,10 +10,11 @@ const Numbers = ({ color, size, listCount }) => {
 	};
 	const iconSize = sizeValues[size] || sizeValues.medium;
 
-    return (
-        <span style={{ fontSize: iconSize, color: color || 'gray' ,fontWeight:500}}>
-          {listCount < 10 ? `0${listCount}` : listCount}
-        </span>
-      );};
+	return (
+		<span style={{ fontSize: iconSize, color: color || 'gray', fontWeight: 500 }}>
+			{listCount < 10 ? `0${listCount}` : listCount}
+		</span>
+	);
+};
 
 export default Numbers;
