c135cf1b992e518e6fcaac6faf8392044f0986ba
Sheshant Manure <sheshant@ve.ai>
2025-09-25 14:27:59 +0530

    Add 'plotly.js-basic-dist' dependency and update build memory limit in package.json

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,6 @@ export default defineConfig({
 		target: 'es2015',
 		sourcemap: true,
 		reportCompressedSize: false,
-		rollupOptions: {
-			external: ['plotly.js-basic-dist'],
-		},
 	},
 	plugins: [
 		react(),
