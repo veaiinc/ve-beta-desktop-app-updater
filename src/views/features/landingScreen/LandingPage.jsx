6ec20d3eaf997ff13fafb7b9032db84ae4ad22d0
shahnawaz <shahnawaz@ve.ai>
2025-09-27 12:54:41 +0530

    responsive

--- src/views/features/landingScreen/LandingPage.jsx
+++ src/views/features/landingScreen/LandingPage.jsx
@@ -179,6 +179,7 @@ const LandingPage = () => {
 				<FullscreenIMac ref={fullscreenIMacRef} />
 				{/* Additional content to ensure scrollable height for fullscreen animation and pinned text */}
 				<div
+					className="dummy-div"
 					style={{
 						height: '93vh',
 						background: 'transparent',
