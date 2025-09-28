bbb082dc861128c1eaeff9710f04be2e74c90db6
gowtham <gowtham@ve.ai>
2025-09-28 18:00:07 +0530

    undermaintainence + comment signup

--- src/views/features/landingScreen/LandingPage.jsx
+++ src/views/features/landingScreen/LandingPage.jsx
@@ -243,7 +243,8 @@ const LandingPage = () => {
 							<div className="login-container">
 								<button
 									className="login-btn"
-									onClick={handleDownloadVeAppPopupOpen}
+									// onClick={handleDownloadVeAppPopupOpen}
+									onClick={() => navigate('/verify-user')}
 								>
 									Signup
 								</button>
