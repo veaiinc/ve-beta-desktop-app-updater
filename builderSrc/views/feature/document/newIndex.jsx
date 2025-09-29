2b1d2425ee60dc32fc3211e7bbfae2428707cbce
Rakeshveai <rakesh@ve.ai>
2025-09-29 17:56:31 +0530

    span added instead of p

--- builderSrc/views/feature/document/newIndex.jsx
+++ builderSrc/views/feature/document/newIndex.jsx
@@ -1761,7 +1761,7 @@ const CreateDocument = () => {
 									onOpenChange={() => closeToolTip()}
 								>
 									<div className="client-name">
-										<span>Client Name</span>
+										<span style={{ color: '#94989e' }}>Client Name</span>
 									</div>
 									{/* <div className="chooseClientTriggerContainer">
 										<span>Client Name</span>
