12c23e126cd2893c4702f4a37169d4e0a358d92c
Sheshant Manure <sheshant@ve.ai>
2025-09-29 18:07:10 +0530

    Refactor token refresh logic to separate access token retrieval and retry mechanism

--- src/services/index.js
+++ src/services/index.js
@@ -31,7 +31,7 @@ const handleHeaders = (token, type) => {
 
 export const internalServerEmitter = mitt();
 
-const refreshAccessTokenAndRetry = async (requestData) => {
+const refreshAccessToken = async () => {
 	const token = Cookies.get('usertoken') ?? localStorage.getItem('usertoken');
 	const region = Cookies.get('region') ?? localStorage.getItem('region') ?? 'us-east-1';
 	const baseUrl = getBaseUrl({ type: 'auth', region });
@@ -44,36 +44,53 @@ const refreshAccessTokenAndRetry = async (requestData) => {
 		},
 		credentials: 'include',
 	});
-	if (response.status === 200) {
-		const jsonData = await response.json();
-		const { tokens } = jsonData;
+	return response;
+};
+
+const refreshAccessTokenAndRetry = async (requestData) => {
+	const response = await refreshAccessToken();
+	const status = response.status;
+	const refreshTokenResponse = await response.json();
+	if (status === 200) {
+		// set the new access token and access token expiry
+		const { tokens } = refreshTokenResponse;
 		const { accessToken, accessTokenExpiry } = tokens;
 		const host = fetchDomainName();
 		Cookies.set('usertoken', accessToken, { sameSite: 'lax', domain: host });
 		Cookies.set('accessTokenExpiry', accessTokenExpiry, { sameSite: 'lax', domain: host });
 		localStorage.setItem('usertoken', accessToken);
 		localStorage.setItem('accessTokenExpiry', accessTokenExpiry);
-
-		const { endpoint, method, headers, body } = requestData;
+		// retry the request with the new access token
+		const { endpoint, method, body, type } = requestData;
+		const headers = handleHeaders(accessToken, type);
 		const resp = await fetch(endpoint, { method, headers, body });
-		return await processResponse(resp, requestData, true);
-	} else if (response.status === 401 || response.status === 403) {
-		logout();
-		return [false, {}, response.status];
+		const success = resp.status >= 200 && resp.status < 300;
+		const data = await resp.json();
+		const status = resp.status;
+		return [success, data, status];
+	} else if (status === 401 || status === 403) {
+		if (
+			refreshTokenResponse.message === 'jwt expired' ||
+			refreshTokenResponse.message === 'Invalid refresh token, please login again'
+		) {
+			logout();
+			return [false, refreshTokenResponse, status];
+		} else {
+			return [false, refreshTokenResponse, status];
+		}
 	} else {
-		const jsonData = await response.json();
-		return [false, jsonData, response.status];
+		return [false, refreshTokenResponse, status];
 	}
 };
 
-const processResponse = async (response, requestData, shouldExit = false) => {
-	if (shouldExit) logout();
+const processResponse = async (response, requestData) => {
 	const jsonData = await response.json();
 	const responseStatus = response.status;
 	if (responseStatus >= 200 && responseStatus < 300) {
 		return [true, jsonData, responseStatus];
-	} else if (responseStatus === 401 || responseStatus === 403) {
-		return await refreshAccessTokenAndRetry(requestData);
+	} else if (responseStatus === 401 && jsonData.message === 'jwt expired') {
+		const [success, data, status] = await refreshAccessTokenAndRetry(requestData);
+		return [success, data, status];
 	} else if (responseStatus === 500) {
 		internalServerEmitter.emit('serverError', jsonData);
 		return [false, jsonData, responseStatus];
@@ -117,7 +134,7 @@ const apiFetch = async (url, method, body, token, type, abortSignal = null) => {
 		}
 
 		const response = await fetch(endpoint, options);
-		const requestData = { endpoint, ...options };
+		const requestData = { endpoint, ...options, type };
 		const [success, data, status] = await processResponse(response, requestData);
 		return [success, data, status];
 	} catch (error) {
