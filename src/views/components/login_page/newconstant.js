9d873699e2717e5aac2c3640d6a4f4eafc3002ff
jeevanPalapati10 <jeevan@ve.ai>
2025-10-01 23:23:50 +0530

    added new ui for description

---
+++ src/views/components/login_page/newconstant.js
@@ -0,0 +1,18 @@
+export const newLoginDescription = [
+	{
+		title: 'Built around you. Learns with you.',
+		description:
+			'Always present. Always learning. Ve adapts with you, so momentum never breaks.',
+	},
+	{
+		title: 'OWN YOUR MEMORY. WORK WITH CLARITY.',
+		description:
+			'Ve captures context, decisions, and details so nothing slips through the cracks.',
+	},
+
+	{
+		title: 'SAFE AND SECURE. ALWAYS IN CONTROL.',
+		description:
+			'Your data stays private, encrypted, and fully yours no exceptions, no surprises.',
+	},
+];
