1050ca6016091a4ec4b48cd05e59e26fc72e85bb
jeevanPalapati10 <jeevan@ve.ai>
2025-10-01 12:22:14 +0530

    corrected base url

--- src/context/Templates/state.jsx
+++ src/context/Templates/state.jsx
@@ -1654,7 +1654,7 @@ export const TemplatesState = (props) => {
 					path: `/auth/gmail/${workspaceId}/${id}/deactivate-integration`,
 					apiType: 'calendar_api',
 				},
-				googleCalendar: {
+				'google-calendar': {
 					path: `/google-calendar/${workspaceId}/${id}/deactivate-integration`,
 					apiType: 'calendar_api',
 				},
@@ -1662,7 +1662,7 @@ export const TemplatesState = (props) => {
 					path: `/slack/${workspaceId}/${id}/deactivate-integration`,
 					apiType: 'third_party_integrations_api',
 				},
-				outlookCalendars: {
+				outlookCalendar: {
 					path: `/outlookcalendar/${workspaceId}/${id}/deactivate-integration`,
 					apiType: 'microsoft_integration_api',
 				},
