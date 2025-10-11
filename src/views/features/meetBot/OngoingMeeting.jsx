83a12210e2343b0a923141a753f81ff4ee8abc1a
sabith <sabith@ve.ai>
2025-10-11 19:04:45 +0530

    removed the default close and topbar from the ongoing meeting page

--- src/views/features/meetBot/OngoingMeeting.jsx
+++ src/views/features/meetBot/OngoingMeeting.jsx
@@ -377,12 +377,12 @@ const OngoingMeeting = memo(() => {
 				{/* <button className={s.ongoingMeetingHeaderButton}>
 					<Maximize2 size={16} />
 				</button> */}
-				<button
+				{/* <button
 					onClick={() => navigate('/home')}
 					className={s.ongoingMeetingHeaderButtonClose}
 				>
 					<X size={16} />
-				</button>
+				</button> */}
 			</div>
 			<div className={s.ongoingMeetingContentWrapper}>
 				<div className={s.ongoingMeetingContainer}>
