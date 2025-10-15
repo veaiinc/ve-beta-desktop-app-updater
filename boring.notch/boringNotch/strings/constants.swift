5aecb6be1ad5871deb82e8c297e56c268f8ff975
Rakeshveai <rakesh@ve.ai>
2025-10-15 18:13:26 +0530

    added ve desktop github link

--- boring.notch/boringNotch/strings/constants.swift
+++ boring.notch/boringNotch/strings/constants.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-let productPage = URL(string: "https://github.com/TheBoredTeam/boring.notch")!
+let productPage = URL(string: "https://github.com/veaiinc/ve-desktop-app")!
 let sponsorPage = URL(string: "https://buymeacoffee.com/jfxh67wvfxq")!
 
 
